373ef572ae3485b07a37fbf7331807f4a1bd3fd2
Przemek Hugh Kaznowski <phkaznowski@gmail.com>
2017-10-16 16:18:01 +0100

    Revert "Added missing license"
    This reverts commit 5a33a30d8a7553617ce94a1ba9005abcce34d56b.

--- private/enterprise/causal-clustering-commercial/src/test/java/org/neo4j/causalclustering/backup/EncryptedBackupIT.java
+++ private/enterprise/causal-clustering-commercial/src/test/java/org/neo4j/causalclustering/backup/EncryptedBackupIT.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2002-2017 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- * This file is a commercial add-on to Neo4j Enterprise Edition.
- */
 package org.neo4j.causalclustering.backup;
 
 import org.junit.After;
