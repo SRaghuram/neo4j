385f3bb19b4ccab52aef722495285415393b9ace
Chris Gioran <chris@neotechnology.com>
2018-03-27 16:51:32 +0200

    Updates bootstrapping code and hierachy with removal of OpenEnterpriseEntryPoint
    Now everything should rely on and test only CommercialEntryPoint. Also all
     relevant enterprise server tests should be executed only in this repo

--- private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialBootstrapper.java
+++ private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialBootstrapper.java
@@ -5,18 +5,37 @@
  */
 package com.neo4j.server.enterprise;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+
+import org.neo4j.causalclustering.core.CausalClusterConfigurationValidator;
+import org.neo4j.configuration.HaConfigurationValidator;
 import org.neo4j.kernel.GraphDatabaseDependencies;
 import org.neo4j.kernel.configuration.Config;
+import org.neo4j.kernel.configuration.ConfigurationValidator;
 import org.neo4j.logging.LogProvider;
+import org.neo4j.server.CommunityBootstrapper;
 import org.neo4j.server.NeoServer;
-import org.neo4j.server.enterprise.OpenEnterpriseBootstrapper;
 
-public class CommercialBootstrapper extends OpenEnterpriseBootstrapper
+public class CommercialBootstrapper extends CommunityBootstrapper
 {
     @Override
     protected NeoServer createNeoServer( Config configurator, GraphDatabaseDependencies dependencies,
             LogProvider userLogProvider )
     {
         return new CommercialNeoServer( configurator, dependencies, userLogProvider );
+    }
+
+    @Override
+    @Nonnull
+    protected Collection<ConfigurationValidator> configurationValidators()
+    {
+        ArrayList<ConfigurationValidator> validators = new ArrayList<>();
+        validators.addAll( super.configurationValidators() );
+        validators.add( new HaConfigurationValidator() );
+        validators.add( new CausalClusterConfigurationValidator() );
+        return validators;
     }
 }
