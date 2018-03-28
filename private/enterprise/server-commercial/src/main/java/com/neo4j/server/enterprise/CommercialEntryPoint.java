9020715d2ef401d9f1a57cfee7e146535304d49f
lutovich <konstantin.lutovich@neotechnology.com>
2018-03-28 16:58:11 +0200

    Removed unused methods in CommercialEntryPoint

--- private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialEntryPoint.java
+++ private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialEntryPoint.java
@@ -5,14 +5,10 @@
  */
 package com.neo4j.server.enterprise;
 
-import org.neo4j.server.BlockingBootstrapper;
-import org.neo4j.server.Bootstrapper;
 import org.neo4j.server.ServerBootstrapper;
 
 public class CommercialEntryPoint
 {
-    private static Bootstrapper bootstrapper;
-
     private CommercialEntryPoint()
     {
     }
@@ -23,20 +19,6 @@ public class CommercialEntryPoint
         if ( status != 0 )
         {
             System.exit( status );
-        }
-    }
-
-    public static void start( String[] args )
-    {
-        bootstrapper = new BlockingBootstrapper( new CommercialBootstrapper() );
-        System.exit( ServerBootstrapper.start( bootstrapper, args ) );
-    }
-
-    public static void stop( @SuppressWarnings( "UnusedParameters" ) String[] args )
-    {
-        if ( bootstrapper != null )
-        {
-            bootstrapper.stop();
         }
     }
 }
