c1e69710056b613521e0e9aa9747af29de49ddcc
lutovich <konstantin.lutovich@neotechnology.com>
2018-03-29 14:10:12 +0200

    Revert "Merge pull request #40 from neo-technology/3.4-unused-methods-in-CommercialEntryPoint"
    This reverts commit 249ee8bcca80b625768272f21d0bd541c3b818d7, reversing
    changes made to 5f124ad97b2b82c67223adab4270c2938c3c2a44.
    
    Turned out `*EntryPoint#start()` and `*EntryPoint#stop()` are used in
    Windows startup scripts. They are invoked by Prunsrv in Get-Neo4jPrunsrv
    script.

--- private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialEntryPoint.java
+++ private/enterprise/server-commercial/src/main/java/com/neo4j/server/enterprise/CommercialEntryPoint.java
@@ -5,10 +5,14 @@
  */
 package com.neo4j.server.enterprise;
 
+import org.neo4j.server.BlockingBootstrapper;
+import org.neo4j.server.Bootstrapper;
 import org.neo4j.server.ServerBootstrapper;
 
 public class CommercialEntryPoint
 {
+    private static Bootstrapper bootstrapper;
+
     private CommercialEntryPoint()
     {
     }
@@ -19,6 +23,20 @@ public class CommercialEntryPoint
         if ( status != 0 )
         {
             System.exit( status );
+        }
+    }
+
+    public static void start( String[] args )
+    {
+        bootstrapper = new BlockingBootstrapper( new CommercialBootstrapper() );
+        System.exit( ServerBootstrapper.start( bootstrapper, args ) );
+    }
+
+    public static void stop( @SuppressWarnings( "UnusedParameters" ) String[] args )
+    {
+        if ( bootstrapper != null )
+        {
+            bootstrapper.stop();
         }
     }
 }
