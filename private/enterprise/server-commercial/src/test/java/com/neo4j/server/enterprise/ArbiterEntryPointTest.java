cc1d4de5ac636d8178af7d03cc83b5c71dec3e81
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-06-14 16:00:09 +0200

    Cleanup usage of custom System.out channels in tests.
    Switch all of the custom System.out replacements with
    standard SuppressOutput rule or extension.

--- private/enterprise/server-commercial/src/test/java/com/neo4j/server/enterprise/ArbiterEntryPointTest.java
+++ private/enterprise/server-commercial/src/test/java/com/neo4j/server/enterprise/ArbiterEntryPointTest.java
@@ -5,46 +5,30 @@
  */
 package com.neo4j.server.enterprise;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.neo4j.server.enterprise.ArbiterEntryPoint;
+import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.SuppressOutputExtension;
+import org.neo4j.test.rule.SuppressOutput;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.commandline.Util.neo4jVersion;
 
-public class ArbiterEntryPointTest
+@ExtendWith( SuppressOutputExtension.class )
+class ArbiterEntryPointTest
 {
-    private PrintStream realSystemOut;
-    private PrintStream fakeSystemOut;
-
-    @Before
-    public void setup()
-    {
-        realSystemOut = System.out;
-        fakeSystemOut = mock( PrintStream.class );
-        System.setOut( fakeSystemOut );
-    }
-
-    @After
-    public void teardown()
-    {
-        System.setOut( realSystemOut );
-    }
+    @Inject
+    private SuppressOutput suppressOutput;
 
     @Test
-    public void mainPrintsVersion() throws Exception
+    void mainPrintsVersion()
     {
         // when
         ArbiterEntryPoint.main( new String[]{ "--version" } );
 
         // then
-        verify( fakeSystemOut ).println( "neo4j " + neo4jVersion() );
-        verifyNoMoreInteractions( fakeSystemOut );
+        assertTrue( suppressOutput.getOutputVoice().containsMessage( "neo4j " + neo4jVersion() ) );
     }
 }
