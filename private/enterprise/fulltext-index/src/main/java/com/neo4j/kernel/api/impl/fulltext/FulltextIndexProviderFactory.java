b215c9fc1fa62e04df91058d365ebc8838e1b9cd
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-07-26 09:24:24 +0200

    Rename contextDirectory -> directory

--- private/enterprise/fulltext-index/src/main/java/com/neo4j/kernel/api/impl/fulltext/FulltextIndexProviderFactory.java
+++ private/enterprise/fulltext-index/src/main/java/com/neo4j/kernel/api/impl/fulltext/FulltextIndexProviderFactory.java
@@ -70,7 +70,7 @@ public class FulltextIndexProviderFactory extends KernelExtensionFactory<Fulltex
         DirectoryFactory directoryFactory = directoryFactory( ephemeral, fileSystemAbstraction );
         OperationalMode operationalMode = context.databaseInfo().operationalMode;
         NeoStoreDataSource neoStoreDataSource = dependencies.neoStoreDataSource();
-        IndexDirectoryStructure.Factory directoryStructureFactory = subProviderDirectoryStructure( context.contextDirectory() );
+        IndexDirectoryStructure.Factory directoryStructureFactory = subProviderDirectoryStructure( context.directory() );
         Supplier<TokenHolders> tokenHoldersSupplier = dataSourceDependency( neoStoreDataSource, TokenHolders.class );
         Log log = dependencies.getLogService().getInternalLog( FulltextIndexProvider.class );
 
