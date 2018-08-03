fe23d88035d13943c1b520de4f525770d3215005
Satia Herfert <satiaherfert@gmx.de>
2018-08-03 15:58:24 +0200

    Checkstyle

--- private/enterprise/fulltext-index/src/main/java/com/neo4j/kernel/api/impl/fulltext/lucene/FulltextIndexReader.java
+++ private/enterprise/fulltext-index/src/main/java/com/neo4j/kernel/api/impl/fulltext/lucene/FulltextIndexReader.java
@@ -46,7 +46,8 @@ public abstract class FulltextIndexReader implements IndexReader
     }
 
     @Override
-    public void query( IndexProgressor.NodeValueClient client, IndexOrder indexOrder, boolean needsValues, IndexQuery... query ) throws IndexNotApplicableKernelException
+    public void query( IndexProgressor.NodeValueClient client, IndexOrder indexOrder, boolean needsValues, IndexQuery... query )
+            throws IndexNotApplicableKernelException
     {
         throw new IndexNotApplicableKernelException( "Fulltext indexes does not support IndexQuery queries" );
     }
