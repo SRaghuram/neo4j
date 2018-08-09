fb2aa3259d39c32c1fd97dc245ce76eb05fc0b32
lojjs <louise.matematik@gmail.com>
2018-08-09 15:24:10 +0200

    Increase embedded test coverage
    Add the same embedded tests that are present for InternalFlatFileRealm.
    Also fix a test with wrong assertion in
    AuthScenariosInteractionTestBase.

---
+++ private/enterprise/security-commercial/src/test/java/com/neo4j/security/SystemGraphEmbeddedAuthScenariosInteractionIT.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.security;
+
+import java.util.Map;
+
+import org.neo4j.kernel.enterprise.api.security.EnterpriseLoginContext;
+import org.neo4j.server.security.enterprise.auth.AuthScenariosInteractionTestBase;
+import org.neo4j.server.security.enterprise.auth.NeoInteractionLevel;
+
+public class SystemGraphEmbeddedAuthScenariosInteractionIT extends AuthScenariosInteractionTestBase<EnterpriseLoginContext>
+{
+    @Override
+    protected NeoInteractionLevel<EnterpriseLoginContext> setUpNeoServer( Map<String, String> config )
+            throws Throwable
+    {
+        return new SystemGraphEmbeddedInteraction( config, testDirectory );
+    }
+
+    @Override
+    protected Object valueOf( Object obj )
+    {
+        if ( obj instanceof Integer )
+        {
+            return ((Integer) obj).longValue();
+        }
+        else
+        {
+            return obj;
+        }
+    }
+}
---
+++ private/enterprise/security-commercial/src/test/java/com/neo4j/security/SystemGraphEmbeddedConfiguredAuthScenariosInteractionIT.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.security;
+
+import java.util.Map;
+
+import org.neo4j.kernel.enterprise.api.security.EnterpriseLoginContext;
+import org.neo4j.server.security.enterprise.auth.ConfiguredAuthScenariosInteractionTestBase;
+import org.neo4j.server.security.enterprise.auth.NeoInteractionLevel;
+
+public class SystemGraphEmbeddedConfiguredAuthScenariosInteractionIT extends ConfiguredAuthScenariosInteractionTestBase<EnterpriseLoginContext>
+{
+    @Override
+    protected NeoInteractionLevel<EnterpriseLoginContext> setUpNeoServer( Map<String, String> config ) throws Throwable
+    {
+        return new SystemGraphEmbeddedInteraction( config, testDirectory );
+    }
+}
