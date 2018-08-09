c19985b4e60e7cf435fd1c7d9a7b0992b008f4f3
lojjs <louise.matematik@gmail.com>
2018-08-09 15:32:29 +0200

    Add user management procedure logging test

---
+++ private/enterprise/security-commercial/src/test/java/com/neo4j/security/SystemGraphUserManagementProceduresLoggingTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.security;
+
+import com.neo4j.commercial.edition.factory.CommercialGraphDatabaseFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockito.Mockito;
+
+import org.neo4j.dbms.database.DatabaseManager;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.server.security.auth.AuthenticationStrategy;
+import org.neo4j.server.security.auth.BasicPasswordPolicy;
+import org.neo4j.server.security.auth.InMemoryUserRepository;
+import org.neo4j.server.security.enterprise.auth.EnterpriseUserManager;
+import org.neo4j.server.security.enterprise.auth.SecureHasher;
+import org.neo4j.server.security.enterprise.auth.UserManagementProceduresLoggingTest;
+import org.neo4j.server.security.enterprise.configuration.SecuritySettings;
+import org.neo4j.test.rule.TestDirectory;
+
+public class SystemGraphUserManagementProceduresLoggingTest extends UserManagementProceduresLoggingTest
+{
+    private GraphDatabaseService database;
+    private DatabaseManager databaseManager;
+
+    @Rule
+    public final TestDirectory testDirectory = TestDirectory.testDirectory();
+
+    @Before
+    @Override
+    public void setUp() throws Throwable
+    {
+        CommercialGraphDatabaseFactory factory = new CommercialGraphDatabaseFactory();
+        final GraphDatabaseBuilder builder = factory.newEmbeddedDatabaseBuilder( testDirectory.databaseDir() );
+        builder.setConfig( SecuritySettings.auth_provider, SecuritySettings.SYSTEM_GRAPH_REALM_NAME );
+        database = builder.newGraphDatabase();
+        databaseManager = getDatabaseManager();
+        super.setUp();
+    }
+
+    @After
+    public void tearDown()
+    {
+        if ( database != null )
+        {
+            database.shutdown();
+            database = null;
+        }
+    }
+
+    private DatabaseManager getDatabaseManager()
+    {
+        return ((GraphDatabaseAPI) database).getDependencyResolver().resolveDependency( DatabaseManager.class );
+    }
+
+    @Override
+    protected EnterpriseUserManager getUserManager() throws Throwable
+    {
+
+        SystemGraphRealm realm = new SystemGraphRealm(
+                new SystemGraphExecutor( databaseManager, DatabaseManager.DEFAULT_DATABASE_NAME ),
+                new SecureHasher(),
+                new BasicPasswordPolicy(),
+                Mockito.mock( AuthenticationStrategy.class ),
+                true,
+                true,
+                new InMemoryUserRepository(),
+                new InMemoryUserRepository()
+        );
+        realm.initialize();
+        realm.start(); // creates default user and roles
+        return realm;
+    }
+}
