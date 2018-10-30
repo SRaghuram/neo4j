0898b351fcd1de623bfcc6941df927e86c2c0c3a
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-10-30 13:01:12 +0100

    Do not try to evaluate property with same type and different name.

--- private/enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/core/HostnamePortAsListenAddress.java
+++ private/enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/core/HostnamePortAsListenAddress.java
@@ -5,10 +5,6 @@
  */
 package org.neo4j.causalclustering.core;
 
-import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.neo4j.graphdb.config.Setting;
 import org.neo4j.helpers.HostnamePort;
 import org.neo4j.helpers.ListenSocketAddress;
@@ -17,18 +13,9 @@ import org.neo4j.kernel.configuration.Settings;
 
 class HostnamePortAsListenAddress
 {
-    private static final Pattern portRange = Pattern.compile( "(:\\d+)(-\\d+)?$" );
-
     static ListenSocketAddress resolve( Config config, Setting<HostnamePort> setting )
     {
-        Function<String,ListenSocketAddress> resolveFn = Settings.LISTEN_SOCKET_ADDRESS.compose( HostnamePortAsListenAddress::stripPortRange );
-        return config.get( Settings.setting( setting.name(), resolveFn, setting.getDefaultValue() ) );
+        HostnamePort hostnamePort = config.get( setting );
+        return Settings.LISTEN_SOCKET_ADDRESS.apply( hostnamePort.toString() );
     }
-
-    private static String stripPortRange( String address )
-    {
-        Matcher m = portRange.matcher( address );
-        return m.find() ? m.replaceAll( "$1" ) : address;
-    }
-
 }
