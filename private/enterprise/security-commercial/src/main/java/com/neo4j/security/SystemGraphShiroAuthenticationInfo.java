b0c93589b1ce48b6c2203c78509a51f2c1289eab
Henrik Nyman <henrik.nyman@neotechnology.com>
2018-11-05 18:14:07 +0100

    Fix system graph credentials matcher
    The credentials matcher needs to handle migrated legacy credentials
    with a different number of hash iterations also when matching
    cached credentials.
    
    Move credentials matching out of doGetAuthenticationInfo() and
    do it with a custom CredentialsMatcher so that the same code is
    used both the first time and when credentials are already cached.
    Store the user record in the cached authentiction info.

---
+++ private/enterprise/security-commercial/src/main/java/com/neo4j/security/SystemGraphShiroAuthenticationInfo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.security;
+
+import org.neo4j.internal.kernel.api.security.AuthenticationResult;
+import org.neo4j.kernel.impl.security.User;
+import org.neo4j.server.security.enterprise.auth.ShiroAuthenticationInfo;
+
+/**
+ * This is used by SystemGraphRealm to cache a user record in the authentication caches
+ * and update the authentication result based on the outcome of its CredentialsMatcher
+ */
+class SystemGraphShiroAuthenticationInfo extends ShiroAuthenticationInfo
+{
+    private final User userRecord;
+
+    SystemGraphShiroAuthenticationInfo( User userRecord, String realmName )
+    {
+        super( userRecord.name(), realmName, AuthenticationResult.FAILURE );
+        this.userRecord = userRecord;
+    }
+
+    User getUserRecord()
+    {
+        return userRecord;
+    }
+
+    void setAuthenticationResult( AuthenticationResult authenticationResult )
+    {
+        this.authenticationResult = authenticationResult;
+    }
+}
