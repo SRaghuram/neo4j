db97d3ed1587e170c46ccb69c4e911e9ba6f63c6
Zhen Li <zhen.li@neotechnology.com>
2018-11-06 12:15:36 +0100

    Removing changes used in tests

--- private/integrationtests/src/test/java/org/neo4j/bolt/SessionResetIT.java
+++ private/integrationtests/src/test/java/org/neo4j/bolt/SessionResetIT.java
@@ -24,7 +24,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -97,13 +96,6 @@ public class SessionResetIT
     public void tearDown()
     {
         IOUtils.closeAllSilently( driver );
-    }
-
-    @Test
-    public void name() throws InterruptedException
-    {
-        System.out.println(db.boltURI());
-        TimeUnit.HOURS.sleep( 5 );
     }
 
     @Test
