d91c375d5bd1699ce98bdb58fc6e943de005c24c
Louise Söderström <louise.matematik@gmail.com>
2018-11-07 13:30:34 +0100

    Add more testing
    - Refactor ConstraintsWithValuesAcceptanceTest
    - Add more tests for multiple labels (review comments)
    - Add tests for drop constraint and drop index to ensure caches
      are invalidated correctly

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ConstraintsWithValuesAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ConstraintsWithValuesAcceptanceTest.scala
@@ -6,7 +6,7 @@
 package org.neo4j.internal.cypher.acceptance
 
 import org.neo4j.cypher.{ExecutionEngineFunSuite, QueryStatisticsTestSupport}
-import org.neo4j.internal.cypher.acceptance.comparisonsupport.CypherComparisonSupport
+import org.neo4j.internal.cypher.acceptance.comparisonsupport._
 
 class ConstraintsWithValuesAcceptanceTest extends ExecutionEngineFunSuite with QueryStatisticsTestSupport with CypherComparisonSupport  {
 
@@ -15,61 +15,45 @@ class ConstraintsWithValuesAcceptanceTest extends ExecutionEngineFunSuite with Q
     createSomeNodes()
   }
 
-  private def createSingleIndexes(): Unit = {
-    graph.createIndex("Awesome", "prop1")
-    graph.createIndex("Awesome", "prop2")
-    graph.createIndex("Label", "prop1")
-  }
-
-  // Invoked once before the Tx and once in the same Tx
-  def createSomeNodes(): Unit = {
-    graph.execute(
-      """
-      CREATE (:Awesome:Label {prop1: 40, prop2: 5})-[:R]->()
-      CREATE (:Awesome:Label {prop1: 41, prop2: 2})-[:R]->()
-      CREATE (:Awesome:Label {prop1: 42, prop2: 3})-[:R]->()
-      CREATE (:Awesome:Label {prop1: 43, prop2: 1})-[:R]->()
-      CREATE (:Awesome:Label {prop1: 44, prop2: 3})-[:R]->()
-      """)
-  }
-
   test("should use index when existence constraint for property") {
     // Given
     createSingleIndexes()
     graph.createExistenceConstraint("Awesome", "prop1")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should (
-      not(includeSomewhere.aPlan("Projection").withDBHits()) and
-        includeSomewhere.aPlan("NodeIndexScan")
-          .withExactVariables("n", "cached[n.prop1]"))
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertIndexScan(standardResult, "Awesome")
   }
 
-  test("should use index when existence constraint for property - multiple labels") {
+  test("should use index when existence constraint for multiple labels") {
     // Given
     createSingleIndexes()
-    graph.execute(
-      """
-        |CREATE (:Awesome {prop1: 1337, prop2: 5})
-      """.stripMargin)
+    graph.execute("CREATE (:Awesome {prop1: 1337, prop2: 5})")
     graph.createExistenceConstraint("Awesome", "prop1")
     graph.createExistenceConstraint("Label", "prop1")
 
-    // When
-    val query = "MATCH (n:Label:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
+    // Then
+    assertIndexScan(standardResult,"Label", "Awesome")
+  }
+
+  test("should use index when existence constraint for the first of multiple labels") {
+    // Given
+    createSingleIndexes()
+    graph.execute("CREATE (:Awesome {prop1: 1337, prop2: 5})")
+    graph.createExistenceConstraint("Label", "prop1")
 
     // Then
-    result.executionPlanDescription() should (
-      not(includeSomewhere.aPlan("Projection").withDBHits()) and
-        includeSomewhere.aPlan("NodeIndexScan")
-          .withExactVariables("n", "cached[n.prop1]"))
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertIndexScan(standardResult, "Label")
+  }
+
+  test("should use index when existence constraint for the last of multiple labels") {
+    // Given
+    createSingleIndexes()
+    graph.execute("CREATE (:Awesome {prop1: 1337, prop2: 5})")
+    graph.createExistenceConstraint("Awesome", "prop1")
+
+    // Then
+    val expectedResult =  List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44), Map("n.prop1" -> 1337))
+    assertIndexScan(expectedResult,"Awesome")
   }
 
   test("should use index when existence constraint for multiple properties") {
@@ -78,70 +62,70 @@ class ConstraintsWithValuesAcceptanceTest extends ExecutionEngineFunSuite with Q
     graph.createExistenceConstraint("Awesome", "prop1")
     graph.createExistenceConstraint("Awesome", "prop2")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should (
-      not(includeSomewhere.aPlan("Projection").withDBHits()) and
-        includeSomewhere.aPlan("NodeIndexScan"))
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertIndexScan(standardResult,"Awesome")
   }
 
   test("should use index when node key constraint for property") {
     // Given
     graph.createNodeKeyConstraint("Awesome", "prop1")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should (
-      not(includeSomewhere.aPlan("Projection").withDBHits()) and
-        includeSomewhere.aPlan("NodeIndexScan")
-          .withExactVariables("n", "cached[n.prop1]"))
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertIndexScan(standardResult,"Awesome")
   }
 
   test("should not use index when no constraint") {
     // Given
     createSingleIndexes()
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertNodeByLabelScan(standardResult)
   }
 
   test("should not use index when unique constraint") {
     // Given
     graph.createUniqueConstraint( "Awesome", "prop1")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
+    // Then
+    assertNodeByLabelScan(standardResult)
+  }
+
+  test( "should handle constraint drop") {
+    // Given
+    createSingleIndexes()
+    graph.createExistenceConstraint("Awesome", "prop1")
 
     // Then
-    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertIndexScan(standardResult,"Awesome")
+
+    // When
+    graph.execute( "DROP CONSTRAINT ON (n:Awesome) ASSERT exists(n.prop1)" )
+
+    // Then
+    assertNodeByLabelScan(standardResult)
+  }
+
+  test( "should handle index drop") {
+    // Given
+    createSingleIndexes()
+    graph.createExistenceConstraint("Awesome", "prop1")
+
+    // Then
+    assertIndexScan(standardResult, "Awesome")
+
+    // When
+    graph.execute( "DROP INDEX ON :Awesome(prop1)" )
+
+    // Then
+    assertNodeByLabelScan(standardResult)
   }
 
   test("no support for using index when composite node key constraint") {
     // Given
     graph.createNodeKeyConstraint("Awesome", "prop1", "prop2")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertNodeByLabelScan(standardResult)
   }
 
   test("no support for using composite index") {
@@ -149,13 +133,8 @@ class ConstraintsWithValuesAcceptanceTest extends ExecutionEngineFunSuite with Q
     graph.createIndex("Awesome", "prop1", "prop2")
     graph.createExistenceConstraint("Awesome", "prop1")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertNodeByLabelScan(standardResult)
   }
 
   test("existence constraint without index should give labelscan")
@@ -163,13 +142,47 @@ class ConstraintsWithValuesAcceptanceTest extends ExecutionEngineFunSuite with Q
     // Given
     graph.createExistenceConstraint("Awesome", "prop1")
 
-    // When
-    val query = "MATCH (n:Awesome) RETURN n.prop1"
-    val result = executeSingle(query)
-
     // Then
-    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
-    result.toList should equal(List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44)))
+    assertNodeByLabelScan(standardResult)
   }
 
+  private val standardResult = List(Map("n.prop1" -> 40), Map("n.prop1" -> 41), Map("n.prop1" -> 42), Map("n.prop1" -> 43), Map("n.prop1" -> 44))
+
+  private def createSingleIndexes(): Unit = {
+    graph.createIndex("Awesome", "prop1")
+    graph.createIndex("Awesome", "prop2")
+    graph.createIndex("Label", "prop1")
+  }
+
+  private def createSomeNodes(): Unit = {
+    createLabeledNode( Map("prop1" -> 40, "prop2" -> 5), "Awesome", "Label" )
+    createLabeledNode( Map("prop1" -> 41, "prop2" -> 2), "Awesome", "Label" )
+    createLabeledNode( Map("prop1" -> 42, "prop2" -> 3), "Awesome", "Label" )
+    createLabeledNode( Map("prop1" -> 43, "prop2" -> 1), "Awesome", "Label" )
+    createLabeledNode( Map("prop1" -> 44, "prop2" -> 3), "Awesome", "Label" )
+  }
+
+  private def assertNodeByLabelScan( expectedResult: Seq[Map[String, Int]]): Unit = {
+    val query = "MATCH (n:Awesome) RETURN n.prop1"
+    val result = executeWith(Configs.All, query)
+
+    result.executionPlanDescription() should includeSomewhere.aPlan("NodeByLabelScan")
+    result.toList should equal(expectedResult)
+  }
+
+  private def assertIndexScan(expectedResult: Seq[Map[String, Int]], labels: String*): Unit = {
+    val query = s"MATCH (n:${labels.mkString(":")}) RETURN n.prop1"
+
+    // NodeIndexScan is not supported in the compiled runtime
+    val supportedConfig = Configs.All -  TestConfiguration(Versions.V4_0, Planners.all, Runtimes(Runtimes.CompiledBytecode, Runtimes.CompiledSource))
+
+    val result = executeWith(supportedConfig, query)
+
+    result.executionPlanDescription() should (
+      not(includeSomewhere.aPlan("Projection").withDBHits()) and
+        includeSomewhere.aPlan("NodeIndexScan")
+          .withExactVariables("n", "cached[n.prop1]"))
+
+    result.toList should equal(expectedResult)
+  }
 }
