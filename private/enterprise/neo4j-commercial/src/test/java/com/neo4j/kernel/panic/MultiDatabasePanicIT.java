dd6f6bfa74f96a5519fc8c819337925bd1aa1e80
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-11-08 15:03:22 +0100

    Rename KernelEventHandler into DatabaseEventHandler. Introduce new DatabaseEventHandlerAdapter adapter class.

---
+++ private/enterprise/neo4j-commercial/src/test/java/com/neo4j/kernel/panic/MultiDatabasePanicIT.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.kernel.panic;
+
+import com.neo4j.commercial.edition.factory.CommercialGraphDatabaseFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.neo4j.dbms.database.DatabaseManager;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.event.DatabaseEventHandlerAdapter;
+import org.neo4j.graphdb.event.ErrorState;
+import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
+import org.neo4j.kernel.internal.DatabaseHealth;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.TestDirectoryExtension;
+import org.neo4j.test.rule.TestDirectory;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith( TestDirectoryExtension.class )
+class MultiDatabasePanicIT
+{
+    @Inject
+    private TestDirectory testDirectory;
+    private GraphDatabaseService database;
+
+    @BeforeEach
+    void setUp()
+    {
+        database = new CommercialGraphDatabaseFactory().newEmbeddedDatabase( testDirectory.storeDir() );
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        database.shutdown();
+    }
+
+    @Test
+    void databasesPanicSeparately()
+    {
+        DatabaseManager databaseManager = getDatabaseManager();
+        GraphDatabaseFacade firstDatabase = databaseManager.createDatabase( "first" );
+        GraphDatabaseFacade secondDatabase = databaseManager.createDatabase( "second" );
+        PanicDatabaseEventListener firstPanicListener = new PanicDatabaseEventListener();
+        PanicDatabaseEventListener secondPanicListener = new PanicDatabaseEventListener();
+        firstDatabase.registerKernelEventHandler( firstPanicListener );
+        secondDatabase.registerKernelEventHandler( secondPanicListener );
+
+        assertFalse( firstPanicListener.isPanic() );
+        assertFalse( secondPanicListener.isPanic() );
+
+        getDatabaseHealth( firstDatabase ).panic( new IllegalStateException() );
+        assertTrue( firstPanicListener.isPanic() );
+        assertFalse( secondPanicListener.isPanic() );
+
+        getDatabaseHealth( secondDatabase ).panic( new IllegalStateException() );
+        assertTrue( firstPanicListener.isPanic() );
+        assertTrue( secondPanicListener.isPanic() );
+    }
+
+    private DatabaseHealth getDatabaseHealth( GraphDatabaseFacade facade )
+    {
+        return facade.getDependencyResolver().resolveDependency( DatabaseHealth.class );
+    }
+
+    private DatabaseManager getDatabaseManager()
+    {
+        return ((GraphDatabaseAPI) database).getDependencyResolver().resolveDependency( DatabaseManager.class );
+    }
+
+    private static class PanicDatabaseEventListener extends DatabaseEventHandlerAdapter
+    {
+        private volatile boolean panic;
+
+        @Override
+        public void panic( ErrorState error )
+        {
+            panic = true;
+        }
+
+        boolean isPanic()
+        {
+            return panic;
+        }
+    }
+}
