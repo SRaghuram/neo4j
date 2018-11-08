1bd2cacd5c62840fb3edc71f1116309dc3281f40
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-11-08 17:47:02 +0100

    Bump dependencies and plugin versions.
    Bump following version:
    akka 2.5.14 -> 2.5.18
    hazelcast 3.7.8 -> 3.11
    java-8-matchers 1.5 -> 1.6
    netty-tcnative-boringssl-static 2.0.17.Final -> 2.0.19.Final
    pitest-maven 1.0.0 -> 1.4.3
    json 20180130 -> 20180813
    maven-shade-plugin 3.1.1 -> 3.2.0
    checkstyle 8.13 -> 8.14
    guava-testlib 26.0-jre -> 27.0-jre
    commons-lang3 3.8 -> 3.8.1
    commons-text 1.4 -> 1.6
    jProcesses 1.6.4 -> 1.6.5
    assertj-core 3.10.0 -> 3.11.1

--- private/enterprise/causal-clustering/src/test/java/org/neo4j/causalclustering/discovery/HazelcastClientTest.java
+++ private/enterprise/causal-clustering/src/test/java/org/neo4j/causalclustering/discovery/HazelcastClientTest.java
@@ -5,6 +5,7 @@
  */
 package org.neo4j.causalclustering.discovery;
 
+import com.hazelcast.aggregation.Aggregator;
 import com.hazelcast.core.Client;
 import com.hazelcast.core.ClientService;
 import com.hazelcast.core.Cluster;
@@ -25,6 +26,7 @@ import com.hazelcast.core.MultiExecutionCallback;
 import com.hazelcast.core.MultiMap;
 import com.hazelcast.map.EntryProcessor;
 import com.hazelcast.map.MapInterceptor;
+import com.hazelcast.map.QueryCache;
 import com.hazelcast.map.listener.MapListener;
 import com.hazelcast.map.listener.MapPartitionLostListener;
 import com.hazelcast.mapreduce.JobTracker;
@@ -33,6 +35,7 @@ import com.hazelcast.mapreduce.aggregation.Supplier;
 import com.hazelcast.monitor.LocalExecutorStats;
 import com.hazelcast.monitor.LocalMapStats;
 import com.hazelcast.monitor.LocalMultiMapStats;
+import com.hazelcast.projection.Projection;
 import com.hazelcast.query.Predicate;
 import org.junit.Test;
 
@@ -481,6 +484,12 @@ public class HazelcastClientTest
         }
 
         @Override
+        public ICompletableFuture<Object> putAsync( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
+        {
+            return null;
+        }
+
+        @Override
         public ICompletableFuture<Void> setAsync( Object o, Object o2 )
         {
             return null;
@@ -488,6 +497,12 @@ public class HazelcastClientTest
 
         @Override
         public ICompletableFuture<Void> setAsync( Object o, Object o2, long l, TimeUnit timeUnit )
+        {
+            return null;
+        }
+
+        @Override
+        public ICompletableFuture<Void> setAsync( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
         {
             return null;
         }
@@ -517,7 +532,19 @@ public class HazelcastClientTest
         }
 
         @Override
+        public Object put( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
+        {
+            return null;
+        }
+
+        @Override
         public void putTransient( Object key, Object value, long ttl, TimeUnit timeunit )
+        {
+
+        }
+
+        @Override
+        public void putTransient( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
         {
 
         }
@@ -619,6 +646,54 @@ public class HazelcastClientTest
         }
 
         @Override
+        public <R> R aggregate( Aggregator<Entry<Object,Object>,R> aggregator )
+        {
+            return null;
+        }
+
+        @Override
+        public <R> R aggregate( Aggregator<Entry<Object,Object>,R> aggregator, Predicate<Object,Object> predicate )
+        {
+            return null;
+        }
+
+        @Override
+        public <R> Collection<R> project( Projection<Entry<Object,Object>,R> projection )
+        {
+            return null;
+        }
+
+        @Override
+        public <R> Collection<R> project( Projection<Entry<Object,Object>,R> projection, Predicate<Object,Object> predicate )
+        {
+            return null;
+        }
+
+        @Override
+        public QueryCache<Object,Object> getQueryCache( String name )
+        {
+            return null;
+        }
+
+        @Override
+        public QueryCache<Object,Object> getQueryCache( String name, Predicate<Object,Object> predicate, boolean includeValue )
+        {
+            return null;
+        }
+
+        @Override
+        public QueryCache<Object,Object> getQueryCache( String name, MapListener listener, Predicate<Object,Object> predicate, boolean includeValue )
+        {
+            return null;
+        }
+
+        @Override
+        public boolean setTtl( Object key, long ttl, TimeUnit timeunit )
+        {
+            return false;
+        }
+
+        @Override
         public Object aggregate( Supplier supplier, Aggregation aggregation, JobTracker jobTracker )
         {
             return null;
@@ -655,9 +730,21 @@ public class HazelcastClientTest
         }
 
         @Override
+        public Object putIfAbsent( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
+        {
+            return null;
+        }
+
+        @Override
         public boolean remove( Object key, Object value )
         {
             return delegate.remove( key, value );
+        }
+
+        @Override
+        public void removeAll( Predicate<Object,Object> predicate )
+        {
+
         }
 
         @Override
@@ -710,6 +797,12 @@ public class HazelcastClientTest
 
         @Override
         public void set( Object key, Object value, long ttl, TimeUnit timeunit )
+        {
+
+        }
+
+        @Override
+        public void set( Object key, Object value, long ttl, TimeUnit ttlUnit, long maxIdle, TimeUnit maxIdleUnit )
         {
 
         }
@@ -1030,6 +1123,12 @@ public class HazelcastClientTest
         public Collection<Object> remove( Object key )
         {
             return asSet( delegate.remove( key ) );
+        }
+
+        @Override
+        public void delete( Object key )
+        {
+
         }
 
         @Override
