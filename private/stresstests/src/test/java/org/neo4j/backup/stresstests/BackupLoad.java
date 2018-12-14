fbbdc82b843890efb13866cb0b7c9e654d6a6410
lutovich <konstantin.lutovich@neo4j.com>
2018-12-14 10:34:44 +0100

    Simplified error handling in backup stress test

--- private/stresstests/src/test/java/org/neo4j/backup/stresstests/BackupLoad.java
+++ private/stresstests/src/test/java/org/neo4j/backup/stresstests/BackupLoad.java
@@ -5,16 +5,35 @@
  */
 package org.neo4j.backup.stresstests;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
-import org.neo4j.backup.BackupHelper;
-import org.neo4j.backup.BackupResult;
+import org.neo4j.backup.impl.BackupExecutionException;
+import org.neo4j.backup.impl.ConsistencyCheckExecutionException;
+import org.neo4j.backup.impl.OnlineBackupContext;
+import org.neo4j.backup.impl.OnlineBackupExecutor;
 import org.neo4j.causalclustering.stresstests.Control;
+import org.neo4j.function.Predicates;
+import org.neo4j.helper.IsChannelClosedException;
+import org.neo4j.helper.IsConnectionException;
+import org.neo4j.helper.IsConnectionResetByPeer;
+import org.neo4j.helper.IsStoreClosed;
 import org.neo4j.helper.Workload;
+import org.neo4j.helpers.progress.ProgressMonitorFactory;
+import org.neo4j.logging.FormattedLogProvider;
 
 class BackupLoad extends Workload
 {
+    private static final Predicate<Throwable> isTransientError = Predicates.any(
+            new IsConnectionException(),
+            new IsConnectionResetByPeer(),
+            new IsChannelClosedException(),
+            new IsStoreClosed() );
 
     private final String backupHostname;
     private final int backupPort;
@@ -31,14 +50,59 @@ class BackupLoad extends Workload
     @Override
     protected void doWork() throws Exception
     {
-        BackupResult backupResult = BackupHelper.backup( backupHostname, backupPort, backupDir );
-        if ( !backupResult.isConsistent() )
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try
         {
-            throw new RuntimeException( "Inconsistent backup" );
+            executeBackup( backupHostname, backupPort, backupDir, outputStream );
         }
-        if ( backupResult.isTransientErrorOnBackup() )
+        catch ( Exception e )
         {
-            TimeUnit.MILLISECONDS.sleep( 10 );
+            if ( isTransientError.test( e ) )
+            {
+                TimeUnit.MILLISECONDS.sleep( 100 );
+            }
+            else
+            {
+                flushToStandardOutput( outputStream );
+                throw e;
+            }
+        }
+        finally
+        {
+            outputStream.close();
+        }
+    }
+
+    private static void executeBackup( String host, int port, Path targetDir, OutputStream outputStream )
+            throws BackupExecutionException, ConsistencyCheckExecutionException
+    {
+        OnlineBackupContext context = OnlineBackupContext.builder()
+                .withHostnamePort( host, port )
+                .withBackupDirectory( targetDir )
+                .withReportsDirectory( targetDir )
+                .withFallbackToFullBackup( true )
+                .withConsistencyCheck( true )
+                .withConsistencyCheckPropertyOwners( true )
+                .build();
+
+        OnlineBackupExecutor executor = OnlineBackupExecutor.builder()
+                .withOutputStream( outputStream )
+                .withProgressMonitorFactory( ProgressMonitorFactory.textual( outputStream ) )
+                .withLogProvider( FormattedLogProvider.toOutputStream( outputStream ) )
+                .build();
+
+        executor.executeBackup( context );
+    }
+
+    private static void flushToStandardOutput( ByteArrayOutputStream outputStream )
+    {
+        try
+        {
+            outputStream.writeTo( System.out );
+        }
+        catch ( IOException e )
+        {
+            throw new UncheckedIOException( e );
         }
     }
 }
