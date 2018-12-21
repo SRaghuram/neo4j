8fbde76442eddafc32a3849e62a4e32e0b94c788
Pontus Melke <pontusmelke@gmail.com>
2018-12-21 23:07:11 +0100

    Port test to use junit5

--- private/enterprise/cypher/compatibility-spec-suite/src/test/scala/cypher/features/EnterpriseBaseTCKTests.scala
+++ private/enterprise/cypher/compatibility-spec-suite/src/test/scala/cypher/features/EnterpriseBaseTCKTests.scala
@@ -12,8 +12,8 @@ import org.opencypher.tools.tck.api.{CypherTCK, Scenario}
 abstract class EnterpriseBaseTCKTests extends BaseFeatureTest {
 
   // these two should be empty on commit!
-  val featureToRun = "VarLengthAcceptance2"
-  val scenarioToRun = "Handling relationships that are already bound in variable length paths"
+  val featureToRun = ""
+  val scenarioToRun = ""
 
   val scenarios: Seq[Scenario] = filterScenarios(CypherTCK.allTckScenarios, featureToRun, scenarioToRun)
 
