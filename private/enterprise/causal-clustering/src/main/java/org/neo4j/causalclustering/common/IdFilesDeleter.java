f76de03e72d0b79385a2b6e487ee623c8c887d37
Martin Furmanski <martin.furmanski@neotechnology.com>
2019-01-18 13:00:03 +0100

    Factor out ID-files deleter

---
+++ private/enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/common/IdFilesDeleter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package org.neo4j.causalclustering.common;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.io.layout.DatabaseLayout;
+
+public class IdFilesDeleter
+{
+    public static boolean deleteIdFiles( DatabaseLayout databaseLayout, FileSystemAbstraction fileSystem )
+    {
+        if ( !fileSystem.fileExists( databaseLayout.databaseDirectory() ) )
+        {
+            return false;
+        }
+
+        boolean anyIdFilesDeleted = false;
+        for ( File idFile : databaseLayout.idFiles() )
+        {
+            try
+            {
+                if ( fileSystem.fileExists( idFile ) )
+                {
+                    fileSystem.deleteFileOrThrow( idFile );
+                }
+                anyIdFilesDeleted = true;
+            }
+            catch ( IOException e )
+            {
+                throw new RuntimeException( "Could not delete ID-file", e );
+            }
+        }
+
+        return anyIdFilesDeleted;
+    }
+}
