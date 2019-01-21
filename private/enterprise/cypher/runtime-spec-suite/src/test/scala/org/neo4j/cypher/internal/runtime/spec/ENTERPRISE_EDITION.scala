9ee9f0bd03387c2823574c44f591ece8747d1013
fickludd <johan.teleman@neotechnology.com>
2019-01-21 11:40:10 +0100

    It's a new year...

--- private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/ENTERPRISE_EDITION.scala
+++ private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/ENTERPRISE_EDITION.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
 package org.neo4j.cypher.internal.runtime.spec
 
 import com.neo4j.test.TestEnterpriseGraphDatabaseFactory
