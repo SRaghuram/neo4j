076e351a48e8f5b4d1b9c473a8a1a4d59090fda2
Martin Furmanski <martin.furmanski@neotechnology.com>
2019-01-23 15:07:01 +0100

    Improve benign error detection in BackupHelper

--- private/stresstests/src/test/java/com/neo4j/causalclustering/stresstests/BackupHelper.java
+++ private/stresstests/src/test/java/com/neo4j/causalclustering/stresstests/BackupHelper.java
@@ -5,14 +5,20 @@
  */
 package com.neo4j.causalclustering.stresstests;
 
+import com.neo4j.causalclustering.catchup.storecopy.StoreCopyFailedException;
+import com.neo4j.causalclustering.catchup.storecopy.StoreIdDownloadFailedException;
+import com.neo4j.causalclustering.common.ClusterMember;
+
 import java.io.File;
+import java.io.IOException;
 import java.net.ConnectException;
 import java.nio.channels.ClosedChannelException;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.neo4j.causalclustering.common.ClusterMember;
+import org.neo4j.backup.impl.BackupExecutionException;
+import org.neo4j.backup.impl.ConsistencyCheckExecutionException;
 import org.neo4j.backup.impl.OnlineBackupContext;
 import org.neo4j.backup.impl.OnlineBackupExecutor;
 import org.neo4j.helpers.AdvertisedSocketAddress;
@@ -27,6 +33,11 @@ class BackupHelper
     private static final Set<Class<? extends Throwable>> BENIGN_EXCEPTIONS = asSet(
             ConnectException.class,
             ClosedChannelException.class
+    );
+
+    private static final Set<Class<? extends Throwable>> STORE_COPY_EXCEPTIONS = asSet(
+            StoreCopyFailedException.class,
+            StoreIdDownloadFailedException.class
     );
 
     AtomicLong backupNumber = new AtomicLong();
@@ -46,9 +57,10 @@ class BackupHelper
      *
      * @param member The member to perform the backup against.
      * @return The optional backup.
-     * @throws Exception If any unexpected exceptions happen.
+     * @throws BackupExecutionException if the backup fails for an unexpected reason during the backup phase.
+     * @throws ConsistencyCheckExecutionException if the backup fails during the consistency checking phase.
      */
-    Optional<File> backup( ClusterMember member ) throws Exception
+    Optional<File> backup( ClusterMember member ) throws BackupExecutionException, ConsistencyCheckExecutionException
     {
         AdvertisedSocketAddress address = member.config().get( transaction_advertised_address );
         String backupName = "backup-" + backupNumber.getAndIncrement();
@@ -68,6 +80,32 @@ class BackupHelper
             successfulBackups.incrementAndGet();
 
             return Optional.of( new File( baseBackupDir, backupName ) );
+        }
+        catch ( BackupExecutionException e )
+        {
+            // TODO: Fix backup error hierarchy so that regular benign transient errors can be clearly distinguished.
+
+            /* We don't know if these are benign or not, but we use some inside
+               knowledge of the implementation to make a reasonable guess. The basic
+               idea is to treat IOExceptions as hard failures, regardless of exception
+               wrapping, any other store-copy related exception as benign, and everything
+               else also as hard and unexpected failures. */
+
+            if ( findCauseOrSuppressed( e, t -> t.getClass().equals( IOException.class ) ).isPresent() )
+            {
+                throw e;
+            }
+
+            Optional<Throwable> benignException = findCauseOrSuppressed( e, t -> STORE_COPY_EXCEPTIONS.contains( t.getClass() ) );
+            if ( benignException.isPresent() )
+            {
+                // StoreCopyException which is not caused by IOException
+                log.info( "Benign failure: " + benignException.get().getMessage() );
+            }
+            else
+            {
+                throw e;
+            }
         }
         catch ( Exception e )
         {
