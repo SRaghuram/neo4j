f45dcf6e85d8d4f2b36d5afa1356c213269c7179
Jarosław Pałka <jaroslaw.palka@neotechnology.com>
2019-01-30 15:05:11 +0100

    added license headers, still struggling with jar licensing

--- private/benchmarks/benchmark-results-client/src/main/java/com/neo4j/bench/client/process/BaseProcess.java
+++ private/benchmarks/benchmark-results-client/src/main/java/com/neo4j/bench/client/process/BaseProcess.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
 package com.neo4j.bench.client.process;
 
 public interface BaseProcess
