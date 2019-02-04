26c7e7c5010bfcd35de9a6e9a95957d3ab4dd44f
Therese Magnusson <scout.therese@gmail.com>
2019-02-04 16:53:28 +0100

    Continued the work on using available methods
    Still only for direct subclasses and not indirect

--- private/enterprise/cypher/slotted-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/slotted/expressions/CompiledExpressionConverterTest.scala
+++ private/enterprise/cypher/slotted-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/slotted/expressions/CompiledExpressionConverterTest.scala
@@ -10,7 +10,7 @@ import org.neo4j.cypher.internal.physicalplanning.SlotAllocation.PhysicalPlan
 import org.neo4j.cypher.internal.planner.v4_0.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.v4_0.ast.AstConstructionTestSupport
-import org.neo4j.cypher.internal.v4_0.expressions.{Add, StringLiteral}
+import org.neo4j.cypher.internal.v4_0.expressions.Add
 import org.neo4j.cypher.internal.v4_0.util.attribution.Id
 import org.neo4j.cypher.internal.v4_0.util.test_helpers.CypherFunSuite
 import org.neo4j.logging.BufferingLog
@@ -25,7 +25,7 @@ class CompiledExpressionConverterTest extends CypherFunSuite with AstConstructio
     // When
     //There is a limit of 65535 on the length of a String literal, so by exceeding that limit
     //we trigger a compilation error
-    val e = Add(StringLiteral("*" * (65535 + 1))(pos), StringLiteral("*")(pos))(pos)
+    val e = Add(literalString("*" * (65535 + 1)), literalString("*"))(pos)
 
     // Then
     converter.toCommandExpression(Id.INVALID_ID, e, mock[ExpressionConverters]) should equal(None)
