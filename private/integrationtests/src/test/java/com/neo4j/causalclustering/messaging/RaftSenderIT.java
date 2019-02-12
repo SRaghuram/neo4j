d556136681f68ddc079b223ab2d3c1b048bb0b15
RagnarW <ragnar.wernersson@neotechnology.com>
2019-02-12 14:28:31 +0100

    Migrate RaftSenderIT to Junit5
    Also ensure it actually runs on all protocols

--- private/integrationtests/src/test/java/com/neo4j/causalclustering/messaging/RaftSenderIT.java
+++ private/integrationtests/src/test/java/com/neo4j/causalclustering/messaging/RaftSenderIT.java
@@ -31,39 +31,34 @@ import com.neo4j.causalclustering.protocol.handshake.ModifierSupportedProtocols;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.neo4j.helpers.AdvertisedSocketAddress;
 import org.neo4j.helpers.ListenSocketAddress;
 import org.neo4j.kernel.configuration.Config;
 import org.neo4j.logging.LogProvider;
 import org.neo4j.logging.NullLogProvider;
-import org.neo4j.test.rule.CleanupRule;
 import org.neo4j.test.scheduler.ThreadPoolJobScheduler;
 
 import static com.neo4j.causalclustering.handlers.VoidPipelineWrapperFactory.VOID_WRAPPER;
 import static com.neo4j.causalclustering.messaging.RaftChannelPoolService.raftChannelPoolService;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.graphdb.factory.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 import static org.neo4j.helpers.collection.Iterators.asSet;
 
-@RunWith( Parameterized.class )
-public class RaftSenderIT
+class RaftSenderIT
 {
     private final LogProvider logProvider = NullLogProvider.getInstance();
 
@@ -76,28 +71,26 @@ public class RaftSenderIT
     private final ModifierProtocolRepository modifierProtocolRepository =
             new ModifierProtocolRepository( ModifierProtocols.values(), supportedModifierProtocols );
 
-    @Rule
-    public CleanupRule cleanupRule = new CleanupRule();
-
-    @Parameterized.Parameter
-    public boolean blocking;
-
-    @Parameterized.Parameter( 1 )
-    public ApplicationProtocols clientProtocol;
-
-    @Parameterized.Parameters( name = "blocking={0} protocol={1}" )
-    public static Iterable<Object[]> params()
-    {
-        return clientRepositories().stream().flatMap( r -> Stream.of( new Object[]{true, r}, new Object[]{false, r} ) ).collect( Collectors.toList() );
-    }
-
     private static Collection<ApplicationProtocols> clientRepositories()
     {
         return Arrays.asList( ApplicationProtocols.RAFT_1, ApplicationProtocols.RAFT_2 );
     }
 
-    @Test
-    public void shouldSendAndReceive() throws Throwable
+    @ParameterizedTest
+    @EnumSource( value = ApplicationProtocols.class, mode = EnumSource.Mode.MATCH_ALL, names = "^RAFT_\\d" )
+    void shouldSendAndReceiveBlocking( ApplicationProtocols clientProtocol ) throws Throwable
+    {
+        shouldSendAndReceive( clientProtocol, true );
+    }
+
+    @ParameterizedTest
+    @EnumSource( value = ApplicationProtocols.class, mode = EnumSource.Mode.MATCH_ALL, names = "^RAFT_\\d" )
+    void shouldSendAndReceiveNoneBlocking( ApplicationProtocols clientProtocol ) throws Throwable
+    {
+        shouldSendAndReceive( clientProtocol, false );
+    }
+
+    private void shouldSendAndReceive( ApplicationProtocols clientProtocol, boolean blocking ) throws Throwable
     {
         // given: raft server handler
         Semaphore messageReceived = new Semaphore( 0 );
@@ -113,7 +106,7 @@ public class RaftSenderIT
         raftServer.start();
 
         // given: raft messaging service
-        RaftChannelPoolService raftPooLService = raftPooLService();
+        RaftChannelPoolService raftPooLService = raftPooLService( clientProtocol );
         raftPooLService.start();
 
         RaftSender sender = new RaftSender( logProvider, raftPooLService );
@@ -151,22 +144,32 @@ public class RaftSenderIT
                 installer, pipelineFactory, logProvider );
 
         ListenSocketAddress listenAddress = new ListenSocketAddress( "localhost", 0 );
-        ExecutorService serverExecutor = cleanupRule.add( Executors.newCachedThreadPool() );
+        ExecutorService serverExecutor = Executors.newCachedThreadPool();
         return new Server( channelInitializer, null, logProvider, logProvider, listenAddress, "raft-server", serverExecutor,
                 BootstrapConfiguration.serverConfig( Config.defaults() ) );
     }
 
-    private RaftChannelPoolService raftPooLService()
+    private RaftChannelPoolService raftPooLService( ApplicationProtocols clientProtocol )
     {
         NettyPipelineBuilderFactory pipelineFactory = new NettyPipelineBuilderFactory( VOID_WRAPPER );
 
-        RaftProtocolClientInstallerV1.Factory factoryV1 = new RaftProtocolClientInstallerV1.Factory( pipelineFactory, logProvider );
-        RaftProtocolClientInstallerV2.Factory factoryV2 =
-                new RaftProtocolClientInstallerV2.Factory( pipelineFactory, logProvider );
-        ProtocolInstallerRepository<ProtocolInstaller.Orientation.Client> protocolInstaller =
-                new ProtocolInstallerRepository<>( Arrays.asList( factoryV1, factoryV2 ), ModifierProtocolInstaller.allClientInstallers );
+        ProtocolInstallerRepository<ProtocolInstaller.Orientation.Client> protocolInstaller = null;
+        if ( clientProtocol == ApplicationProtocols.RAFT_1 )
+        {
+            RaftProtocolClientInstallerV1.Factory factoryV1 = new RaftProtocolClientInstallerV1.Factory( pipelineFactory, logProvider );
+            protocolInstaller = new ProtocolInstallerRepository<>( Collections.singleton( factoryV1 ), ModifierProtocolInstaller.allClientInstallers );
+        }
+        else if ( clientProtocol == ApplicationProtocols.RAFT_2 )
+        {
+            RaftProtocolClientInstallerV2.Factory factoryV2 = new RaftProtocolClientInstallerV2.Factory( pipelineFactory, logProvider );
+            protocolInstaller = new ProtocolInstallerRepository<>( Collections.singleton( factoryV2 ), ModifierProtocolInstaller.allClientInstallers );
+        }
+        else
+        {
+            throw new IllegalArgumentException( "Unexpected protocol " + clientProtocol );
+        }
 
-        HandshakeClientInitializer channelInitializer = new HandshakeClientInitializer( clientRepository(),
+        HandshakeClientInitializer channelInitializer = new HandshakeClientInitializer( clientRepository( clientProtocol ),
                 modifierProtocolRepository,
                 protocolInstaller,
                 pipelineFactory,
@@ -178,9 +181,9 @@ public class RaftSenderIT
                         logProvider, channelInitializer );
     }
 
-    private ApplicationProtocolRepository clientRepository()
+    private ApplicationProtocolRepository clientRepository( ApplicationProtocols clientProtocol )
     {
-        return new ApplicationProtocolRepository( new ApplicationProtocols[]{ApplicationProtocols.RAFT_2},
+        return new ApplicationProtocolRepository( new ApplicationProtocols[]{clientProtocol},
                 new ApplicationSupportedProtocols( Protocol.ApplicationProtocolCategory.RAFT, emptyList() ) );
     }
 }
