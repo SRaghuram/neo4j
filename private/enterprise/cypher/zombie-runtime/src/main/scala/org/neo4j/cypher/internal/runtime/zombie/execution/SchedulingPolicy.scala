c0df94b2e7b00d8ada8f57f8d7cf826cf96cc997
fickludd <johan.teleman@neotechnology.com>
2019-02-21 09:57:15 +0100

    Make alex gut over-joyed

--- private/enterprise/cypher/zombie-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/zombie/execution/SchedulingPolicy.scala
+++ private/enterprise/cypher/zombie-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/zombie/execution/SchedulingPolicy.scala
@@ -6,7 +6,7 @@
 package org.neo4j.cypher.internal.runtime.zombie.execution
 
 import org.neo4j.cypher.internal.runtime.morsel.QueryResources
-import org.neo4j.cypher.internal.runtime.zombie.PipelineTask
+import org.neo4j.cypher.internal.runtime.zombie.{ExecutablePipeline, PipelineTask}
 
 /**
   * Policy which selects the next task for execute for a given executing query.
@@ -16,26 +16,40 @@ trait SchedulingPolicy {
                queryResources: QueryResources): PipelineTask
 }
 
+object TaskMaker {
+  def taskOrNull(pipeline: ExecutablePipeline,
+                 executingQuery: ExecutingQuery,
+                 queryResources: QueryResources): PipelineTask = {
+
+    val state = executingQuery.executionState
+
+    val task = state.continue(pipeline)
+    if (task != null) {
+      return task
+    }
+
+    val input = state.consumeMorsel(pipeline.inputRowBuffer.id, pipeline)
+    if (input != null) {
+      val pipelineState = state.pipelineState(pipeline.id)
+      val tasks = pipelineState.init(input, executingQuery.queryContext, executingQuery.queryState, queryResources)
+      for (task <- tasks.tail)
+        state.addContinuation(task)
+      return tasks.head
+    }
+
+    null
+  }
+}
+
 object LazyScheduling extends SchedulingPolicy {
 
   def nextTask(executingQuery: ExecutingQuery,
                queryResources: QueryResources): PipelineTask = {
 
-    val state = executingQuery.executionState
-
     for (p <- executingQuery.executablePipelines.reverseIterator) {
-      val task = state.continue(p)
+      val task = TaskMaker.taskOrNull(p, executingQuery, queryResources)
       if (task != null) {
         return task
-      }
-
-      val input = state.consumeMorsel(p.inputRowBuffer.id, p)
-      if (input != null) {
-        val pipelineState = state.pipelineState(p.id)
-        val tasks = pipelineState.init(input, executingQuery.queryContext, executingQuery.queryState, queryResources)
-        for (task <- tasks.tail)
-          state.addContinuation(task)
-        return tasks.head
       }
     }
     null
