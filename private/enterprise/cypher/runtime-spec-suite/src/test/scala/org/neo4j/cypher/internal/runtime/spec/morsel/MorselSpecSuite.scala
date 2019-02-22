f770a21534c5680048ffe922d32d968787523b63
Satia Herfert <satiaherfert@gmx.de>
2019-02-22 11:49:10 +0100

    Cartesian stress tests

--- private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/morsel/MorselSpecSuite.scala
+++ private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/morsel/MorselSpecSuite.scala
@@ -398,3 +398,59 @@ class MorselApplyStressTest extends ParallelStressSuite {
     runtimeResult should beColumns("a", "b", "c").withRows(expected)
   }
 }
+
+// CARTESIAN PRODUCT
+
+class MorselCartesianProductStressTest extends ParallelStressSuite {
+
+  test("should support nested Cartesian Product") {
+    // given
+    init()
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("a", "b", "c")
+      .cartesianProduct()
+      .|.cartesianProduct()
+      .|.|.nodeIndexOperator("c:Label(prop <= 10)")
+      .|.nodeIndexOperator("b:Label(prop <= 20)")
+      .nodeIndexOperator("a:Label(prop <= 40)")
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    val expected = for {
+      a <- nodes if a.getId <= 40
+      b <- nodes if b.getId <= 20
+      c <- nodes if c.getId <= 10
+    } yield Array(a, b, c)
+    runtimeResult should beColumns("a", "b", "c").withRows(expected)
+  }
+
+  // FIXME broken
+//  test("should support Cartesian Product on RHS of apply") {
+//    // given
+//    init()
+//
+//    // when
+//    val logicalQuery = new LogicalQueryBuilder(this)
+//      .produceResults("a", "b", "c")
+//      .apply()
+//      .|.cartesianProduct()
+//      .|.|.nodeIndexOperator("c:Label(prop > ???)", paramExpr = Some(prop("a", "prop")), argumentIds = Set("a"))
+//      .|.nodeIndexOperator("b:Label(prop < ???)", paramExpr = Some(prop("a", "prop")), argumentIds = Set("a"))
+//      .nodeIndexOperator("a:Label(prop <= 40)")
+//      .build()
+//
+//    val runtimeResult = execute(logicalQuery, runtime)
+//
+//    // then
+//    val expected = for {
+//      a <- nodes if a.getId <= 40
+//      b <- nodes if b.getId < a.getId
+//      c <- nodes if c.getId > a.getId
+//    } yield Array(a, b, c)
+//    runtimeResult should beColumns("a", "b", "c").withRows(expected)
+//  }
+}
