605034f4eb17454749029029b3463d67eedbed7c
Satia Herfert <satiaherfert@gmx.de>
2019-02-22 11:32:44 +0100

    Apply stress test

--- private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/morsel/MorselSpecSuite.scala
+++ private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/morsel/MorselSpecSuite.scala
@@ -368,3 +368,33 @@ class MorselInputTest extends InputTestBase(ENTERPRISE_PARALLEL, MorselRuntime, 
     result should beColumns("x").withRows(input.flatten)
   }
 }
+
+// APPLY
+
+class MorselApplyStressTest extends ParallelStressSuite {
+
+  test("should support nested Apply") {
+    // given
+    init()
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("a", "b", "c")
+      .apply()
+      .|.apply()
+      .|.|.nodeIndexOperator("c:Label(prop < ???)", paramExpr = Some(prop("b", "prop")), argumentIds = Set("a", "b"))
+      .|.nodeIndexOperator("b:Label(prop < ???)", paramExpr = Some(prop("a", "prop")), argumentIds = Set("a"))
+      .nodeIndexOperator("a:Label(prop <= 40)")
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    val expected = for {
+      a <- nodes if a.getId <= 40
+      b <- nodes if b.getId < a.getId
+      c <- nodes if c.getId < b.getId
+    } yield Array(a, b, c)
+    runtimeResult should beColumns("a", "b", "c").withRows(expected)
+  }
+}
