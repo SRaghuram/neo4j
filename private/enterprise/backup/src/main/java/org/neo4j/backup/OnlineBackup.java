546a57ef85862d13ab72367ccff0a216cbcd73dd
lutovich <konstantin.lutovich@neo4j.com>
2019-02-26 12:52:06 +0100

    Re-introduce OnlineBackup API
    This API is used to take backups from a client application. It
    existed in the previous branch and relied on HA/com backup
    functionality. It was later removed together with HA.
    
    New version of the API uses catchup protocol, same as the backup
    command line tool.
    
    Changes in OnlineBackup compared to the previous version:
    
     * removed deprecated methods for performing full/incremental backup
    
     * added fluent interface methods for configuring backup settings
       and dependencies (like output stream) instead of method parameters
    
     * keep a single method to execute the actual backup as
       a terminal operation
    
     * return a Result object from OnlineBackup#backup() method instead
       of modifying the OnlineBackup instance with the consistency check
       status of the last backup
    
     * added null-checks for method parameters

---
+++ private/enterprise/backup/src/main/java/org/neo4j/backup/OnlineBackup.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package org.neo4j.backup;
+
+import com.neo4j.kernel.impl.enterprise.configuration.OnlineBackupSettings;
+
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import org.neo4j.backup.impl.BackupExecutionException;
+import org.neo4j.backup.impl.ConsistencyCheckExecutionException;
+import org.neo4j.backup.impl.OnlineBackupContext;
+import org.neo4j.backup.impl.OnlineBackupExecutor;
+import org.neo4j.configuration.Config;
+import org.neo4j.graphdb.config.Setting;
+import org.neo4j.helpers.progress.ProgressMonitorFactory;
+import org.neo4j.logging.FormattedLogProvider;
+import org.neo4j.logging.LogProvider;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * This class encapsulates the information needed to perform an online backup against a running Neo4j instance
+ * configured to act as a backup server. This class is not instantiable, instead factory methods are used to get
+ * instances configured to contact a specific backup server against which all possible backup operations can be
+ * performed.
+ * <p>
+ * All backup methods return the same instance, allowing for chaining calls.
+ *
+ * @see OnlineBackup#from(String)
+ * @see OnlineBackup#from(String, int)
+ */
+public class OnlineBackup
+{
+    private final String hostnameOrIp;
+    private final int port;
+    private final Config config;
+
+    private boolean fallbackToFullBackup = true;
+    private boolean consistencyCheck = true;
+    private OutputStream outputStream = System.out;
+
+    private OnlineBackup( String hostnameOrIp, int port )
+    {
+        this.hostnameOrIp = hostnameOrIp;
+        this.port = port;
+        this.config = Config.defaults();
+    }
+
+    /**
+     * Factory method for this class. The returned instance will perform backup operations against the
+     * hostname passed in as parameter, using the default backup port {@value OnlineBackupSettings#DEFAULT_BACKUP_PORT}.
+     *
+     * @param hostnameOrIp hostname or IP address of the backup server.
+     * @return an {@code OnlineBackup} instance ready to perform backup operations from the given remote server.
+     */
+    public static OnlineBackup from( String hostnameOrIp )
+    {
+        return from( hostnameOrIp, OnlineBackupSettings.DEFAULT_BACKUP_PORT );
+    }
+
+    /**
+     * Factory method for this class. The returned instance will perform backup operations against the
+     * hostname and port passed in as parameters.
+     *
+     * @param hostnameOrIp hostname or IP address of the backup server.
+     * @param port port at which the remote backup server is listening.
+     * @return an {@code OnlineBackup} instance ready to perform backup operations from the given remote server.
+     */
+    public static OnlineBackup from( String hostnameOrIp, int port )
+    {
+        return new OnlineBackup( requireNonNull( hostnameOrIp, "hostnameOrIp" ), port );
+    }
+
+    /**
+     * Allow fallback to a full backup if an incremental backup fails.
+     * This configuration value is only applied when the target directory already contains a backup.
+     * <p>
+     * Default value is {@code true}.
+     *
+     * @param fallbackToFullBackup {@code true} if fallback to a full backup is allowed, {@code false} otherwise.
+     * @return this {@code OnlineBackup} instance.
+     */
+    public OnlineBackup withFallbackToFullBackup( boolean fallbackToFullBackup )
+    {
+        this.fallbackToFullBackup = fallbackToFullBackup;
+        return this;
+    }
+
+    /**
+     * Allow consistency check after a successful backup.
+     * <p>
+     * Status of the consistency check will be available via {@link Result#isConsistent()}.
+     * Consistency check report will be written to the target directory.
+     * <p>
+     * Default value is {@code true}.
+     *
+     * @param consistencyCheck {@code true} if a successful backup should be checked for consistency, {@code false} otherwise.
+     * @return this {@code OnlineBackup} instance.
+     */
+    public OnlineBackup withConsistencyCheck( boolean consistencyCheck )
+    {
+        this.consistencyCheck = consistencyCheck;
+        return this;
+    }
+
+    /**
+     * Specify an {@link OutputStream output stream} for the backup to use for printing out status messages and progress information.
+     * <p>
+     * Default value is {@link System#out}.
+     *
+     * @param outputStream the output stream to use.
+     * @return this {@code OnlineBackup} instance.
+     * @throws NullPointerException if the parameter is {@code null};
+     */
+    public OnlineBackup withOutputStream( OutputStream outputStream )
+    {
+        this.outputStream = requireNonNull( outputStream, "outputStream" );
+        return this;
+    }
+
+    /**
+     * Override a default configuration value for the backup.
+     * <p>
+     * {@link Setting#getDefaultValue()} is used for every setting by default.
+     *
+     * @param setting the setting key to override.
+     * @param value new value as string.
+     * @return this {@code OnlineBackup} instance.
+     * @throws NullPointerException if the given {@link Setting} is {@code null}.
+     */
+    public OnlineBackup withSetting( Setting<?> setting, String value )
+    {
+        config.augment( requireNonNull( setting, "setting" ), value );
+        return this;
+    }
+
+    /**
+     * Performs a backup of the specified database into the into target directory. The server contacted is the one
+     * configured in the factory method used to obtain this instance.
+     * <p>
+     * After the backup is complete, a consistency check phase will take place, checking the database for consistency.
+     * If any errors are found, {@link Result#isConsistent()} will return {@code false} and the consistency check report will
+     * be written to the target directory. Consistency check can be turned on/off using {@link #withConsistencyCheck(boolean)}.
+     * <p>
+     * If the target directory does not contain a database, a full backup will be performed, otherwise an incremental
+     * backup mechanism is used.
+     * <p>
+     * If the backup has become too far out of date for an incremental backup to succeed, a full backup is performed.
+     *
+     * @param databaseName name of the database to backup.
+     * @param targetDirectory directory where the backup should be placed. Can contain a previous full backup.
+     * @return the same {@code OnlineBackup} instance, possible to use for a new backup operation.
+     */
+    public Result backup( String databaseName, Path targetDirectory )
+    {
+        requireNonNull( databaseName, "databaseName" );
+        requireNonNull( targetDirectory, "targetDirectory" );
+
+        LogProvider logProvider = FormattedLogProvider.toOutputStream( outputStream );
+        try
+        {
+            executeBackup( databaseName, targetDirectory, logProvider );
+            return new Result( consistencyCheck );
+        }
+        catch ( BackupExecutionException e )
+        {
+            throw new RuntimeException( "Backup failed", e );
+        }
+        catch ( ConsistencyCheckExecutionException e )
+        {
+            logProvider.getLog( getClass() ).warn( "Consistency check failed", e );
+            return new Result( false );
+        }
+    }
+
+    private void executeBackup( String databaseName, Path targetDirectory, LogProvider logProvider )
+            throws BackupExecutionException, ConsistencyCheckExecutionException
+    {
+        OnlineBackupExecutor backupExecutor = OnlineBackupExecutor.builder()
+                .withUserLogProvider( logProvider )
+                .withProgressMonitorFactory( ProgressMonitorFactory.textual( outputStream ) )
+                .build();
+
+        OnlineBackupContext context = OnlineBackupContext.builder()
+                .withAddress( hostnameOrIp, port )
+                .withConfig( config )
+                .withConsistencyCheck( consistencyCheck )
+                .withFallbackToFullBackup( fallbackToFullBackup )
+                .withBackupDirectory( targetDirectory )
+                .withReportsDirectory( targetDirectory )
+                .withDatabaseName( databaseName )
+                .build();
+
+        backupExecutor.executeBackup( context );
+    }
+
+    /**
+     * Represents an outcome of a successful backup.
+     */
+    public static class Result
+    {
+        private final boolean consistent;
+
+        private Result( boolean consistent )
+        {
+            this.consistent = consistent;
+        }
+
+        /**
+         * Tests whether the executed backup passed a consistency check.
+         * <p>
+         * Consistency check can be switched on/off using {@link OnlineBackup#withConsistencyCheck(boolean)}.
+         *
+         * @return {@code true} if a consistency check was performed and was successful, {@code false} otherwise.
+         */
+        public boolean isConsistent()
+        {
+            return consistent;
+        }
+    }
+}
---
+++ private/enterprise/commercial-it/causalclustering-it/src/test/java/org/neo4j/backup/OnlineBackupIT.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package org.neo4j.backup;
+
+import com.neo4j.causalclustering.catchup.storecopy.StoreIdDownloadFailedException;
+import com.neo4j.test.TestCommercialGraphDatabaseFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.neo4j.backup.impl.BackupExecutionException;
+import org.neo4j.configuration.connectors.ConnectorPortRegister;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.HostnamePort;
+import org.neo4j.internal.recordstorage.RecordStorageEngine;
+import org.neo4j.io.layout.DatabaseLayout;
+import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.kernel.impl.store.MetaDataStore;
+import org.neo4j.kernel.impl.store.NodeStore;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.test.DbRepresentation;
+import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.RandomExtension;
+import org.neo4j.test.extension.SuppressOutputExtension;
+import org.neo4j.test.extension.TestDirectoryExtension;
+import org.neo4j.test.rule.RandomRule;
+import org.neo4j.test.rule.TestDirectory;
+
+import static com.neo4j.causalclustering.core.TransactionBackupServiceProvider.BACKUP_SERVER_NAME;
+import static com.neo4j.kernel.impl.enterprise.configuration.OnlineBackupSettings.online_backup_enabled;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+import static org.neo4j.configuration.Settings.TRUE;
+import static org.neo4j.graphdb.Label.label;
+import static org.neo4j.kernel.impl.store.MetaDataStore.Position.RANDOM_NUMBER;
+import static org.neo4j.kernel.impl.store.MetaDataStore.Position.TIME;
+
+@ExtendWith( {TestDirectoryExtension.class, RandomExtension.class, SuppressOutputExtension.class} )
+class OnlineBackupIT
+{
+    private static final String DB_NAME = DEFAULT_DATABASE_NAME;
+
+    @Inject
+    private TestDirectory testDirectory;
+    @Inject
+    private RandomRule random;
+
+    private Path backupsDir;
+    private Path defaultDbBackupDir;
+    private GraphDatabaseAPI db;
+    private HostnamePort backupAddress;
+
+    @BeforeEach
+    void setUp()
+    {
+        backupsDir = testDirectory.directory( "backups" ).toPath();
+        defaultDbBackupDir = backupsDir.resolve( DB_NAME );
+
+        db = (GraphDatabaseAPI) new TestCommercialGraphDatabaseFactory()
+                .newEmbeddedDatabaseBuilder( testDirectory.databaseDir() )
+                .setConfig( online_backup_enabled, TRUE )
+                .newGraphDatabase();
+
+        backupAddress = db.getDependencyResolver().resolveDependency( ConnectorPortRegister.class ).getLocalAddress( BACKUP_SERVER_NAME );
+
+        writeRandomData();
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        if ( db != null )
+        {
+            db.shutdown();
+        }
+    }
+
+    @Test
+    void shouldPerformFullBackup()
+    {
+        OnlineBackup.Result result = executeBackupWithFallbackToFull();
+
+        assertTrue( result.isConsistent() );
+
+        assertEquals( DbRepresentation.of( db ), backupDbRepresentation() );
+    }
+
+    @Test
+    void shouldPerformIncrementalBackup()
+    {
+        OnlineBackup.Result result1 = executeBackupWithFallbackToFull();
+        assertTrue( result1.isConsistent() );
+
+        writeRandomData();
+
+        OnlineBackup.Result result2 = executeBackupWithoutFallbackToFull();
+        assertTrue( result2.isConsistent() );
+
+        assertEquals( DbRepresentation.of( db ), backupDbRepresentation() );
+    }
+
+    @Test
+    void shouldPerformConsistencyCheckOnInconsistentDatabase()
+    {
+        corruptNodeStore();
+
+        OnlineBackup.Result result = executeBackupWithFallbackToFull();
+
+        assertFalse( result.isConsistent() );
+    }
+
+    @Test
+    void shouldThrowWhenTargetDatabaseDoesNotExist()
+    {
+        RuntimeException error = assertThrows( RuntimeException.class, () ->
+                OnlineBackup.from( backupAddress.getHost(), backupAddress.getPort() )
+                        .backup( "unknown.db", backupsDir ) );
+
+        assertThat( error.getCause(), instanceOf( BackupExecutionException.class ) );
+    }
+
+    @Test
+    void shouldThrowWhenTargetDirectoryDoesNotExist()
+    {
+        RuntimeException error = assertThrows( RuntimeException.class, () ->
+                OnlineBackup.from( backupAddress.getHost(), backupAddress.getPort() )
+                        .backup( DB_NAME, backupsDir.resolve( "unknownDir" ) ) );
+
+        assertThat( error.getCause(), instanceOf( BackupExecutionException.class ) );
+    }
+
+    @Test
+    void shouldThrowWhenIncrementalBackupNotPossibleAndFallbackToFullNotAllowed() throws Exception
+    {
+        OnlineBackup.Result result1 = executeBackupWithFallbackToFull();
+        assertTrue( result1.isConsistent() );
+
+        corruptStoreIdInBackup(); // change store ID of the backup so that it looks like a different database
+
+        RuntimeException error = assertThrows( RuntimeException.class, this::executeBackupWithoutFallbackToFull );
+        assertThat( getRootCause( error ), instanceOf( StoreIdDownloadFailedException.class ) );
+    }
+
+    private void corruptNodeStore()
+    {
+        RecordStorageEngine storageEngine = db.getDependencyResolver().resolveDependency( RecordStorageEngine.class );
+        NodeStore nodeStore = storageEngine.testAccessNeoStores().getNodeStore();
+        nodeStore.scanAllRecords( record ->
+        {
+            record.setInUse( false );
+            nodeStore.updateRecord( record );
+            return false;
+        } );
+    }
+
+    private void corruptStoreIdInBackup() throws IOException
+    {
+        Path backupDir = backupsDir.resolve( DB_NAME );
+        PageCache pageCache = db.getDependencyResolver().resolveDependency( PageCache.class );
+        File metadataStore = DatabaseLayout.of( backupDir.toFile() ).metadataStore();
+
+        // update store creation time and store random number
+        MetaDataStore.setRecord( pageCache, metadataStore, TIME, random.nextInt() );
+        MetaDataStore.setRecord( pageCache, metadataStore, RANDOM_NUMBER, random.nextInt() );
+    }
+
+    private OnlineBackup.Result executeBackupWithFallbackToFull()
+    {
+        return OnlineBackup.from( backupAddress.getHost(), backupAddress.getPort() )
+                .withFallbackToFullBackup( true )
+                .withConsistencyCheck( true )
+                .withOutputStream( System.out )
+                .backup( DB_NAME, backupsDir );
+    }
+
+    private OnlineBackup.Result executeBackupWithoutFallbackToFull()
+    {
+        return OnlineBackup.from( backupAddress.getHost(), backupAddress.getPort() )
+                .withFallbackToFullBackup( false )
+                .withConsistencyCheck( true )
+                .withOutputStream( System.out )
+                .backup( DB_NAME, backupsDir );
+    }
+
+    private HostnamePort backupAddress()
+    {
+        ConnectorPortRegister portRegister = db.getDependencyResolver().resolveDependency( ConnectorPortRegister.class );
+        HostnamePort backupAddress = portRegister.getLocalAddress( BACKUP_SERVER_NAME );
+        assertNotNull( backupAddress );
+        return backupAddress;
+    }
+
+    private DbRepresentation backupDbRepresentation()
+    {
+        return DbRepresentation.of( defaultDbBackupDir.toFile() );
+    }
+
+    private void writeRandomData()
+    {
+        Label label = label( random.nextAsciiString() );
+        String property = random.nextAsciiString();
+        RelationshipType relType = RelationshipType.withName( random.nextAsciiString() );
+
+        try ( Transaction tx = db.beginTx() )
+        {
+            db.schema().indexFor( label ).on( property ).create();
+            tx.success();
+        }
+        try ( Transaction tx = db.beginTx() )
+        {
+            db.schema().awaitIndexesOnline( 1, MINUTES );
+            tx.success();
+        }
+
+        int transactions = random.nextInt( 5, 20 );
+        int nodesInTransaction = random.nextInt( 10, 50 );
+
+        for ( int i = 0; i < transactions; i++ )
+        {
+            try ( Transaction tx = db.beginTx() )
+            {
+                Node previousNode = null;
+                for ( int j = 0; j < nodesInTransaction; j++ )
+                {
+                    Node node = db.createNode( label );
+                    node.setProperty( property, random.nextString() );
+                    if ( previousNode == null )
+                    {
+                        previousNode = node;
+                    }
+                    previousNode.createRelationshipTo( node, relType );
+                    previousNode = node;
+                }
+                tx.success();
+            }
+        }
+    }
+}
