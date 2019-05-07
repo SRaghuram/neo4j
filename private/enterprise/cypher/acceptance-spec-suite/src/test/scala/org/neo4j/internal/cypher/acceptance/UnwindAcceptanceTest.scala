e798b49b77f4519dcecc2706c2db672fa1947e1d
Louise Söderström <louise.matematik@gmail.com>
2019-05-07 16:04:48 +0200

    Unwind of scalar value.
    This should be supported and give the same result as for a list of
    length one containing the scalar value.

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnwindAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnwindAcceptanceTest.scala
@@ -5,10 +5,48 @@
  */
 package org.neo4j.internal.cypher.acceptance
 
+import java.time.LocalDate
+
 import org.neo4j.cypher.ExecutionEngineFunSuite
 import org.neo4j.internal.cypher.acceptance.comparisonsupport.{Configs, CypherComparisonSupport}
 
 class UnwindAcceptanceTest extends ExecutionEngineFunSuite with CypherComparisonSupport {
+
+  test("should unwind scalar integer") {
+    val query = "UNWIND 7 AS x RETURN x"
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+
+    result.toList should equal(List(Map("x" -> 7)))
+  }
+
+  test("should unwind scalar string") {
+    val query = "UNWIND 'this string' AS x RETURN x"
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+
+    result.toList should equal(List(Map("x" -> "this string")))
+  }
+
+  test("should unwind scalar boolean") {
+    val query = "UNWIND false AS x RETURN x"
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+
+    result.toList should equal(List(Map("x" -> false)))
+  }
+
+  test("should unwind scalar temporal") {
+    val query = "UNWIND date('2019-05-07') AS x RETURN x"
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+
+    result.toList should equal(List(Map("x" -> LocalDate.of(2019, 5, 7))))
+  }
+
+  test("should unwind parameterized scalar value") {
+    val query = "UNWIND $value AS x RETURN x"
+    val result = executeWith(Configs.All, query, params = Map("value" -> 42))
+
+    result.toList should equal(List(Map("x" -> 42)))
+
+  }
 
   test("should unwind nodes") {
     val n = createNode("prop" -> 42)
@@ -26,6 +64,22 @@ class UnwindAcceptanceTest extends ExecutionEngineFunSuite with CypherComparison
     val result = executeWith(Configs.All, query, params = Map("node" -> n))
 
     result.toList should equal(List(Map("n" -> n)))
+  }
+
+  test("should unwind scalar node") {
+
+    val n = createNode("prop" -> 42)
+
+    val query =
+      """
+        | MATCH (n {prop: 42})
+        | WITH n
+        | UNWIND n AS x
+        | RETURN x
+      """.stripMargin
+
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+    result.toList should equal(List(Map("x" -> n)))
   }
 
   test("should unwind relationships") {
@@ -48,5 +102,22 @@ class UnwindAcceptanceTest extends ExecutionEngineFunSuite with CypherComparison
     val result = executeWith(Configs.All, query, params = Map("relationship" -> r))
 
     result.toList should equal(List(Map("r" -> r)))
+  }
+
+  test("should unwind scalar relationship") {
+    val a = createNode()
+    val b = createNode()
+    val r = relate(a, b)
+
+    val query =
+      """
+        | MATCH ()-[r]->()
+        | WITH r
+        | UNWIND r AS x
+        | RETURN x
+      """.stripMargin
+
+    val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, query)
+    result.toList should equal(List(Map("x" -> r)))
   }
 }
