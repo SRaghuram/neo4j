22aa6904b36c7d21c8a25186df6bb343266820d0
Pontus Melke <pontusmelke@gmail.com>
2019-05-08 13:07:27 +0200

    test mocking

--- private/enterprise/cypher/zombie-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/zombie/state/QueryCompletionTrackerTest.scala
+++ private/enterprise/cypher/zombie-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/zombie/state/QueryCompletionTrackerTest.scala
@@ -5,6 +5,7 @@
  */
 package org.neo4j.cypher.internal.runtime.zombie.state
 
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
 import org.neo4j.cypher.internal.runtime.QueryContext
 import org.neo4j.cypher.internal.runtime.morsel.{ConcurrentDemandControlSubscription, StandardDemandControlSubscription}
 import org.neo4j.cypher.internal.v4_0.util.test_helpers.CypherFunSuite
@@ -13,13 +14,13 @@ import org.neo4j.kernel.impl.query.QuerySubscriber
 class StandardQueryCompletionTrackerTest extends QueryCompletionTrackerTest {
   override def newTracker(): QueryCompletionTracker = new StandardQueryCompletionTracker(mock[QuerySubscriber],
                                                                                          new StandardDemandControlSubscription,
-                                                                                         mock[QueryContext])
+                                                                                         mock[QueryContext](RETURNS_DEEP_STUBS))
 }
 
 class ConcurrentQueryCompletionTrackerTest extends QueryCompletionTrackerTest {
   override def newTracker(): QueryCompletionTracker = new StandardQueryCompletionTracker(mock[QuerySubscriber],
                                                                                          new ConcurrentDemandControlSubscription,
-                                                                                         mock[QueryContext])
+                                                                                         mock[QueryContext](RETURNS_DEEP_STUBS))
 }
 
 abstract class QueryCompletionTrackerTest extends CypherFunSuite {
