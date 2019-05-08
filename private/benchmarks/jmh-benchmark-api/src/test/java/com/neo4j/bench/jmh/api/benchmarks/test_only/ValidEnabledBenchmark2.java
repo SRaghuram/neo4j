ca940924a8f7a6416dabee5f3d9002425322e578
Alex Averbuch <alex.averbuch@gmail.com>
2019-05-08 16:09:13 +0300

    introduces new common JMH modules  * jmh-benchmark-api: intended as common JMH-wrapping module  * jmh-benchmark-api-test-suite: test only module

---
+++ private/benchmarks/jmh-benchmark-api/src/test/java/com/neo4j/bench/jmh/api/benchmarks/test_only/ValidEnabledBenchmark2.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.jmh.api.benchmarks.test_only;
+
+import com.neo4j.bench.jmh.api.BaseBenchmark;
+import com.neo4j.bench.jmh.api.config.BenchmarkEnabled;
+import com.neo4j.bench.jmh.api.config.ParamValues;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Param;
+
+import static org.openjdk.jmh.annotations.Mode.SampleTime;
+
+@BenchmarkEnabled( true )
+public class ValidEnabledBenchmark2 extends BaseBenchmark
+{
+    @ParamValues(
+            allowed = {"true", "false"},
+            base = {"true"} )
+    @Param( {} )
+    public boolean ValidEnabledBenchmark2_boolean;
+
+    @Override
+    public String benchmarkGroup()
+    {
+        return "Example";
+    }
+
+    @Override
+    public String description()
+    {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public boolean isThreadSafe()
+    {
+        return true;
+    }
+
+    @Benchmark
+    @BenchmarkMode( {SampleTime} )
+    public void method()
+    {
+    }
+}
