2b1a9c30907fb13dec49d49f849b427cdad517e5
Alex Averbuch <alex.averbuch@gmail.com>
2019-05-14 13:16:24 +0300

    fixes code-style and adds missing license headers

--- private/benchmarks/jmh-benchmark-api/src/test/java/com/neo4j/bench/jmh/api/config/AnnotationsFixture.java
+++ private/benchmarks/jmh-benchmark-api/src/test/java/com/neo4j/bench/jmh/api/config/AnnotationsFixture.java
@@ -1,9 +1,14 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
 package com.neo4j.bench.jmh.api.config;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class AnnotationsFixture
+class AnnotationsFixture
 {
     private final String packageName = "com.neo4j.bench.jmh.api.benchmarks";
     private Map<String,Annotations> annotationsCache = new HashMap<>();
