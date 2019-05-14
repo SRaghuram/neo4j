150afafbefd4dca7d00aac62d8f3ad4225838300
Jarosław Pałka <jaroslaw.palka@neotechnology.com>
2019-05-14 08:45:47 +0200

    extended JVM process timeout

--- private/benchmarks/ldbc/neo4j-connectors/src/main/java/com/neo4j/bench/ldbc/profiling/ProfilerRunner.java
+++ private/benchmarks/ldbc/neo4j-connectors/src/main/java/com/neo4j/bench/ldbc/profiling/ProfilerRunner.java
@@ -60,7 +60,7 @@ public class ProfilerRunner
         try
         {
             // If process is still alive, reset start time and wait another timeout
-            JpsPid jpsPid = JpsPid.tryFindFor( jvm, now(), Duration.of( 10, SECONDS ), processName );
+            JpsPid jpsPid = JpsPid.tryFindFor( jvm, now(), Duration.of( 1, MINUTES ), processName );
             if ( !jpsPid.pid().isPresent() )
             {
                 throw new RuntimeException( "Process '" + processName + "' not found!!" );
