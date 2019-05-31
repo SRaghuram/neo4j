203bddbc512e5d0e7326a3b15817bd4f79722a44
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2019-05-31 14:01:48 +0200

    Do not select legacy graph.db as default db when new neo4j already exists.
    In 4.0 default database is `neo4j` and `graph.db`is the database in any
    pre version. We will try to choose graph.db as a default database only
    if neo4j does not exist yet. Before this PR that was not the case and
    selection could happen any time.

--- private/enterprise/commercial-it/commercial-it/src/test/java/org/neo4j/dbms/DefaultDatabaseSelectionIT.java
+++ private/enterprise/commercial-it/commercial-it/src/test/java/org/neo4j/dbms/DefaultDatabaseSelectionIT.java
@@ -7,52 +7,63 @@ package org.neo4j.dbms;
 
 import com.neo4j.commercial.edition.factory.CommercialDatabaseManagementServiceBuilder;
 import com.neo4j.kernel.impl.enterprise.configuration.OnlineBackupSettings;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
 import java.util.Set;
 
-import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.configuration.Config;
 import org.neo4j.configuration.Settings;
 import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
+import org.neo4j.dbms.api.DatabaseNotFoundException;
 import org.neo4j.dbms.database.DatabaseManager;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.io.fs.FileSystemAbstraction;
 import org.neo4j.io.layout.DatabaseLayout;
 import org.neo4j.kernel.database.DatabaseId;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.test.extension.DefaultFileSystemExtension;
 import org.neo4j.test.extension.Inject;
 import org.neo4j.test.extension.TestDirectoryExtension;
 import org.neo4j.test.rule.TestDirectory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+import static org.neo4j.configuration.GraphDatabaseSettings.SYSTEM_DATABASE_NAME;
+import static org.neo4j.configuration.GraphDatabaseSettings.default_database;
 
-@ExtendWith( TestDirectoryExtension.class )
+@ExtendWith( {DefaultFileSystemExtension.class, TestDirectoryExtension.class} )
 class DefaultDatabaseSelectionIT
 {
     private static final String LEGACY_DATABASE_NAME = "graph.db";
     @Inject
     private TestDirectory testDirectory;
+    @Inject
+    private FileSystemAbstraction fileSystem;
     private DatabaseManagementService managementService;
+
+    @AfterEach
+    void tearDown()
+    {
+        if ( managementService != null )
+        {
+            managementService.shutdown();
+        }
+    }
 
     @Test
     void startWithSystemAndNeo4jByDefault()
     {
         GraphDatabaseService database = startDatabase();
-        try
-        {
-            checkDatabaseNames( database, "neo4j" );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, "neo4j" );
     }
 
     @Test
@@ -60,14 +71,7 @@ class DefaultDatabaseSelectionIT
     {
         String customDefaultDatabase = "customDefaultDatabase";
         GraphDatabaseService database = startDatabase( customDefaultDatabase );
-        try
-        {
-            checkDatabaseNames( database, customDefaultDatabase );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, customDefaultDatabase );
     }
 
     @Test
@@ -76,14 +80,7 @@ class DefaultDatabaseSelectionIT
         prepareLegacyStandalone( LEGACY_DATABASE_NAME );
 
         GraphDatabaseService database = startDatabase( LEGACY_DATABASE_NAME );
-        try
-        {
-            checkDatabaseNames( database, LEGACY_DATABASE_NAME );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, LEGACY_DATABASE_NAME );
     }
 
     @Test
@@ -93,32 +90,19 @@ class DefaultDatabaseSelectionIT
 
         String customDatabase = "custom";
         GraphDatabaseService database = startDatabase( customDatabase );
-        try
-        {
-            checkDatabaseNames( database, customDatabase );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, customDatabase );
+        managementService.shutdown();
     }
 
     @Test
     void startWithSystemAndLegacyConfiguredActiveDatabase()
     {
         String customDbName = "activeDb";
-        DatabaseManagementService managementService = getDatabaseBuilder()
+        managementService = getDatabaseBuilder()
                 .setConfig( "dbms.active_database", customDbName )
                 .build();
         GraphDatabaseService database = managementService.database( customDbName );
-        try
-        {
-            checkDatabaseNames( database, customDbName );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, customDbName );
     }
 
     @Test
@@ -126,32 +110,66 @@ class DefaultDatabaseSelectionIT
     {
         prepareLegacyStandalone( LEGACY_DATABASE_NAME );
         String customDbName = "legacyCustomDb";
-        DatabaseManagementService managementService = getDatabaseBuilder()
+        managementService = getDatabaseBuilder()
                 .setConfig( "dbms.active_database", customDbName )
                 .build();
         GraphDatabaseService database = managementService.database( customDbName );
-        try
-        {
-            checkDatabaseNames( database, customDbName );
-        }
-        finally
-        {
-            managementService.shutdown();
-        }
+        checkDatabaseNames( database, customDbName );
     }
 
-    private static void checkDatabaseNames( GraphDatabaseService database, String defaultDatabaseName )
+    @Test
+    void restartWithLegacyDatabasesAndIgnoreIt() throws IOException
+    {
+        managementService = createManagementService();
+        checkDatabaseNames( managementService.database( SYSTEM_DATABASE_NAME ), "neo4j" );
+        DatabaseLayout neo4j = (getDatabaseApiByName( "neo4j" )).databaseLayout();
+        managementService.shutdown();
+
+        DatabaseLayout legacyDbLayout = testDirectory.databaseLayout( LEGACY_DATABASE_NAME );
+        copyDatabaseToLegacyDatabase( neo4j, legacyDbLayout );
+        managementService = createManagementService();
+        Config systemConfig = getDatabaseApiByName( SYSTEM_DATABASE_NAME ).getDependencyResolver().resolveDependency( Config.class );
+        assertEquals( "neo4j", systemConfig.get( default_database ) );
+        assertThrows( DatabaseNotFoundException.class, () -> managementService.database( LEGACY_DATABASE_NAME ) );
+    }
+
+    @Test
+    void restartWithChangedDefaultDatabase()
+    {
+        managementService = createManagementService();
+        checkDatabaseNames( managementService.database( SYSTEM_DATABASE_NAME ), "neo4j" );
+        managementService.shutdown();
+
+        String customDbName = "newBraveDatabase";
+        managementService = getDatabaseBuilder()
+                .setConfig( default_database, customDbName )
+                .build();
+        checkDatabaseNames( managementService.database( SYSTEM_DATABASE_NAME ), customDbName );
+    }
+
+    private GraphDatabaseAPI getDatabaseApiByName( String systemDatabaseName )
+    {
+        return (GraphDatabaseAPI) managementService.database( systemDatabaseName );
+    }
+
+    private void copyDatabaseToLegacyDatabase( DatabaseLayout neo4j, DatabaseLayout legacyDbLayout ) throws IOException
+    {
+        fileSystem.copyRecursively( neo4j.databaseDirectory(), legacyDbLayout.databaseDirectory() );
+        fileSystem.copyRecursively( neo4j.getTransactionLogsDirectory(), legacyDbLayout.getTransactionLogsDirectory() );
+    }
+
+    private static void checkDatabaseNames( GraphDatabaseService database, String databaseName )
     {
         DatabaseManager<?> databaseManager = getDatabaseManager( database );
         Set<DatabaseId> databases = databaseManager.registeredDatabases().keySet();
-        assertThat( databases, containsInAnyOrder( new DatabaseId( defaultDatabaseName ), new DatabaseId( "system" ) ) );
+        assertThat( databases, containsInAnyOrder( new DatabaseId( databaseName ), new DatabaseId( SYSTEM_DATABASE_NAME ) ) );
     }
 
     private void prepareLegacyStandalone( String databaseName ) throws IOException
     {
-        GraphDatabaseService database = startDatabase( databaseName );
+        startDatabase( databaseName );
         managementService.shutdown();
-        DatabaseLayout systemLayout = testDirectory.databaseLayout( GraphDatabaseSettings.SYSTEM_DATABASE_NAME );
+        DatabaseLayout systemLayout = testDirectory.databaseLayout( SYSTEM_DATABASE_NAME );
         assertTrue( systemLayout.metadataStore().exists() );
         FileSystemAbstraction fileSystem = testDirectory.getFileSystem();
         fileSystem.deleteRecursively( systemLayout.getTransactionLogsDirectory() );
@@ -161,14 +179,18 @@ class DefaultDatabaseSelectionIT
 
     private GraphDatabaseService startDatabase()
     {
-        managementService = getDatabaseBuilder().build();
+        managementService = createManagementService();
         return managementService.database( DEFAULT_DATABASE_NAME );
+    }
+
+    private DatabaseManagementService createManagementService()
+    {
+        return getDatabaseBuilder().build();
     }
 
     private GraphDatabaseService startDatabase( String databaseName )
     {
-        managementService = getDatabaseBuilder()
-               .setConfig( GraphDatabaseSettings.default_database, databaseName ).build();
+        managementService = getDatabaseBuilder().setConfig( default_database, databaseName ).build();
         return managementService.database( databaseName );
     }
 
