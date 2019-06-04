2e9becc20f259ad5e1d21f29add4cf071d0b7640
Ali Ince <ali.ince@neotechnology.com>
2019-06-04 10:05:25 +0100

    Fix space handling while invoking powershell scripts

--- private/packaging/standalone/src/main/distribution/shell-scripts/bin/neo4j-admin.bat
+++ private/packaging/standalone/src/main/distribution/shell-scripts/bin/neo4j-admin.bat
@@ -5,5 +5,5 @@ rem This file is a commercial add-on to Neo4j Enterprise Edition.
 
 SETLOCAL
 
-Powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass -File %~dp0neo4j-admin.ps1 %*
+Powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass -File "%~dp0neo4j-admin.ps1" %*
 EXIT /B %ERRORLEVEL%
