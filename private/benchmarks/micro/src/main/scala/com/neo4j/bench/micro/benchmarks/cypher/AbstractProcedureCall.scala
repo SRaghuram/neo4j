10eacd1f758561caa4241aa1cbe7118e7502295b
Pontus Melke <pontusmelke@gmail.com>
2019-06-07 16:51:30 +0200

    Benchmarks for procedure calls
    Adds two micro-benchmarks calling the equivalent of
    
    - `CALL db.labels`
    - `CALL db.labels YIELD label, nodeCount RETURN *`
    
    The two queries are semantically equivalent but are for historical reasons
    are using two different runtimes. This poses some problem keeping the
    behaviors of the two code paths in sync and we would like to merge the two
    and keeping only the the one used for the second query. However before
    we do that we want to make sure performance is comparable.

---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/AbstractProcedureCall.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.micro.data.Plans.{Pos, astVariable}
+import org.neo4j.cypher.internal.logical.plans._
+import org.neo4j.cypher.internal.spi.procsHelpers.{asCypherType, asCypherValue, asOption}
+import org.neo4j.cypher.internal.v4_0.ast.semantics.{ExpressionTypeInfo, SemanticTable}
+import org.neo4j.cypher.internal.v4_0.ast.{ASTAnnotationMap, ProcedureResultItem}
+import org.neo4j.cypher.internal.v4_0.expressions.Expression
+import org.neo4j.internal.kernel.api.procs.{QualifiedName => KernelQualifiedName}
+import org.neo4j.kernel.api.procedure.GlobalProcedures
+import org.neo4j.kernel.internal.GraphDatabaseAPI
+
+import scala.collection.JavaConverters._
+
+abstract class AbstractProcedureCall extends AbstractCypherBenchmark {
+  protected var procedureSignature: ProcedureSignature = _
+
+  /**
+    * Called after starting of database
+    */
+  override protected def afterDatabaseStart(): Unit = {
+    super.afterDatabaseStart()
+    val resolver = db.asInstanceOf[GraphDatabaseAPI].getDependencyResolver
+    val procs = resolver.resolveDependency(classOf[GlobalProcedures])
+
+    val procedure = procs.procedure(new KernelQualifiedName(procedureName.namespace.toArray, procedureName.name))
+    val signature = procedure.signature()
+    val input = signature.inputSignature().asScala
+      .map(s => FieldSignature(s.name(), asCypherType(s.neo4jType()), asOption(s.defaultValue()).map(asCypherValue)))
+      .toIndexedSeq
+    val output = if (signature.isVoid) None else Some(
+      signature.outputSignature().asScala
+        .map(s => FieldSignature(s.name(), asCypherType(s.neo4jType()), deprecated = s.isDeprecated)).toIndexedSeq)
+
+    procedureSignature = ProcedureSignature(procedureName,
+                                            input,
+                                            output,
+                                            None,
+                                            ProcedureReadOnlyAccess(signature.allowed()),
+                                            asOption(signature.description()),
+                                            asOption(signature.warning),
+                                            procedure.signature().eager(),
+                                            procedure.id())
+
+  }
+
+  def columns: Seq[String] = procedureSignature.outputSignature.map(f => f.map(_.name)).getOrElse(List.empty)
+
+  def resolvedCall: ResolvedCall = {
+    val callResults = columns.map(n => ProcedureResultItem(astVariable(n))(Pos)).toIndexedSeq
+    ResolvedCall(procedureSignature, Seq.empty, callResults)(Pos)
+  }
+
+  def semanticTable: SemanticTable = {
+    val semanticTypes = procedureSignature.outputSignature.getOrElse(IndexedSeq.empty).foldLeft(ASTAnnotationMap.empty[Expression, ExpressionTypeInfo]) {
+      case (acc, current) => acc.updated(astVariable(current.name), ExpressionTypeInfo(current.typ, None))
+    }
+    SemanticTable(types = semanticTypes)
+  }
+
+   protected def procedureName: QualifiedName
+}
---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/StandAloneProcedureCall.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.micro.Main
+import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.config.{BenchmarkEnabled, ParamValues}
+import com.neo4j.bench.micro.data.Plans.IdGen
+import com.neo4j.bench.micro.data.{DataGeneratorConfig, DataGeneratorConfigBuilder}
+import org.neo4j.cypher.internal.logical.plans
+import org.neo4j.cypher.internal.logical.plans.QualifiedName
+import org.neo4j.cypher.internal.planner.spi.PlanContext
+import org.neo4j.cypher.internal.v4_0.ast.semantics.SemanticTable
+import org.neo4j.graphdb.Label
+import org.neo4j.kernel.impl.coreapi.InternalTransaction
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+
+@BenchmarkEnabled(true)
+class StandAloneProcedureCall extends AbstractProcedureCall {
+
+  @ParamValues(
+    allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, EnterpriseInterpreted.NAME, Morsel.NAME),
+    base = Array(EnterpriseInterpreted.NAME))
+  @Param(Array[String]())
+  var StandAloneProcedureCall_runtime: String = _
+
+  @ParamValues(
+    allowed = Array("1", "100", "1000"),
+    base = Array("1", "100", "1000"))
+  @Param(Array[String]())
+  var StandAloneProcedureCall_labels: Int = _
+
+  override def description = "Stand-alone procedure call, CALL db.labels"
+
+  override protected def getConfig: DataGeneratorConfig = {
+    val labels = (0 until StandAloneProcedureCall_labels).map(c => Label.label(s"label$c"))
+    new DataGeneratorConfigBuilder()
+      .withNodeCount(1)
+      .withLabels(labels:_*)
+      .isReusableStore(true)
+      .build()
+  }
+
+  override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
+    val plan = plans.StandAloneProcedureCall(
+      procedureSignature,
+      resolvedCall.callArguments,
+      resolvedCall.callResultTypes,
+      resolvedCall.callResultIndices
+      )(IdGen)
+
+    (plan, semanticTable, columns.toList)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SampleTime))
+  def executePlan(threadState: StandAloneProcedureCallThreadState, bh: Blackhole): Long = {
+    val visitor = new CountVisitor(bh)
+    threadState.executablePlan.execute(tx = threadState.tx).accept(visitor)
+    assertExpectedRowCount(StandAloneProcedureCall_labels, visitor)
+  }
+
+  override protected def procedureName: QualifiedName = QualifiedName(Seq("db"), "labels")
+}
+
+object StandAloneProcedureCall {
+  def main(args: Array[String]): Unit = {
+    Main.run(classOf[StandAloneProcedureCall], args:_*)
+  }
+}
+
+@State(Scope.Thread)
+class StandAloneProcedureCallThreadState {
+
+  var tx: InternalTransaction = _
+  var executablePlan: ExecutablePlan = _
+
+  @Setup
+  def setUp(benchmarkState: StandAloneProcedureCall): Unit = {
+    executablePlan = benchmarkState.buildPlan(from(benchmarkState.StandAloneProcedureCall_runtime))
+    tx = benchmarkState.beginInternalTransaction()
+  }
+
+  @TearDown
+  def tearDown(): Unit = {
+    tx.close()
+  }
+}
