5f45d0f9def703c3433e3990b1b7f2f04f37aeb2
Alex Averbuch <alex.averbuch@gmail.com>
2019-06-13 19:29:27 +0200

    fix bug when NodeCountFromCountStore on RHS of apply

--- private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/NodeCountFromCountStoreOperator.scala
+++ private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/NodeCountFromCountStoreOperator.scala
@@ -36,36 +36,43 @@ class NodeCountFromCountStoreOperator(val workIdentity: WorkIdentity,
 
     override def toString: String = "NodeFromCountStoreTask"
 
+    private var hasNext = false
+
     override protected def initializeInnerLoop(context: QueryContext, state: QueryState, resources: QueryResources): Boolean = {
+      hasNext = true
       true
     }
 
     override protected def innerLoop(outputRow: MorselExecutionContext, context: QueryContext, state: QueryState): Unit = {
-      var count = 1L
+      if (hasNext) {
+        var count = 1L
 
-      var i = 0
-      while (i < lazyLabels.length) {
-        val idOfLabel = lazyLabels(i).getId(context)
-        if (idOfLabel == LazyLabel.UNKNOWN) {
-          count = 0
-        } else {
-          count = count * context.nodeCountByCountStore(idOfLabel)
+        var i = 0
+        while (i < lazyLabels.length) {
+          val idOfLabel = lazyLabels(i).getId(context)
+          if (idOfLabel == LazyLabel.UNKNOWN) {
+            count = 0
+          } else {
+            count = count * context.nodeCountByCountStore(idOfLabel)
+          }
+          i += 1
         }
-        i += 1
-      }
-      i = 0
-      while (i < wildCards) {
-        count *= context.nodeCountByCountStore(NameId.WILDCARD)
-        i += 1
-      }
+        i = 0
+        while (i < wildCards) {
+          count *= context.nodeCountByCountStore(NameId.WILDCARD)
+          i += 1
+        }
 
-      outputRow.copyFrom(inputMorsel, argumentSize.nLongs, argumentSize.nReferences)
-      outputRow.setRefAt(offset, Values.longValue(count))
-      outputRow.moveToNextRow()
+        outputRow.copyFrom(inputMorsel, argumentSize.nLongs, argumentSize.nReferences)
+        outputRow.setRefAt(offset, Values.longValue(count))
+        outputRow.moveToNextRow()
+        hasNext = false
+      }
     }
 
     override protected def closeInnerLoop(resources: QueryResources): Unit = {
       // nothing to do here
     }
   }
+
 }
