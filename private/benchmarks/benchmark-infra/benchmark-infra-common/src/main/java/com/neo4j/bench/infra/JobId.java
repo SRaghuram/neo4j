a6f0b7c4e7b9843b0b6040976d7ba8207178b7bc
Alex Averbuch <alex.averbuch@gmail.com>
2019-07-09 12:11:50 +0300

    changes from PR review comments

--- private/benchmarks/benchmark-infra/benchmark-infra-common/src/main/java/com/neo4j/bench/infra/JobId.java
+++ private/benchmarks/benchmark-infra/benchmark-infra-common/src/main/java/com/neo4j/bench/infra/JobId.java
@@ -5,6 +5,8 @@
  */
 package com.neo4j.bench.infra;
 
+import java.util.Objects;
+
 public class JobId
 {
     private final String id;
@@ -17,5 +19,26 @@ public class JobId
     public String id()
     {
         return id;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o )
+        {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() )
+        {
+            return false;
+        }
+        JobId jobId = (JobId) o;
+        return Objects.equals( id, jobId.id );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( id );
     }
 }
