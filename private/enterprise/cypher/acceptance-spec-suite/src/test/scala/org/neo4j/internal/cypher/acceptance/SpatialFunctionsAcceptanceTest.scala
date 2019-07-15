3ce46531dfe1514fd715d7a7af0d75bab019be3b
Sascha Peukert <sascha.peukert@neotechnology.com>
2019-07-15 15:05:21 +0200

    Properly validate input coordinates on WGS-84 points
    X - coordinates are bound by [-180, 180]. If something else is entered, it will be wrapped
    Y - coordinates are bound by [-90, 90]. If something else is entered, an error will be thrown

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/SpatialFunctionsAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/SpatialFunctionsAcceptanceTest.scala
@@ -17,6 +17,35 @@ class SpatialFunctionsAcceptanceTest extends ExecutionEngineFunSuite with Cypher
     executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude:1, latitude:2, height:3})) AS s").toList should equal(List(Map("s" -> "point({x: 1.0, y: 2.0, z: 3.0, crs: 'wgs-84-3d'})")))
   }
 
+  test("geometric points should validate and possibly wrap coordinates") {
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 180, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 180.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: -180, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: -180.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 190, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: -170.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: -190, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 170.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 360, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 0.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: -360, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 0.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 540, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 180.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: -540, latitude: 0})) AS s").toList should
+      equal(List(Map("s" -> "point({x: -180.0, y: 0.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 0, latitude: 90})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 0.0, y: 90.0, crs: 'wgs-84'})")))
+    executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 0, latitude: -90})) AS s").toList should
+      equal(List(Map("s" -> "point({x: 0.0, y: -90.0, crs: 'wgs-84'})")))
+
+    // no wrapping for y coordinates
+    failWithError(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 0, latitude: 91})) AS s", errorType = Seq("InvalidArgumentException"),
+      message = Seq("Cannot create WGS84 point with invalid coordinate for Y: [0.0, 91.0]. Valid range is [-90,90]."))
+    failWithError(Configs.InterpretedAndSlottedAndMorsel, "RETURN toString(point({longitude: 0, latitude: -91})) AS s", errorType = Seq("InvalidArgumentException"),
+      message = Seq("Cannot create WGS84 point with invalid coordinate for Y: [0.0, -91.0]. Valid range is [-90,90]."))
+  }
+
   test("point function should work with literal map") {
     val result = executeWith(Configs.InterpretedAndSlottedAndMorsel, "RETURN point({latitude: 12.78, longitude: 56.7}) as point",
       planComparisonStrategy = ComparePlansWithAssertion(_ should includeSomewhere.aPlan("Projection").containingArgumentRegex("\\{point : .*\\}".r)))
