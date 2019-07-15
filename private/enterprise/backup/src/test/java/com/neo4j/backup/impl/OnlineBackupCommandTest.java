e33d506c2f6a84a78e9981ebca31a55705f19748
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2019-07-15 14:04:24 +0200

    Adapt to command output format change.

--- private/enterprise/backup/src/test/java/com/neo4j/backup/impl/OnlineBackupCommandTest.java
+++ private/enterprise/backup/src/test/java/com/neo4j/backup/impl/OnlineBackupCommandTest.java
@@ -29,63 +29,57 @@ class OnlineBackupCommandTest
             CommandLine.usage( command, new PrintStream( out ) );
         }
         assertThat( baos.toString().trim(), equalTo( String.format(
-                "Perform an online backup from a running Neo4j enterprise server.%n" +
-                "%n" +
-                "USAGE%n" +
-                "%n" +
-                "backup [--check-consistency] [--fallback-to-full] [--verbose]%n" +
-                "       [--additional-config=<path>] --backup-dir=<path>%n" +
-                "       [--check-graph=<true/false>] [--check-indexes=<true/false>]%n" +
-                "       [--check-label-scan-store=<true/false>]%n" +
-                "       [--check-property-owners=<true/false>] [--database=<database>]%n" +
-                "       [--from=<host:port>] [--pagecache=<size>] [--report-dir=<path>]%n" +
-                "%n" +
-                "DESCRIPTION%n" +
-                "%n" +
-                "Perform an online backup from a running Neo4j enterprise server. Neo4j's backup%n" +
-                "service must have been configured on the server beforehand.%n" +
-                "%n" +
-                "All consistency checks except 'cc-graph' can be quite expensive so it may be%n" +
-                "useful to turn them off for very large databases. Increasing the heap size can%n" +
-                "also be a good idea. See 'neo4j-admin help' for details.%n" +
-                "%n" +
-                "For more information see: https://neo4j.%n" +
-                "com/docs/operations-manual/current/backup/%n" +
-                "%n" +
-                "OPTIONS%n" +
-                "%n" +
-                "      --verbose             Enable verbose output.%n" +
-                "      --backup-dir=<path>   Directory to place backup in.%n" +
-                "      --from=<host:port>    Host and port of Neo4j.%n" +
-                "                              Default: localhost:6362%n" +
-                "      --database=<database> Name of the remote database to backup.%n" +
-                "                              Default: neo4j%n" +
-                "      --fallback-to-full    If an incremental backup fails backup will move the old%n" +
-                "                              backup to <name>.err.<N> and fallback to a full.%n" +
-                "                              Default: true%n" +
-                "      --pagecache=<size>    The size of the page cache to use for the backup process.%n" +
-                "                              Default: 8m%n" +
-                "      --check-consistency   If a consistency check should be made.%n" +
-                "                              Default: true%n" +
-                "      --report-dir=<path>   Directory where consistency report will be written.%n" +
-                "                              Default: .%n" +
-                "      --check-graph=<true/false>%n" +
-                "                            Perform consistency checks between nodes, relationships,%n" +
-                "                              properties, types and tokens.%n" +
-                "                              Default: true%n" +
-                "      --check-indexes=<true/false>%n" +
-                "                            Perform consistency checks on indexes.%n" +
-                "                              Default: true%n" +
-                "      --check-label-scan-store=<true/false>%n" +
-                "                            Perform consistency checks on the label scan store.%n" +
-                "                              Default: true%n" +
-                "      --check-property-owners=<true/false>%n" +
-                "                            Perform additional consistency checks on property%n" +
-                "                              ownership. This check is very expensive in time and%n" +
-                "                              memory.%n" +
-                "                              Default: false%n" +
-                "      --additional-config=<path>%n" +
-                "                            Configuration file to supply additional configuration in."
+                "Perform an online backup from a running Neo4j enterprise server.%n%n" +
+                        "USAGE%n" + "%n" +
+                        "backup [--check-consistency] [--fallback-to-full] [--verbose]%n" +
+                        "       [--additional-config=<path>] --backup-dir=<path>%n" +
+                        "       [--check-graph=<true/false>] [--check-indexes=<true/false>]%n" +
+                        "       [--check-label-scan-store=<true/false>]%n" +
+                        "       [--check-property-owners=<true/false>] [--database=<database>]%n" +
+                        "       [--from=<host:port>] [--pagecache=<size>] [--report-dir=<path>]%n%n"
+                        + "DESCRIPTION%n" + "%n" +
+                        "Perform an online backup from a running Neo4j enterprise server. Neo4j's backup%n" +
+                        "service must have been configured on the server beforehand.%n%n" +
+                        "All consistency checks except 'cc-graph' can be quite expensive so it may be%n" +
+                        "useful to turn them off for very large databases. Increasing the heap size can%n" +
+                        "also be a good idea. See 'neo4j-admin help' for details.%n%n"
+                        + "For more information see: https://neo4j.%ncom/docs/operations-manual/current/backup/%n" + "%n"
+                        + "OPTIONS%n%n" +
+                        "      --verbose             Enable verbose output.%n" +
+                        "      --backup-dir=<path>   Directory to place backup in.%n" +
+                        "      --from=<host:port>    Host and port of Neo4j.%n" +
+                        "                              Default: localhost:6362%n" +
+                        "      --database=<database> Name of the remote database to backup.%n" +
+                        "                              Default: neo4j%n" +
+                        "      --fallback-to-full    If an incremental backup fails backup will move the%n" +
+                        "                              old backup to <name>.err.<N> and fallback to a%n" +
+                        "                              full.%n" +
+                        "                              Default: true%n" +
+                        "      --pagecache=<size>    The size of the page cache to use for the backup%n" +
+                        "                              process.%n" +
+                        "                              Default: 8m%n" +
+                        "      --check-consistency   If a consistency check should be made.%n" +
+                        "                              Default: true%n" +
+                        "      --report-dir=<path>   Directory where consistency report will be written.%n" +
+                        "                              Default: .%n" +
+                        "      --check-graph=<true/false>%n" +
+                        "                            Perform consistency checks between nodes,%n" +
+                        "                              relationships, properties, types and tokens.%n" +
+                        "                              Default: true%n" +
+                        "      --check-indexes=<true/false>%n" +
+                        "                            Perform consistency checks on indexes.%n" +
+                        "                              Default: true%n" +
+                        "      --check-label-scan-store=<true/false>%n" +
+                        "                            Perform consistency checks on the label scan store.%n" +
+                        "                              Default: true%n" +
+                        "      --check-property-owners=<true/false>%n" +
+                        "                            Perform additional consistency checks on property%n" +
+                        "                              ownership. This check is very expensive in time%n" +
+                        "                              and memory.%n" +
+                        "                              Default: false%n" +
+                        "      --additional-config=<path>%n" +
+                        "                            Configuration file to supply additional%n" +
+                        "                              configuration in."
         ) ) );
     }
 }
