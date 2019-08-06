c2f9c274346fd7936b1be100d830ee95cd9439e9
Sascha Peukert <sascha.peukert@neotechnology.com>
2019-08-06 10:39:03 +0200

    Properly count relationships for restricted user

--- private/enterprise/commercial-it/cypher-it/src/test/scala/com/neo4j/internal/cypher/acceptance/PrivilegeEnforcementDDLAcceptanceTest.scala
+++ private/enterprise/commercial-it/cypher-it/src/test/scala/com/neo4j/internal/cypher/acceptance/PrivilegeEnforcementDDLAcceptanceTest.scala
@@ -2904,6 +2904,59 @@ class PrivilegeEnforcementDDLAcceptanceTest extends DDLAcceptanceTestBase {
     }) should be(0)
   }
 
+  test("Counting queries should work with restricted user") {
+    // The two queries are used by the browser
+    val countingNodesQuery = "MATCH () RETURN { name:'nodes', data:count(*) } AS result"
+    val countingRelsQuery = "MATCH ()-[]->() RETURN { name:'relationships', data: count(*)} AS result"
+
+    // Given
+    selectDatabase(DEFAULT_DATABASE_NAME)
+    execute("CREATE (:Person)-[:WROTE]->(:Letter)<-[:HAS_STAMP]-(:Stamp)")
+
+    selectDatabase(SYSTEM_DATABASE_NAME)
+    execute("CREATE USER tim SET PASSWORD '123' CHANGE NOT REQUIRED")
+    execute("CREATE ROLE role")
+    execute("GRANT ROLE role TO tim")
+    execute("GRANT MATCH(*) ON GRAPH * ELEMENTS * TO role")
+    execute("DENY TRAVERSE ON GRAPH * RELATIONSHIP WROTE TO role")
+
+    // RELS
+    selectDatabase(DEFAULT_DATABASE_NAME)
+    // When & Then
+
+    // unrestricted:
+    execute(countingRelsQuery).toList should be(List(Map("result" -> Map("data" -> 2, "name" -> "relationships"))))
+
+    // restricted
+    executeOnDefault("tim", "123", countingRelsQuery,
+      resultHandler = (row, _) => {
+        val result = row.get("result").asInstanceOf[util.Map[String, AnyRef]]
+        result.get("data") should be (1L)
+        result.get("name") should be ("relationships")
+      }
+    ) should be(1)
+
+    // Given
+    selectDatabase(SYSTEM_DATABASE_NAME)
+    execute("DENY TRAVERSE ON GRAPH * NODES Person TO role")
+
+    // NODES
+    selectDatabase(DEFAULT_DATABASE_NAME)
+    // When & Then
+
+    // unrestricted:
+    execute(countingNodesQuery).toList should be(List(Map("result" -> Map("data" -> 3, "name" -> "nodes"))))
+
+    // restricted
+    executeOnDefault("tim", "123", countingNodesQuery,
+      resultHandler = (row, _) => {
+        val result = row.get("result").asInstanceOf[util.Map[String, AnyRef]]
+        result.get("data") should be (2L)
+        result.get("name") should be ("nodes")
+      }
+    ) should be(1)
+  }
+
   // helper variable, methods and class
 
   private def setupMultiLabelData = {
