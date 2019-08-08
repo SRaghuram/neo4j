7d7116d67be479e61d1c06551427e87ed05fca19
Pontus Melke <pontusmelke@gmail.com>
2019-08-08 11:27:09 +0200

    Revert "Update test config"
    This reverts commit 031f37f7717b53fa6931d2555b0f2106b5435266.

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ProfilerAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ProfilerAcceptanceTest.scala
@@ -94,7 +94,7 @@ class ProfilerAcceptanceTest extends ExecutionEngineFunSuite with CreateTempFile
                   includeSomewhere.aPlan("AllNodesScan").withTime()
                 )
             ),
-        expectPlansToFail = Configs.InterpretedAndSlottedAndMorsel)
+        expectPlansToFail = Configs.InterpretedAndSlotted)
     )
   }
 
