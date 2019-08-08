1db850f4a9fa6b9926c0c9edb362034df957264f
Pontus Melke <pontusmelke@gmail.com>
2019-08-08 11:27:09 +0200

    Revert "Fix rebase error"
    This reverts commit 58b3e397e44c99e2b1a54b2f1ee54cf5f48469a9.

--- private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/ProjectOperator.scala
+++ private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/ProjectOperator.scala
@@ -54,7 +54,7 @@ class ProjectOperatorTemplate(override val inner: OperatorTaskTemplate,
   private var projections: IntermediateExpression = _
 
   override def genOperate: IntermediateRepresentation = {
-    projections = codeGen.intermediateCompileProjection(projectionOps).getOrElse(throw new CantCompileQueryException(s"The expression compiler could not compile $projectionOps"))
+    projections = codeGen.intermediateCompileProjection(projectionOps).getOrElse(throw new CantCompileQueryException())
     block(
       projections.ir,
       profileRow(id),
