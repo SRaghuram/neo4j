af225d1d06942a48470006fd6093d31b990902b5
Satia Herfert <satiaherfert@gmx.de>
2019-08-14 17:05:51 +0200

    After rebase

--- private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/ProjectOperator.scala
+++ private/enterprise/cypher/morsel-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/morsel/operators/ProjectOperator.scala
@@ -7,9 +7,7 @@ package org.neo4j.cypher.internal.runtime.morsel.operators
 
 import org.neo4j.codegen.api.IntermediateRepresentation.block
 import org.neo4j.codegen.api.{Field, IntermediateRepresentation, LocalVariable}
-import org.neo4j.codegen.api.IntermediateRepresentation.block
-import org.neo4j.codegen.api.{Field, IntermediateRepresentation, LocalVariable}
-import org.neo4j.cypher.CantCompileQueryException
+import org.neo4j.exceptions.CantCompileQueryException
 import org.neo4j.cypher.internal.runtime.{NoMemoryTracker, QueryContext}
 import org.neo4j.cypher.internal.runtime.compiled.expressions.IntermediateExpression
 import org.neo4j.cypher.internal.runtime.interpreted.CommandProjection
