9788f765dbb9a3b7d186ac66c5f942a1d2568af0
Alex Averbuch <alex.averbuch@gmail.com>
2019-08-21 14:30:24 +0300

    configure file writers to flush & sync

--- private/benchmarks/benchmark-common/src/main/java/com/neo4j/bench/common/profiling/StackCollapseWriter.java
+++ private/benchmarks/benchmark-common/src/main/java/com/neo4j/bench/common/profiling/StackCollapseWriter.java
@@ -5,10 +5,12 @@
  */
 package com.neo4j.bench.common.profiling;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import static java.lang.String.format;
 
@@ -16,11 +18,11 @@ public class StackCollapseWriter
 {
     public static void write( StackCollapse stackCollapse, Path file )
     {
-        try ( PrintWriter printWriter = new PrintWriter( file.toFile() ) )
+        try ( PrintWriter printWriter = new PrintWriter( Files.newOutputStream( file, StandardOpenOption.SYNC ), true /*auto flush*/ ) )
         {
             stackCollapse.forEachStackTrace( ( stackTrace, sum ) -> printWriter.println( format( "%s %d", stackTrace, sum ) ) );
         }
-        catch ( FileNotFoundException e )
+        catch ( IOException e )
         {
             throw new UncheckedIOException( "Error trying to write stack collapse to file", e );
         }
