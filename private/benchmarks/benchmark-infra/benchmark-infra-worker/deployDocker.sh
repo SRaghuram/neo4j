8fe6b92ec4fcafb0f7057e449a9bdf6bf632294f
Simon Svensson <simpan.svensson@gmail.com>
2019-08-21 10:42:20 +0200

    Added pushing of profiler docker

--- private/benchmarks/benchmark-infra/benchmark-infra-worker/deployDocker.sh
+++ private/benchmarks/benchmark-infra/benchmark-infra-worker/deployDocker.sh
@@ -3,9 +3,10 @@
 # shellcheck disable=SC2091
 $(aws ecr get-login --no-include-email --region eu-west-1)
 #build
-docker build -f profilers/Dockerfile .  -t profile:latest
+docker build -f profilers/Dockerfile .  -t 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:profile
 docker build  -f oraclejdk-11/Dockerfile . -t 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:oracle_11
 docker build  -f oraclejdk-8/Dockerfile . -t 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:oracle_8
 #Push
+docker push 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:profile
 docker push 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:oracle_11
 docker push 065531048259.dkr.ecr.eu-west-1.amazonaws.com/benchmarks-worker:oracle_8
