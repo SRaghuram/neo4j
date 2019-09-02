926a045fc1310459d2199421b1103e619e432ebf
Pontus Melke <pontusmelke@gmail.com>
2019-09-02 14:19:54 +0200

    More test assertion updates

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/HintAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/HintAcceptanceTest.scala
@@ -134,7 +134,7 @@ class HintAcceptanceTest
         |AND date("2017-01-01") <= r.date <= date("2018-01-01")
         |RETURN COUNT(*)""".stripMargin
 
-    val result = executeWith(Configs.InterpretedAndSlotted, query)
+    val result = executeWith(Configs.UDF, query)
 
     // Then
     result.toList should be(List(Map("COUNT(*)" -> 1)))
