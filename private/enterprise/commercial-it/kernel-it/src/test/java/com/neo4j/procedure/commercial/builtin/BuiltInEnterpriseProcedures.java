f30f7b8be32bebe51ce1baf94b396947e892a9eb
Sascha Peukert <sascha.peukert@neotechnology.com>
2019-09-06 13:05:41 +0200

    dbms.procedures and dbms.functions have now same return columns in CE and EE

--- private/enterprise/commercial-it/kernel-it/src/test/java/com/neo4j/procedure/commercial/builtin/BuiltInEnterpriseProcedures.java
+++ private/enterprise/commercial-it/kernel-it/src/test/java/com/neo4j/procedure/commercial/builtin/BuiltInEnterpriseProcedures.java
@@ -6,19 +6,53 @@
 package com.neo4j.procedure.commercial.builtin;
 
 import com.neo4j.test.extension.CommercialDbmsExtension;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.procedure.builtin.BuiltInDbmsProcedures;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 
 @CommercialDbmsExtension
 class BuiltInEnterpriseProcedures extends SystemBuiltInEnterpriseProcedures
 {
-    // This class is to make sure we get the same result for listProcedures on user database
-
     @Override
     public GraphDatabaseAPI getGraphDatabaseAPI()
     {
         return (GraphDatabaseAPI)databaseManagementService.database( DEFAULT_DATABASE_NAME );
+    }
+
+    @Test
+    public void checkThatEnterpriseAndCommunityHaveSameResultColumsForListProcedures()
+    {
+        checkClassesHaveSameFinalFields( BuiltInDbmsProcedures.ProcedureResult.class, EnterpriseBuiltInDbmsProcedures.ProcedureResult.class );
+    }
+
+    @Test
+    public void checkThatEnterpriseAndCommunityHaveSameResultColumsForListFunctions()
+    {
+        checkClassesHaveSameFinalFields( BuiltInDbmsProcedures.FunctionResult.class, EnterpriseBuiltInDbmsProcedures.FunctionResult.class );
+    }
+
+    private void checkClassesHaveSameFinalFields( Class community, Class enterprise )
+    {
+        Field[] communityFields = community.getFields();
+        Field[] enterpriseFields = enterprise.getFields();
+        assertEquals( communityFields.length, enterpriseFields.length );
+
+        for ( int i = 0; i < communityFields.length; i++ )
+        {
+            Field comField = communityFields[i];
+            Field entField = enterpriseFields[i];
+
+            assertTrue( Modifier.isFinal( comField.getModifiers() ) );
+            assertTrue( Modifier.isFinal( entField.getModifiers() ) );
+            assertEquals( comField.getName(), entField.getName() );
+        }
     }
 }
