d5f1a5545ae304951b5e4edd32a811a995422c5a
Pontus Melke <pontusmelke@gmail.com>
2019-09-15 22:32:08 +0200

    Reenable disabled micro benchmarks
    These benchmarks were disabled at some point but I still believe they are
    relevant.

--- private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/GreaterThanIndexSeek.scala
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/GreaterThanIndexSeek.scala
@@ -20,7 +20,7 @@ import org.neo4j.kernel.impl.coreapi.InternalTransaction
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 
-@BenchmarkEnabled(false)
+@BenchmarkEnabled(true)
 class GreaterThanIndexSeek extends AbstractCypherBenchmark {
   @ParamValues(
     allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, Slotted.NAME, Morsel.NAME),
