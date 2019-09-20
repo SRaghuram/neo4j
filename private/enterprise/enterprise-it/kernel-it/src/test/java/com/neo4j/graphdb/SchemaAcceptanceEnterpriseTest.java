f0e9bb27b2a7abb37f4d153e4a0ed3feb1acb272
Anton Persson <anton.persson@neotechnology.com>
2019-09-20 15:35:19 +0200

    Test Core API schema rule creation exceptions, part 1
    Extract some functionality into SchemaAcceptanceTestBase and introduce
    SchemaAcceptanceEnterpriseTest that include testing for NodeKey and
    Existence constraints.
    
    Exceptions tested so for:
    - EquivalentSchemaRuleExistsException
    - AlreadyIndexedException

---
+++ private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/graphdb/SchemaAcceptanceEnterpriseTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.graphdb;
+
+import com.neo4j.test.extension.ImpermanentEnterpriseDbmsExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import org.neo4j.graphdb.ConstraintViolationException;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.SchemaAcceptanceTestBase;
+import org.neo4j.kernel.api.exceptions.schema.AlreadyIndexedException;
+import org.neo4j.kernel.api.exceptions.schema.EquivalentSchemaRuleAlreadyExistsException;
+import org.neo4j.test.extension.Inject;
+
+@ImpermanentEnterpriseDbmsExtension
+class SchemaAcceptanceEnterpriseTest extends SchemaAcceptanceTestBase
+{
+    @Inject
+    private GraphDatabaseService db;
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfEquivalentNodeKeyConstraintExist( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "name" ).create(),
+                ConstraintViolationException.class );
+        Class<EquivalentSchemaRuleAlreadyExistsException> expectedCause = EquivalentSchemaRuleAlreadyExistsException.class;
+        String expectedMessage = "An equivalent constraint already exists, 'Constraint( UNIQUE_EXISTS, :MY_LABEL(my_property_key) )'.";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfEquivalentExistenceConstraintExist( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyExists( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyExists( propertyKey ).withName( "name" ).create(),
+                ConstraintViolationException.class );
+        Class<EquivalentSchemaRuleAlreadyExistsException> expectedCause = EquivalentSchemaRuleAlreadyExistsException.class;
+        String expectedMessage = "An equivalent constraint already exists, 'Constraint( EXISTS, :MY_LABEL(my_property_key) )'.";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfSchemaAlreadyIndexedWhenCreatingNodeKeyConstraint( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.indexFor( label ).on( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "otherName" ).create(),
+                ConstraintViolationException.class );
+        Class<AlreadyIndexedException> expectedCause = AlreadyIndexedException.class;
+        String expectedMessage = "There already exists an index :MY_LABEL(my_property_key). A constraint cannot be created until the index has been dropped.";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    //todo
+    // X shouldThrowIfEquivalentNodeKeyConstraintExist
+    // X shouldThrowIfEquivalentExistenceConstraintExist
+    // X shouldThrowIfSchemaAlreadyIndexedWhenCreatingNodeKeyConstraint
+}
