f3aa3093fdf7069722233faf73b847bf5d140f30
Anton Persson <anton.persson@neotechnology.com>
2019-09-20 15:51:48 +0200

    Test Core API schema rule creation exceptions, part 2
    Also test AlreadyConstrainedException

--- private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/graphdb/SchemaAcceptanceEnterpriseTest.java
+++ private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/graphdb/SchemaAcceptanceEnterpriseTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.neo4j.graphdb.ConstraintViolationException;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.SchemaAcceptanceTestBase;
+import org.neo4j.kernel.api.exceptions.schema.AlreadyConstrainedException;
 import org.neo4j.kernel.api.exceptions.schema.AlreadyIndexedException;
 import org.neo4j.kernel.api.exceptions.schema.EquivalentSchemaRuleAlreadyExistsException;
 import org.neo4j.test.extension.Inject;
@@ -61,8 +62,64 @@ class SchemaAcceptanceEnterpriseTest extends SchemaAcceptanceTestBase
         assertExpectedException( expectedCause, expectedMessage, exception );
     }
 
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfSchemaAlreadyUniquenessConstrainedWhenCreatingNodeKeyConstraint( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyIsUnique( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "otherName" ).create(),
+                ConstraintViolationException.class );
+        Class<AlreadyConstrainedException> expectedCause = AlreadyConstrainedException.class;
+        String expectedMessage = "Constraint already exists: CONSTRAINT ON ( my_label:MY_LABEL ) ASSERT (my_label.my_property_key) IS UNIQUE";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingIndex( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.indexFor( label ).on( propertyKey ).withName( "otherName" ).create(),
+                ConstraintViolationException.class );
+        Class<AlreadyConstrainedException> expectedCause = AlreadyConstrainedException.class;
+        String expectedMessage = "There is a uniqueness constraint on :MY_LABEL(my_property_key), so an index is already created that matches this.";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingUniquenessConstraint( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyIsUnique( propertyKey ).withName( "otherName" ).create(),
+                ConstraintViolationException.class );
+        Class<AlreadyConstrainedException> expectedCause = AlreadyConstrainedException.class;
+        String expectedMessage = "Constraint already exists: CONSTRAINT ON ( my_label:MY_LABEL ) ASSERT (my_label.my_property_key) IS NODE KEY";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
+    @ParameterizedTest()
+    @EnumSource( SchemaTxStrategy.class )
+    void shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingNodeKeyConstraint( SchemaTxStrategy txStrategy )
+    {
+        final ConstraintViolationException exception = txStrategy.execute( db,
+                schema -> schema.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "name" ).create(),
+                schema1 -> schema1.constraintFor( label ).assertPropertyIsNodeKey( propertyKey ).withName( "otherName" ).create(),
+                ConstraintViolationException.class );
+        Class<AlreadyConstrainedException> expectedCause = AlreadyConstrainedException.class;
+        String expectedMessage = "Constraint already exists: CONSTRAINT ON ( my_label:MY_LABEL ) ASSERT (my_label.my_property_key) IS NODE KEY";
+        assertExpectedException( expectedCause, expectedMessage, exception );
+    }
+
     //todo
     // X shouldThrowIfEquivalentNodeKeyConstraintExist
     // X shouldThrowIfEquivalentExistenceConstraintExist
     // X shouldThrowIfSchemaAlreadyIndexedWhenCreatingNodeKeyConstraint
+    // X shouldThrowIfSchemaAlreadyUniquenessConstrainedWhenCreatingNodeKeyConstraint
+    // X shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingIndex
+    // X shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingUniquenessConstraint
+    // X shouldThrowIfSchemaAlreadyNodeKeyConstrainedWhenCreatingNodeKeyConstraint
 }
