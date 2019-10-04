9e209bd243b59bfefe7ce2813c8860acfc67e0d4
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-10-04 09:20:10 +0200

    Use new error message in test and add case for USE

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnsupportedFeaturesAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnsupportedFeaturesAcceptanceTest.scala
@@ -14,7 +14,12 @@ class UnsupportedFeaturesAcceptanceTest extends ExecutionEngineFunSuite with Cyp
 
   test("from graph") {
     val query = "FROM GRAPH foo.bar MATCH (a)-->() RETURN a"
-    failWithError(Configs.All, query, List("The `FROM GRAPH` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
+    failWithError(Configs.All, query, List("The `FROM GRAPH` clause is not available in this implementation of Cypher due to lack of support for FROM graph selector."))
+  }
+
+  test("use graph") {
+    val query = "USE GRAPH foo.bar MATCH (a)-->() RETURN a"
+    failWithError(Configs.All, query, List("The `USE GRAPH` clause is not available in this implementation of Cypher due to lack of support for USE graph selector."))
   }
 
   test("return graph") {
