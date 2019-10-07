79d6a7645bfc54b8c2d99069a5f3922af2bec15e
olivia <olivia@ytterbrink.com>
2019-10-07 14:44:17 +0200

    Construct privileges given the layout of the graph

--- private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/security/AbstractSecurityBenchmark.scala
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/security/AbstractSecurityBenchmark.scala
@@ -10,10 +10,10 @@ import java.io.IOException
 import com.neo4j.bench.common.Neo4jConfigBuilder
 import com.neo4j.bench.common.model.Neo4jConfig
 import com.neo4j.bench.micro.benchmarks.cypher.AbstractCypherBenchmark
-import com.neo4j.bench.micro.data.DataGeneratorConfig
+import com.neo4j.bench.micro.data.{DataGeneratorConfig, PropertyDefinition, RelationshipDefinition}
 import com.neo4j.server.security.enterprise.auth.EnterpriseAuthAndUserManager
-import com.neo4j.server.security.enterprise.configuration.SecuritySettings
 import org.neo4j.configuration.GraphDatabaseSettings
+import org.neo4j.graphdb.Label
 import org.neo4j.internal.kernel.api.security.LoginContext
 import org.neo4j.kernel.api.exceptions.InvalidArgumentsException
 import org.neo4j.kernel.api.security.AuthToken
@@ -28,30 +28,43 @@ abstract class AbstractSecurityBenchmark extends AbstractCypherBenchmark {
 
   val users: mutable.Map[String, LoginContext] = mutable.Map[String, LoginContext]()
   val neo4jConfig: Neo4jConfig = Neo4jConfigBuilder.empty()
-    .withSetting(SecuritySettings.authentication_providers, SecuritySettings.NATIVE_REALM_NAME)
-    .withSetting(SecuritySettings.authorization_providers, SecuritySettings.NATIVE_REALM_NAME)
-    .withSetting(GraphDatabaseSettings.auth_enabled, "true").build();
+    .withSetting(GraphDatabaseSettings.auth_enabled, "true").build()
 
   override protected def afterDatabaseStart(config: DataGeneratorConfig): Unit = {
     val authManager = db.asInstanceOf[GraphDatabaseAPI].getDependencyResolver.resolveDependency(classOf[EnterpriseAuthAndUserManager])
 
+    val labels: Array[Label] = config.labels()
+    val nodeProperties: Array[PropertyDefinition] = config.nodeProperties()
+    val relTypes: Array[RelationshipDefinition] = config.outRelationships()
+    val relProperties: Array[PropertyDefinition] = config.relationshipProperties()
+
     try {
-      systemDb().executeTransactionally( "CREATE ROLE WhiteRole" );
-      systemDb().executeTransactionally( "GRANT TRAVERSE ON GRAPH * NODES Label TO WhiteRole" );
-      systemDb().executeTransactionally( "GRANT TRAVERSE ON GRAPH * RELATIONSHIPS A,B,C,D,E,F,G,H,I,J,REL TO WhiteRole" );
-      systemDb().executeTransactionally( "GRANT READ {lookup, predicate} ON GRAPH * RELATIONSHIPS A,B,C,D,E,F,G,H,I,J,REL TO WhiteRole" );
+      // Role with explicit privileges to read everything in the graph
+      systemDb().executeTransactionally("CREATE ROLE WhiteRole")
+      labels.foreach { label =>
+        systemDb().executeTransactionally(s"GRANT TRAVERSE ON GRAPH * NODES ${label.name()} TO WhiteRole")
+        nodeProperties.foreach(p => systemDb().executeTransactionally(s"GRANT READ {${p.key()}} ON GRAPH * NODES ${label.name()} TO WhiteRole"))
+      }
 
-      systemDb().executeTransactionally( "CREATE ROLE BlackRole" );
-      systemDb().executeTransactionally( "DENY TRAVERSE ON GRAPH * NODES BlackLabel TO BlackRole" );
-      systemDb().executeTransactionally( "DENY TRAVERSE ON GRAPH * RELATIONSHIPS blackRel TO BlackRole" );
-      systemDb().executeTransactionally( "DENY READ {blackProp} ON GRAPH * RELATIONSHIPS blackRel TO BlackRole" );
+      relTypes.foreach { relDefinition =>
+        val relType = relDefinition.`type`().name()
+        systemDb().executeTransactionally(s"GRANT TRAVERSE ON GRAPH * RELATIONSHIPS $relType TO WhiteRole")
+        relProperties.foreach(p => systemDb().executeTransactionally(s"GRANT READ {${p.key()}} ON GRAPH * RELATIONSHIPS $relType TO WhiteRole"))
+      }
 
-      systemDb().executeTransactionally( "CREATE USER white SET PASSWORD 'abc123' CHANGE NOT REQUIRED" );
-      systemDb().executeTransactionally( "GRANT ROLE WhiteRole TO white" );
+      // Role that denies unused graph elements
+      systemDb().executeTransactionally("CREATE ROLE BlackRole")
+      systemDb().executeTransactionally("DENY TRAVERSE ON GRAPH * ELEMENTS BLACK TO BlackRole")
+      systemDb().executeTransactionally("DENY READ {blackProp} ON GRAPH * ELEMENTS BLACK TO BlackRole")
 
-      systemDb().executeTransactionally( "CREATE USER black SET PASSWORD 'foo' CHANGE NOT REQUIRED" );
-      systemDb().executeTransactionally( "GRANT ROLE WhiteRole TO black" );
-      systemDb().executeTransactionally( "GRANT ROLE BlackRole TO black" );
+      // User with grants
+      systemDb().executeTransactionally("CREATE USER white SET PASSWORD 'abc123' CHANGE NOT REQUIRED")
+      systemDb().executeTransactionally("GRANT ROLE WhiteRole TO white")
+
+      // User with grants and denies
+      systemDb().executeTransactionally("CREATE USER black SET PASSWORD 'foo' CHANGE NOT REQUIRED")
+      systemDb().executeTransactionally("GRANT ROLE WhiteRole TO black")
+      systemDb().executeTransactionally("GRANT ROLE BlackRole TO black")
 
       users += ("white" -> authManager.login(AuthToken.newBasicAuthToken("white", "abc123")))
       users += ("black" -> authManager.login(AuthToken.newBasicAuthToken("black", "foo")))
