60bcdeb80a35d003923090d9b851b1b923fac6c4
Simon Svensson <simpan.svensson@gmail.com>
2019-10-15 11:17:41 +0200

    Added script for moving the ldbc stores to zips

---
+++ private/benchmarks/ldbc/zip-all-stores.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2002-2019 "Neo4j,"
+# Neo4j Sweden AB [http://neo4j.com]
+# This file is part of Neo4j internal tooling.
+#
+
+
+set -e
+set -u
+
+neo4j_version="34"
+
+
+if [[ -z "$JAVA_HOME" ]]; then
+    echo "JAVA_HOME not set, bye, bye"
+fi
+
+dbs=(
+ "db_sf001_p006_regular_utc_${neo4j_version}ce"
+ "db_sf001_p064_regular_utc_${neo4j_version}ce"
+ "db_sf010_p064_regular_utc_${neo4j_version}ce"
+ "db_sf100_p064_dense1_utc_day_${neo4j_version}ce"
+ "db_sf100_p064_dense1_utc_day_${neo4j_version}ee"
+)
+
+working_dir=$(pwd)
+for i in "${dbs[@]}"; do
+
+    # shellcheck disable=SC2206
+    arr=(${i//;/ })
+    db_name=${arr[0]}
+    tar_name="${db_name}".tar.gz
+    echo "Zipping database"
+    echo "DB Name : ${db_name}"
+    echo "targz name : ${tar_name}"
+    aws s3 sync s3://benchmarking.neo4j.com/datasets/ldbc/db/"${db_name}" "${db_name}" --no-progress
+    tar -cvzf "${tar_name}" "${db_name}"
+
+    aws s3 sync "${tar_name}" s3://benchmarking.neo4j.com/datasets/ldbc/db/"${tar_name}" --no-progress --delete
+    rm -rf "${db_name}"
+    rm -rf "${tar_name}"
+done
