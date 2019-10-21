d0f03bb3ccd61d78a1868e210dcf54121b258521
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2019-10-21 13:37:34 +0200

    Do not expose database id from transaction

--- private/enterprise/procedure/src/test/java/com/neo4j/procedure/enterprise/builtin/StubKernelTransaction.java
+++ private/enterprise/procedure/src/test/java/com/neo4j/procedure/enterprise/builtin/StubKernelTransaction.java
@@ -33,7 +33,6 @@ import org.neo4j.internal.schema.constraints.IndexBackedConstraintDescriptor;
 import org.neo4j.kernel.api.KernelTransaction;
 import org.neo4j.kernel.api.Statement;
 import org.neo4j.kernel.api.exceptions.Status;
-import org.neo4j.kernel.database.DatabaseId;
 import org.neo4j.kernel.impl.api.ClockContext;
 import org.neo4j.kernel.impl.coreapi.InternalTransaction;
 
@@ -304,11 +303,5 @@ class StubKernelTransaction implements KernelTransaction
     public boolean isSchemaTransaction()
     {
         return false;
-    }
-
-    @Override
-    public DatabaseId getDatabaseId()
-    {
-        throw new UnsupportedOperationException( "not implemented" );
     }
 }
