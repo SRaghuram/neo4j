53f7ac47f5527a07e4040df981065f6d58238f65
Anton Klaren <anton.klaren@neotechnology.com>
2019-10-22 13:36:00 +0200

    Add setting migrator for auth provider

---
+++ private/enterprise/security/src/main/java/com/neo4j/server/security/enterprise/configuration/SecuritySettingsMigrator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.server.security.enterprise.configuration;
+
+import java.util.Map;
+
+import org.neo4j.annotations.service.ServiceProvider;
+import org.neo4j.configuration.SettingMigrator;
+import org.neo4j.logging.Log;
+
+import static com.neo4j.server.security.enterprise.configuration.SecuritySettings.authentication_providers;
+import static com.neo4j.server.security.enterprise.configuration.SecuritySettings.authorization_providers;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+@ServiceProvider
+public class SecuritySettingsMigrator implements SettingMigrator
+{
+    @Override
+    public void migrate( Map<String,String> values, Map<String,String> defaultValues, Log log )
+    {
+        String value = values.remove( "dbms.security.auth_provider" );
+        if ( isNotBlank( value ) )
+        {
+            log.warn( "Use of deprecated setting dbms.security.auth_provider. It is replaced by " +
+                    "dbms.security.authentication_providers and dbms.security.authorization_providers" );
+            values.putIfAbsent( authentication_providers.name(), value );
+            values.putIfAbsent( authorization_providers.name(), value );
+        }
+    }
+}
---
+++ private/enterprise/security/src/test/java/com/neo4j/server/security/enterprise/configuration/SecuritySettingsMigratorTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.server.security.enterprise.configuration;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.neo4j.configuration.Config;
+
+import static com.neo4j.server.security.enterprise.configuration.SecuritySettings.authentication_providers;
+import static com.neo4j.server.security.enterprise.configuration.SecuritySettings.authorization_providers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SecuritySettingsMigratorTest
+{
+    @Test
+    void testAuthProviderMigrator()
+    {
+        var config = Config.newBuilder().setRaw( Map.of( "dbms.security.auth_provider", "ldap" ) ).build();
+        List<String> authentication = config.get( authentication_providers );
+        List<String> authorization = config.get( authorization_providers );
+        List<String> expected = List.of( "ldap" );
+        assertEquals( expected, authentication );
+        assertEquals( expected, authorization );
+    }
+}
