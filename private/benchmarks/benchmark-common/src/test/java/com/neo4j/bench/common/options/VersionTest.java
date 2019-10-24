f3c595e3b3c5ddb673ad49dc135cc281474a81ac
Simon Svensson <simpan.svensson@gmail.com>
2019-10-24 17:32:47 +0200

    Updated tests to 4.0 infrastructure

--- private/benchmarks/benchmark-common/src/test/java/com/neo4j/bench/common/options/VersionTest.java
+++ private/benchmarks/benchmark-common/src/test/java/com/neo4j/bench/common/options/VersionTest.java
@@ -5,18 +5,14 @@
  */
 package com.neo4j.bench.common.options;
 
-import org.junit.Rule;
+import com.neo4j.bench.common.util.BenchmarkUtil;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class VersionTest
 {
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
-
     @Test
     public void getMainVersion()
     {
@@ -29,22 +25,27 @@ public class VersionTest
     @Test
     public void shouldNotAllowToLongVersions() throws Exception
     {
-        exception.expect( IllegalArgumentException.class );
-        new Version( "3.4.0.0" );
+        BenchmarkUtil.assertException( IllegalArgumentException.class,
+                                       () -> {
+                                           new Version( "3.4.0.0" );
+                                       } );
     }
 
     @Test
     public void shouldNotAllowToShortVersions() throws Exception
     {
-        exception.expect( IllegalArgumentException.class );
-        exception.expectMessage( "Neo4j version have always been on the form x.y.z , but this version is 3.4" );
-        new Version( "3.4" );
+        BenchmarkUtil.assertException( IllegalArgumentException.class,
+                                       () -> {
+                                           new Version( "3.4" );
+                                       } );
     }
 
     @Test
     public void shouldNotAllowToVersionsThatHaveNoneNumberVales() throws Exception
     {
-        exception.expect( IllegalArgumentException.class );
-        new Version( "a.0.0" );
+        BenchmarkUtil.assertException( IllegalArgumentException.class,
+                                       () -> {
+                                           new Version( "a.0.0" );
+                                       } );
     }
 }
