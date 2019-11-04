1c736c5024408e878def08a5c849e10e2412a517
lutovich <konstantin.lutovich@neo4j.com>
2019-11-04 16:47:54 +0100

    Use cluster per method in ClusterFormationIT
    This fixes a problem where the first member from initial discovery member
    settings was killed. It also makes the test easier to reason about.

--- private/enterprise/enterprise-it/causalclustering-it/src/test/java/com/neo4j/causalclustering/scenarios/ClusterFormationIT.java
+++ private/enterprise/enterprise-it/causalclustering-it/src/test/java/com/neo4j/causalclustering/scenarios/ClusterFormationIT.java
@@ -6,42 +6,45 @@
 package com.neo4j.causalclustering.scenarios;
 
 import com.neo4j.causalclustering.common.Cluster;
+import com.neo4j.causalclustering.core.CausalClusteringSettings;
 import com.neo4j.causalclustering.core.CoreClusterMember;
-import com.neo4j.test.causalclustering.ClusterConfig;
 import com.neo4j.test.causalclustering.ClusterExtension;
 import com.neo4j.test.causalclustering.ClusterFactory;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.neo4j.configuration.helpers.SocketAddress;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.internal.helpers.collection.Iterables;
 import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
 import org.neo4j.test.extension.Inject;
 import org.neo4j.test.extension.SkipThreadLeakageGuard;
 
+import static com.neo4j.causalclustering.discovery.InitialDiscoveryMembersResolver.advertisedSocketAddressComparator;
+import static com.neo4j.test.causalclustering.ClusterConfig.clusterConfig;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_METHOD;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 import static org.neo4j.test.assertion.Assert.assertEventually;
 
 @SkipThreadLeakageGuard
 @ClusterExtension
+@TestInstance( PER_METHOD )
 public class ClusterFormationIT
 {
     @Inject
     private ClusterFactory clusterFactory;
 
-    public final ClusterConfig clusterConfig = ClusterConfig.clusterConfig().withNumberOfCoreMembers( 3 ).withNumberOfReadReplicas( 1 );
-
     private Cluster cluster;
 
-    @BeforeAll
+    @BeforeEach
     void setup() throws Exception
     {
-        cluster = clusterFactory.createCluster( clusterConfig );
+        cluster = clusterFactory.createCluster( clusterConfig().withNumberOfCoreMembers( 3 ).withNumberOfReadReplicas( 1 ) );
         cluster.start();
     }
 
@@ -115,7 +118,7 @@ public class ClusterFormationIT
 
         // when
         cluster.shutdown();
-        cluster.startAndUpdateInitialHosts();
+        cluster.start();
 
         // then
         verifyNumberOfCoresReportedByTopology( 3 );
@@ -126,7 +129,7 @@ public class ClusterFormationIT
         cluster.newCoreMember().start();
         cluster.shutdown();
 
-        cluster.startAndUpdateInitialHosts();
+        cluster.start();
 
         // then
         verifyNumberOfCoresReportedByTopology( 3 );
@@ -134,16 +137,32 @@ public class ClusterFormationIT
 
     private CoreClusterMember getExistingCoreMember()
     {
-        return Iterables.last( cluster.coreMembers() );
+        // return the core listed last in discovery members
+        // never return the first core in discovery members because it might be stopped and then Akka cluster can't bootstrap
+        return cluster.coreMembers()
+                .stream()
+                .max( ClusterFormationIT::compareDiscoveryAddresses )
+                .orElseThrow();
     }
 
     private void removeCoreMember()
     {
-        cluster.removeCoreMember( getExistingCoreMember() );
+        var existingCoreMember = getExistingCoreMember();
+        cluster.removeCoreMember( existingCoreMember );
     }
 
     private void verifyNumberOfCoresReportedByTopology( int expected ) throws InterruptedException
     {
         assertEventually( () -> cluster.numberOfCoreMembersReportedByTopology( DEFAULT_DATABASE_NAME ), is( expected ), 30, SECONDS );
+    }
+
+    private static int compareDiscoveryAddresses( CoreClusterMember core1, CoreClusterMember core2 )
+    {
+        return advertisedSocketAddressComparator().compare( discoveryAddress( core1 ), discoveryAddress( core2 ) );
+    }
+
+    private static SocketAddress discoveryAddress( CoreClusterMember core )
+    {
+        return core.config().get( CausalClusteringSettings.discovery_advertised_address );
     }
 }
