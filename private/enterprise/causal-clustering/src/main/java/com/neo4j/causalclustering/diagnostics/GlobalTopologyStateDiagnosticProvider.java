79d3b2b574ce576b294d2437c670dee8676ce8a4
RagnarW <ragnar.wernersson@neotechnology.com>
2019-11-28 15:32:41 +0100

    Add diagnostics for Global topology state
    When ever a log is rotated, the current topology will now be logged,
    including full info of all instances. This way we can strip down the
    logging of state updates when they

---
+++ private/enterprise/causal-clustering/src/main/java/com/neo4j/causalclustering/diagnostics/GlobalTopologyStateDiagnosticProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.causalclustering.diagnostics;
+
+import com.neo4j.causalclustering.discovery.TopologyService;
+
+import org.neo4j.internal.diagnostics.DiagnosticsProvider;
+import org.neo4j.logging.Logger;
+
+import static java.lang.System.lineSeparator;
+import static org.neo4j.internal.helpers.Strings.printMap;
+
+public class GlobalTopologyStateDiagnosticProvider implements DiagnosticsProvider
+{
+
+    private final TopologyService topologyService;
+
+    public GlobalTopologyStateDiagnosticProvider( TopologyService topologyService )
+    {
+        this.topologyService = topologyService;
+    }
+
+    @Override
+    public String getDiagnosticsName()
+    {
+        return "Global topology state";
+    }
+
+    @Override
+    public void dump( Logger logger )
+    {
+        logger.log( "Current core topology:%s%s", newPaddedLIne(), printMap( topologyService.allCoreServers(), newPaddedLIne() ) );
+        logger.log( "Current read replica topology:%s%s", newPaddedLIne(), printMap( topologyService.allReadReplicas(), newPaddedLIne() ) );
+    }
+
+    private static String newPaddedLIne()
+    {
+        return lineSeparator() + "  ";
+    }
+}
