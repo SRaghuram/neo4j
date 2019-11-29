4ee9dd22a570bcd1c3c4b96e74556ac6f329c759
Chris Vest <mr.chrisvest@gmail.com>
2019-11-29 15:18:30 +0100

    Add tests that thoroughly verifies the correctness of our *Record.clone methods.

---
+++ private/enterprise/record-storage-engine/src/test/java/com/neo4j/kernel/impl/store/format/highlimit/HighLimitRecordCloningTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.kernel.impl.store.format.highlimit;
+
+import org.neo4j.kernel.impl.store.format.AbstractRecordCloningTest;
+import org.neo4j.kernel.impl.store.format.RecordFormats;
+
+public class HighLimitRecordCloningTest extends AbstractRecordCloningTest
+{
+    @Override
+    protected RecordFormats formats()
+    {
+        return HighLimit.RECORD_FORMATS;
+    }
+
+    @Override
+    protected int entityBits()
+    {
+        return 50;
+    }
+
+    @Override
+    protected int propertyBits()
+    {
+        return 50;
+    }
+}
