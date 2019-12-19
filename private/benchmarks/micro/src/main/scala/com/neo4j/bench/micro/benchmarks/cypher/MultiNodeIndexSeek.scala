59cd2562c1b792deb04cbfb37e349608b5706ebd
Henrik Nyman <henrik.nyman@neotechnology.com>
2019-12-19 16:18:49 +0100

    Add micro benchmark

---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/MultiNodeIndexSeek.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.jmh.api.config.{BenchmarkEnabled, ParamValues}
+import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.data.DiscreteGenerator.{Bucket, discrete}
+import com.neo4j.bench.micro.data.Plans.{IdGen, astLabelToken, astLiteralFor, astPropertyKeyToken, astVariable}
+import com.neo4j.bench.micro.data.TypeParamValues.{DBL, LNG, STR_BIG, STR_SML}
+import com.neo4j.bench.micro.data.ValueGeneratorUtil.discreteBucketsFor
+import com.neo4j.bench.micro.data.{DataGeneratorConfig, DataGeneratorConfigBuilder, LabelKeyDefinition, PropertyDefinition}
+import org.neo4j.cypher.internal.logical.plans
+import org.neo4j.cypher.internal.logical.plans.{DoNotGetValue, IndexOrderNone, IndexedProperty, SingleQueryExpression}
+import org.neo4j.cypher.internal.planner.spi.PlanContext
+import org.neo4j.cypher.internal.v4_0.ast.semantics.SemanticTable
+import org.neo4j.graphdb.Label
+import org.neo4j.kernel.impl.coreapi.InternalTransaction
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+
+@BenchmarkEnabled(false)
+class MultiNodeIndexSeek extends AbstractCypherBenchmark {
+  @ParamValues(
+    allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, Slotted.NAME, Morsel.NAME),
+    base = Array(Slotted.NAME, Morsel.NAME))
+  @Param(Array[String]())
+  var runtime: String = _
+
+  @ParamValues(
+    allowed = Array("0.001", "0.01", "0.1"),
+    base = Array("0.001", "0.1"))
+  @Param(Array[String]())
+  var selectivity: Double = _
+
+  @ParamValues(
+    allowed = Array(LNG, DBL, STR_SML, STR_BIG),
+    base = Array(LNG, STR_SML))
+  @Param(Array[String]())
+  var propertyType: String = _
+
+  override def description = "Cartesian Product of Multiple Index Seeks"
+
+  private val NODE_COUNT = 1000000
+  private val LABEL = Label.label("SampleLabel")
+  private val KEY = "key"
+
+  private val TOLERATED_ROW_COUNT_ERROR = 0.05
+  // TODO: Maybe use private vars and afterDatabaseRestart() to set these, since they are accessed on each invocation
+  private lazy val expectedRowCount: Double = NODE_COUNT * selectivity
+  private lazy val minExpectedRowCount: Int = Math.round(expectedRowCount - TOLERATED_ROW_COUNT_ERROR * expectedRowCount).toInt
+  private lazy val maxExpectedRowCount: Int = Math.round(expectedRowCount + TOLERATED_ROW_COUNT_ERROR * expectedRowCount).toInt
+
+  private lazy val buckets: Array[Bucket] = discreteBucketsFor(propertyType, selectivity, 1 - selectivity)
+
+  override protected def getConfig: DataGeneratorConfig =
+    new DataGeneratorConfigBuilder()
+      .withNodeCount(NODE_COUNT)
+      .withLabels(LABEL)
+      .withNodeProperties(new PropertyDefinition(KEY, discrete(buckets: _*)))
+      .withSchemaIndexes(new LabelKeyDefinition(LABEL, KEY))
+      .isReusableStore(true)
+      .build()
+
+  override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
+    val a = "a"
+    val b = "b"
+    val literal = astLiteralFor(buckets(0), propertyType)
+    val seekExpression = SingleQueryExpression(literal)
+    val indexSeekA = plans.NodeIndexSeek(
+      a,
+      astLabelToken(LABEL, planContext),
+      Seq(IndexedProperty(astPropertyKeyToken(KEY, planContext), DoNotGetValue)),
+      seekExpression,
+      Set.empty,
+      IndexOrderNone)(IdGen)
+    val indexSeekB = plans.NodeIndexSeek(
+      b,
+      astLabelToken(LABEL, planContext),
+      Seq(IndexedProperty(astPropertyKeyToken(KEY, planContext), DoNotGetValue)),
+      seekExpression,
+      Set.empty,
+      IndexOrderNone)(IdGen)
+
+    val cartesianProduct = plans.CartesianProduct(indexSeekA, indexSeekB)(IdGen)
+    val resultColumns = List(a, b)
+    val produceResults = plans.ProduceResult(cartesianProduct, columns = resultColumns)(IdGen)
+
+    val table = SemanticTable().addNode(astVariable(a)).addNode(astVariable(b))
+
+    (produceResults, table, resultColumns)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SampleTime))
+  def executePlan(threadState: MultipleNodeIndexSeekThreadState, bh: Blackhole): Long = {
+    val subscriber = new CountSubscriber(bh)
+    val result = threadState.executablePlan.execute(tx = threadState.tx, subscriber = subscriber)
+    result.consumeAll()
+    assertExpectedRowCount(minExpectedRowCount, maxExpectedRowCount, subscriber)
+  }
+}
+
+@State(Scope.Thread)
+class MultipleNodeIndexSeekThreadState {
+  var tx: InternalTransaction = _
+  var executablePlan: ExecutablePlan = _
+
+  @Setup
+  def setUp(benchmarkState: CartesianProduct): Unit = {
+    executablePlan = benchmarkState.buildPlan(from(benchmarkState.runtime))
+    tx = benchmarkState.beginInternalTransaction()
+  }
+
+  @TearDown
+  def tearDown(): Unit = {
+    tx.close()
+  }
+}
