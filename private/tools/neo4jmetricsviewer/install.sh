956bae46191acd66cf3a51bb12d99751fea54c3d
roxling <valdemar.roxling@neotechnology.com>
2019-12-20 15:52:59 +0100

    Improved grafana panel, dashboard url + cleared up some scripts & readme
    Also added .gitignore in packaging folder

--- private/tools/neo4jmetricsviewer/install.sh
+++ private/tools/neo4jmetricsviewer/install.sh
@@ -1,3 +1,4 @@
+echo 'Building the docker image may take some time'
 docker image build -t neo4j-metrics-viewer:1.0 . -q
 docker save neo4j-metrics-viewer | gzip > packaging/neo4j-metrics-viewer.tar.gz
 echo 'Created docker image at packaging/neo4j-metrics-viewer.tar.gz'
--- private/tools/neo4jmetricsviewer/packaging/run.sh
+++ private/tools/neo4jmetricsviewer/packaging/run.sh
@@ -5,4 +5,4 @@ docker cp metrics neo4j-metrics-viewer:/src/
 docker exec neo4j-metrics-viewer node builddashboards.js --loglevel=error
 docker exec neo4j-metrics-viewer npm -s install fastify@2.11.0 csvtojson@2.0.10  &> /dev/null
 docker exec neo4j-metrics-viewer node --max_old_space_size=8000 csvtojsonserver.js --loglevel=error &
-echo 'Grafana should be available at http://localhost:3000/'
+echo 'Grafana should be available at http://localhost:3000/d/neo4j-metrics/'
