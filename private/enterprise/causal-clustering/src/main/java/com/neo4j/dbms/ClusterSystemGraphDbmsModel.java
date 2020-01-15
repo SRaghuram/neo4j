7eb1178a5d68a15648ffc949f7e8253636f286d6
Hugo Firth <hugo.firth@neo4j.com>
2020-01-15 15:00:13 +0000

    Remove system graph model NPE
        - If a databases desired state is set to DROPPED during
    bootstrapping then DatabaseStartAborter may throw an NPE.
        - This is due to the system graph query the aborter executes only
    checking for nodes with the Database label. The query should also allow
    the DeletedDatabase label.

--- private/enterprise/causal-clustering/src/main/java/com/neo4j/dbms/ClusterSystemGraphDbmsModel.java
+++ private/enterprise/causal-clustering/src/main/java/com/neo4j/dbms/ClusterSystemGraphDbmsModel.java
@@ -6,6 +6,7 @@
 package com.neo4j.dbms;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -13,6 +14,7 @@ import java.util.function.Supplier;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.NotFoundException;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.database.NamedDatabaseId;
 import org.neo4j.kernel.impl.store.MetaDataStore;
 import org.neo4j.storageengine.api.StoreId;
@@ -39,7 +41,8 @@ public class ClusterSystemGraphDbmsModel extends EnterpriseSystemGraphDbmsModel
     {
         try ( var tx = systemDatabase.get().beginTx() )
         {
-            Node node = tx.findNode( DATABASE_LABEL, DATABASE_UUID_PROPERTY, namedDatabaseId.databaseId().uuid().toString() );
+            var node = databaseNode( namedDatabaseId, tx );
+
             String[] initialMembers = (String[]) node.getProperty( INITIAL_MEMBERS );
             return Arrays.stream( initialMembers )
                          .map( UUID::fromString )
@@ -55,11 +58,18 @@ public class ClusterSystemGraphDbmsModel extends EnterpriseSystemGraphDbmsModel
     {
         try ( var tx = systemDatabase.get().beginTx() )
         {
-            Node node = tx.findNode( DATABASE_LABEL, DATABASE_UUID_PROPERTY, namedDatabaseId.databaseId().uuid().toString() );
+            var node = databaseNode( namedDatabaseId, tx );
             long creationTime = (long) node.getProperty( STORE_CREATION_TIME );
             long randomId = (long) node.getProperty( STORE_RANDOM_ID );
             String storeVersion = (String) node.getProperty( STORE_VERSION );
             return new StoreId( creationTime, randomId, MetaDataStore.versionStringToLong( storeVersion ) );
         }
+    }
+
+    private Node databaseNode( NamedDatabaseId databaseId, Transaction tx )
+    {
+        return Optional.ofNullable( tx.findNode( DATABASE_LABEL, DATABASE_UUID_PROPERTY, databaseId.databaseId().uuid().toString() ) )
+                .or( () -> Optional.ofNullable( tx.findNode( DELETED_DATABASE_LABEL, DATABASE_UUID_PROPERTY, databaseId.databaseId().uuid().toString() ) ) )
+                .orElseThrow( () -> new IllegalStateException( "System database must contain a node for database " + databaseId.name() ) );
     }
 }
