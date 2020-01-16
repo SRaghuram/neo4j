63aa2f78880ad43540b42aae943b924798a40fe2
roxling <valdemar.roxling@neotechnology.com>
2020-01-16 11:48:34 +0100

    Implemented relationships r/w (no properties)

--- public/community/freki-storage-engine/src/main/java/org/neo4j/internal/freki/FrekiNodeCursor.java
+++ public/community/freki-storage-engine/src/main/java/org/neo4j/internal/freki/FrekiNodeCursor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
@@ -124,7 +124,7 @@ class FrekiNodeCursor implements StorageNodeCursor
     @Override
     public long entityReference()
     {
-        return 0;
+        return record.id;
     }
 
     @Override
