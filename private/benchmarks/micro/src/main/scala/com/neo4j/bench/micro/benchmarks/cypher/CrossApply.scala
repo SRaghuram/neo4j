a9a943124441e10f7352c0516697827f3e67ee30
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2020-01-20 10:45:00 +0100

    Add microbenchmark for cross apply

---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/CrossApply.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.jmh.api.config.BenchmarkEnabled
+import com.neo4j.bench.jmh.api.config.ParamValues
+import com.neo4j.bench.micro.Main
+import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.data.Plans.IdGen
+import com.neo4j.bench.micro.data.Plans.astVariable
+import com.neo4j.bench.micro.data.DataGeneratorConfig
+import com.neo4j.bench.micro.data.DataGeneratorConfigBuilder
+import org.neo4j.cypher.internal.logical.plans
+import org.neo4j.cypher.internal.planner.spi.PlanContext
+import org.neo4j.cypher.internal.v4_0.ast.semantics.SemanticTable
+import org.neo4j.kernel.impl.coreapi.InternalTransaction
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+
+@BenchmarkEnabled(true)
+class CrossApply extends AbstractCypherBenchmark {
+  @ParamValues(
+    allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, Slotted.NAME, Morsel.NAME),
+    base = Array(Slotted.NAME))
+  @Param(Array[String]())
+  var runtime: String = _
+
+  @ParamValues(
+    allowed = Array("1", "10", "100", "1000"),
+    base = Array("1", "1000"))
+  @Param(Array[String]())
+  var rows: Int = _
+
+  override def description = "Cross Apply"
+
+  private var expectedRowCount: Int = _
+
+  override protected def getConfig: DataGeneratorConfig =
+    new DataGeneratorConfigBuilder()
+      .withNodeCount(rows)
+      .isReusableStore(true)
+      .build()
+
+  override protected def afterDatabaseStart(config: DataGeneratorConfig): Unit = {
+    expectedRowCount = rows * rows
+  }
+
+  override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
+    val a = "a"
+    val b = "b"
+    val allNodesScanA = plans.AllNodesScan(a, Set.empty)(IdGen)
+    val allNodesScanB = plans.AllNodesScan(b, Set(a))(IdGen)
+    val crossApply = plans.CrossApply(allNodesScanA, allNodesScanB)(IdGen)
+    val resultColumns = List(a, b)
+    val produceResults = plans.ProduceResult(crossApply, columns = resultColumns)(IdGen)
+
+    val table = SemanticTable().addNode(astVariable(a)).addNode(astVariable(b))
+
+    (produceResults, table, resultColumns)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SampleTime))
+  def executePlan(threadState: CrossApplyThreadState, bh: Blackhole): Long = {
+    val subscriber = new CountSubscriber(bh)
+    val result = threadState.executablePlan.execute(tx = threadState.tx, subscriber = subscriber)
+    result.consumeAll()
+    assertExpectedRowCount(expectedRowCount, subscriber)
+  }
+}
+
+@State(Scope.Thread)
+class CrossApplyThreadState {
+  var tx: InternalTransaction = _
+  var executablePlan: ExecutablePlan = _
+
+  @Setup
+  def setUp(benchmarkState: CrossApply): Unit = {
+    executablePlan = benchmarkState.buildPlan(from(benchmarkState.runtime))
+    tx = benchmarkState.beginInternalTransaction()
+  }
+
+  @TearDown
+  def tearDown(): Unit = {
+    tx.close()
+  }
+}
