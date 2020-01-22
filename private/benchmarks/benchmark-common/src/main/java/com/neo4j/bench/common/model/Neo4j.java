4f59ac9d032a466188787d77c09e757f961a0096
Alex Averbuch <alex.averbuch@gmail.com>
2020-01-22 17:14:27 +0200

    remove unused method from model.Neo4j

--- private/benchmarks/benchmark-common/src/main/java/com/neo4j/bench/common/model/Neo4j.java
+++ private/benchmarks/benchmark-common/src/main/java/com/neo4j/bench/common/model/Neo4j.java
@@ -7,30 +7,8 @@ package com.neo4j.bench.common.model;
 
 import com.neo4j.bench.common.options.Edition;
 
-import java.util.Map;
-
 public class Neo4j extends Project
 {
-    private Neo4j()
-    {
-        super();
-    }
-
-    public static Neo4j fromCypherMap( Map<String,Object> map )
-    {
-        Repository repository = Repository.forName( (String) map.get( NAME ) );
-        if ( !repository.equals( Repository.NEO4J ) )
-        {
-            throw new RuntimeException( "Tried to create a Neo4j model instance with repository: " + repository );
-        }
-        String commit = (String) map.get( COMMIT );
-        String version = (String) map.get( VERSION );
-        Edition edition = Edition.valueOf( ((String) map.get( EDITION )).toUpperCase() );
-        String branch = (String) map.get( BRANCH );
-        String owner = (String) map.get( OWNER );
-        return new Neo4j( commit, version, edition, branch, owner );
-    }
-
     public Neo4j( String commit, String version, Edition edition, String branch, String owner )
     {
         super( Repository.NEO4J, commit, version, edition, branch, owner );
