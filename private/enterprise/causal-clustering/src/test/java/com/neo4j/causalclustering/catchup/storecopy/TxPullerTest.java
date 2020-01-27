43849a2406381c6bfe2a7b176377009ab494a750
Balazs Lendvai <56266523+gfx54b@users.noreply.github.com>
2020-01-27 15:48:08 +0100

    fixing logging in generic exception catch thrown by pullTransactions (#4517)

--- private/enterprise/causal-clustering/src/test/java/com/neo4j/causalclustering/catchup/storecopy/TxPullerTest.java
+++ private/enterprise/causal-clustering/src/test/java/com/neo4j/causalclustering/catchup/storecopy/TxPullerTest.java
@@ -25,8 +25,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.neo4j.kernel.database.NamedDatabaseId;
 import org.neo4j.kernel.database.TestDatabaseIdRepository;
-import org.neo4j.logging.LogProvider;
-import org.neo4j.logging.NullLogProvider;
+import org.neo4j.logging.AssertableLogProvider;
 import org.neo4j.storageengine.api.StoreId;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -40,13 +39,15 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.neo4j.logging.AssertableLogProvider.Level.WARN;
+import static org.neo4j.logging.LogAssertions.assertThat;
 
 class TxPullerTest
 {
     private static final NamedDatabaseId DATABASE_ID = TestDatabaseIdRepository.randomNamedDatabaseId();
 
     private static final int MAX_FAILED_TX_PULL_REQUESTS = 5;
-    private final LogProvider logProvider = NullLogProvider.getInstance();
+    private final AssertableLogProvider logProvider = new AssertableLogProvider();
     private TransactionLogCatchUpWriter writer;
     private CatchupAddressProvider addressProvider;
     private TxPullClient client;
@@ -268,5 +269,21 @@ class TxPullerTest
         {
             count = 0;
         }
+    }
+
+    @Test
+    void shouldThrowAfterPullTransactionsGenericException() throws Exception
+    {
+        RequiredTransactions requiredRange = RequiredTransactions.requiredRange( 0, 1 );
+        TxPullRequestContext context = getContext( requiredRange );
+
+        NullPointerException npe = new NullPointerException(  );
+
+        when( client.pullTransactions( any(), any(), anyLong(), any() ) ).thenThrow( npe );
+
+        assertThrows( StoreCopyFailedException.class, () -> executor.pullTransactions( context, writer, client ) );
+
+        assertThat( logProvider ).forClass( TxPuller.class )
+                .forLevel( WARN ).containsMessages( "Unexpected exception when pulling transactions" ).containsException(npe);
     }
 }
