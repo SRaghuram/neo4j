8e0cf0fc5ed4fdd93c8fb1197d138e2e8623abff
Chris Vest <mr.chrisvest@gmail.com>
2020-02-07 11:39:26 +0100

    Make it possible for @DbmsExtension based tests to restart the DBMS or their database.

---
+++ private/enterprise/enterprise-it/it-test-support/src/main/java/com/neo4j/test/extension/EnterpriseDbmsSupportController.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package com.neo4j.test.extension;
+
+import com.neo4j.test.TestEnterpriseDatabaseManagementServiceBuilder;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.neo4j.collection.Dependencies;
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.DbmsController;
+import org.neo4j.test.extension.DbmsSupportController;
+
+public class EnterpriseDbmsSupportController extends DbmsSupportController
+{
+    public EnterpriseDbmsSupportController( ExtensionContext context )
+    {
+        super( context );
+    }
+
+    @Override
+    public void startDbms() throws Exception
+    {
+        // Create and manage TestDirectoryExtension our self
+        // The caveat is that the order of postProcessTestInstance and beforeAll changes if you change TestInstance.Lifecycle
+        injectManagedTestDirectory();
+
+        // Find closest configuration
+        TestConfiguration enterpriseDbmsExtension = getAnnotatedConfiguration(
+                getTestAnnotation( EnterpriseDbmsExtension.class ),
+                getTestAnnotation( ImpermanentEnterpriseDbmsExtension.class ) );
+
+        // Create service
+        DatabaseManagementService dbms = buildDbms( enterpriseDbmsExtension.configurationCallback );
+
+        var dbmsDependency = new Dependencies();
+        dbmsDependency.satisfyDependencies( dbms );
+        injectDependencies( dbmsDependency );
+    }
+
+    @Override
+    public TestDatabaseManagementServiceBuilder createBuilder( File homeDirectory, FileSystemAbstraction fileSystem )
+    {
+        return new TestEnterpriseDatabaseManagementServiceBuilder( homeDirectory ).setFileSystem( fileSystem );
+    }
+
+    @Override
+    public void shutdown()
+    {
+        super.shutdown();
+        removeManagedTestDirectory();
+    }
+
+    @Override
+    public DbmsController asDbmsController()
+    {
+        return this::restartDatabase;
+    }
+
+    @SafeVarargs
+    private static TestConfiguration getAnnotatedConfiguration( Optional<? extends Annotation>... options )
+    {
+        Annotation[] annotations = Arrays.stream( options ).flatMap( Optional::stream ).toArray( Annotation[]::new );
+
+        if ( annotations.length > 1 )
+        {
+            throw new IllegalArgumentException( "Multiple DBMS annotations found for the configuration: " + Arrays.toString( annotations ) + "." );
+        }
+
+        if ( annotations.length == 1 )
+        {
+            if ( annotations[0] instanceof EnterpriseDbmsExtension )
+            {
+                EnterpriseDbmsExtension annotation = (EnterpriseDbmsExtension) annotations[0];
+                return new TestConfiguration( annotation.configurationCallback() );
+            }
+            if ( annotations[0] instanceof ImpermanentEnterpriseDbmsExtension )
+            {
+                ImpermanentEnterpriseDbmsExtension annotation = (ImpermanentEnterpriseDbmsExtension) annotations[0];
+                return new TestConfiguration( annotation.configurationCallback() );
+            }
+        }
+
+        throw new IllegalArgumentException( String.format( "No annotation of type \"%s\" or \"%s\" found.",
+                EnterpriseDbmsExtension.class.getSimpleName(), ImpermanentEnterpriseDbmsExtension.class.getSimpleName() )  );
+    }
+
+    private static class TestConfiguration
+    {
+        private final String configurationCallback;
+
+        private TestConfiguration( String configurationCallback )
+        {
+            this.configurationCallback = configurationCallback;
+        }
+    }
+}
--- private/enterprise/enterprise-it/it-test-support/src/main/java/com/neo4j/test/extension/EnterpriseDbmsSupportExtension.java
+++ private/enterprise/enterprise-it/it-test-support/src/main/java/com/neo4j/test/extension/EnterpriseDbmsSupportExtension.java
@@ -5,7 +5,6 @@
  */
 package com.neo4j.test.extension;
 
-import com.neo4j.test.TestEnterpriseDatabaseManagementServiceBuilder;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
@@ -13,82 +12,37 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import org.neo4j.collection.Dependencies;
-import org.neo4j.dbms.api.DatabaseManagementService;
-import org.neo4j.io.fs.FileSystemAbstraction;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
-import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.DbmsSupportController;
 import org.neo4j.test.extension.DbmsSupportExtension;
-import org.neo4j.test.extension.testdirectory.TestDirectorySupportExtension;
-import org.neo4j.test.rule.TestDirectory;
-
 
 public class EnterpriseDbmsSupportExtension extends DbmsSupportExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback
 {
-    private static final String TEST_DIRECTORY_EXTENSION_KEY = "testDirectoryExtension";
-
     @Override
     public void beforeAll( ExtensionContext context ) throws Exception
     {
-        Object testInstance = context.getRequiredTestInstance();
-
-        // Create and manage TestDirectoryExtension our self
-        // The caveat is that the order of postProcessTestInstance and beforeAll changes if you change TestInstance.Lifecycle
-        TestDirectorySupportExtension testDirectorySupportExtension = new TestDirectorySupportExtension();
-        testDirectorySupportExtension.postProcessTestInstance( testInstance, context );
-        testDirectorySupportExtension.prepare( context );
-        getStore( context ).put( TEST_DIRECTORY_EXTENSION_KEY, testDirectorySupportExtension );
-
-        TestDirectory testDirectory = getTestDirectory( context );
-        FileSystemAbstraction fileSystem = testDirectory.getFileSystem();
-
-        // Find closest configuration
-        TestConfiguration enterpriseDbmsExtension = getAnnotatedConfiguration( context );
-
-        // Create service
-        TestDatabaseManagementServiceBuilder builder =
-                new TestEnterpriseDatabaseManagementServiceBuilder( testDirectory.homeDir() ).setFileSystem( fileSystem );
-        maybeInvokeCallback( testInstance, builder, enterpriseDbmsExtension.configurationCallback );
-        DatabaseManagementService dbms = builder.build();
-
-        // Save in context
-        ExtensionContext.Store store = getStore( context );
-        store.put( DBMS, dbms );
-        var dbmsDependency = new Dependencies();
-        dbmsDependency.satisfyDependencies( dbms );
-        injectInstance( testInstance, lookupInjectableFields( testInstance ), dbmsDependency );
+        EnterpriseDbmsSupportController controller = new EnterpriseDbmsSupportController( context );
+        controller.startDbms();
     }
 
     @Override
     public void afterAll( ExtensionContext context )
     {
-        DatabaseManagementService dbms = getStore( context ).remove( DBMS, DatabaseManagementService.class );
-        dbms.shutdown();
-
-        TestDirectorySupportExtension testDirectorySupportExtension =
-                getStore( context ).remove( TEST_DIRECTORY_EXTENSION_KEY, TestDirectorySupportExtension.class );
-        testDirectorySupportExtension.afterEach( context );
+        DbmsSupportController.remove( context ).shutdown();
     }
 
     @Override
     public void beforeEach( ExtensionContext context )
     {
         // Create a new database for each test method
-        DatabaseManagementService dbms = getDbmsFromStore( context );
+        DbmsSupportController controller = DbmsSupportController.get( context );
         String uniqueTestName = getUniqueTestName( context );
-        dbms.createDatabase( uniqueTestName );
-        GraphDatabaseAPI db = (GraphDatabaseAPI) dbms.database( uniqueTestName );
-
-        // Inject db
-        Object testInstance = context.getRequiredTestInstance();
-        injectInstance( testInstance, lookupInjectableFields( testInstance ), db.getDependencyResolver() );
+        controller.startDatabase( uniqueTestName );
     }
 
     @Override
     public void afterEach( ExtensionContext context )
     {
-        DatabaseManagementService dbms = getDbmsFromStore( context );
-        dbms.shutdownDatabase( getUniqueTestName( context ) );
+        DbmsSupportController.get( context ).stopDatabase();
     }
 
     private static String getUniqueTestName( ExtensionContext context )
@@ -97,46 +51,5 @@ public class EnterpriseDbmsSupportExtension extends DbmsSupportExtension impleme
         String testName = context.getRequiredTestMethod().getName();
         testName = testName.substring( 0, Math.min( 31, testName.length() ) );
         return testName + DigestUtils.md5Hex( context.getUniqueId() );
-    }
-
-    private static DatabaseManagementService getDbmsFromStore( ExtensionContext context )
-    {
-        return getStore( context ).get( DBMS, DatabaseManagementService.class );
-    }
-
-    private static TestConfiguration getAnnotatedConfiguration( ExtensionContext context )
-    {
-        EnterpriseDbmsExtension enterpriseDbmsExtension =
-                context.getRequiredTestClass().getAnnotation( EnterpriseDbmsExtension.class );
-        ImpermanentEnterpriseDbmsExtension impermanentEnterpriseDbmsExtension =
-                context.getRequiredTestClass().getAnnotation( ImpermanentEnterpriseDbmsExtension.class );
-        if ( enterpriseDbmsExtension == null && impermanentEnterpriseDbmsExtension == null )
-        {
-            throw new IllegalArgumentException( String.format( "No annotation of type \"%s\" or \"%s\" found.", EnterpriseDbmsExtension.class.getSimpleName(),
-                    ImpermanentEnterpriseDbmsExtension.class.getSimpleName() )  );
-        }
-
-        if ( enterpriseDbmsExtension != null && impermanentEnterpriseDbmsExtension != null )
-        {
-            throw new IllegalArgumentException( String.format( "Can't mix \"%s\" and \"%s\" annotations.", EnterpriseDbmsExtension.class.getSimpleName(),
-                    ImpermanentEnterpriseDbmsExtension.class.getSimpleName() ) );
-        }
-
-        if ( enterpriseDbmsExtension != null )
-        {
-            return new TestConfiguration( enterpriseDbmsExtension.configurationCallback() );
-        }
-
-        return new TestConfiguration( impermanentEnterpriseDbmsExtension.configurationCallback() );
-    }
-
-    private static class TestConfiguration
-    {
-        private final String configurationCallback;
-
-        private TestConfiguration( String configurationCallback )
-        {
-            this.configurationCallback = configurationCallback;
-        }
     }
 }
