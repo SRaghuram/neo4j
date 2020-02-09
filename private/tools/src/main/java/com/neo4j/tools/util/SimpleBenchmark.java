906e246397ecd40f159b4f17aca78a1fb5640ebe
Mattias Finné <mattias@neotechnology.com>
2020-02-09 15:59:46 +0100

    Benchmark runs on cursor level additionally

--- private/tools/src/main/java/com/neo4j/tools/util/SimpleBenchmark.java
+++ private/tools/src/main/java/com/neo4j/tools/util/SimpleBenchmark.java
@@ -13,7 +13,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.function.BiFunction;
 
+import org.neo4j.common.DependencyResolver;
 import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -22,13 +24,30 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.kernel.api.NodeCursor;
+import org.neo4j.internal.kernel.api.PropertyCursor;
+import org.neo4j.internal.kernel.api.RelationshipTraversalCursor;
+import org.neo4j.internal.kernel.api.exceptions.TransactionFailureException;
+import org.neo4j.internal.kernel.api.security.LoginContext;
 import org.neo4j.io.ByteUnit;
 import org.neo4j.io.fs.FileUtils;
+import org.neo4j.kernel.api.Kernel;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.storageengine.api.StorageEngine;
+import org.neo4j.storageengine.api.StorageNodeCursor;
+import org.neo4j.storageengine.api.StoragePropertyCursor;
+import org.neo4j.storageengine.api.StorageReader;
+import org.neo4j.storageengine.api.StorageRelationshipTraversalCursor;
 import org.neo4j.time.Stopwatch;
+import org.neo4j.token.TokenHolders;
+import org.neo4j.values.storable.IntValue;
 
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 import static org.neo4j.configuration.GraphDatabaseSettings.pagecache_memory;
 import static org.neo4j.configuration.GraphDatabaseSettings.tx_state_max_off_heap_memory;
+import static org.neo4j.io.pagecache.tracing.cursor.PageCursorTracer.NULL;
+import static org.neo4j.storageengine.api.RelationshipSelection.ALL_RELATIONSHIPS;
 
 public class SimpleBenchmark
 {
@@ -44,10 +63,10 @@ public class SimpleBenchmark
         {
             GraphDatabaseService db = dbms.database( DEFAULT_DATABASE_NAME );
             createData( db, 0xBEEF );
-            for ( int i = 0; i < 5; i++ )
-            {
-                runBenchmark( db, 0xDEAD + i );
-            }
+            long baseBenchmarkSeed = 0xDEAD;
+            runBenchmarkACoupleOfTimes( "Core API", db, baseBenchmarkSeed, SimpleBenchmark::runBenchmark );
+            runBenchmarkACoupleOfTimes( "Kernel Cursors", db, baseBenchmarkSeed, SimpleBenchmark::runBenchmarkWithKernelCursors );
+            runBenchmarkACoupleOfTimes( "Storage Cursors", db, baseBenchmarkSeed, SimpleBenchmark::runBenchmarkWithStorageCursors );
         }
         catch ( Throwable t )
         {
@@ -57,6 +76,19 @@ public class SimpleBenchmark
         finally
         {
             dbms.shutdown();
+        }
+    }
+
+    private static void runBenchmarkACoupleOfTimes( String name, GraphDatabaseService db, long baseBenchmarkSeed,
+            BiFunction<GraphDatabaseService,Random,Long> benchmark )
+    {
+        for ( int i = 0; i < 5; i++ )
+        {
+            long seed = baseBenchmarkSeed + i;
+            System.out.println( "Benchmark '" + name + "' started using seed: " + seed );
+            Stopwatch time = Stopwatch.start();
+            long distance = benchmark.apply( db, new Random( seed ) );
+            System.out.println( "Benchmark '" + name + "' ended. Distance traveled " + distance + " Time: " + time.elapsed() + " Hops: " + TRAVEL_HOPS );
         }
     }
 
@@ -112,11 +144,8 @@ public class SimpleBenchmark
         System.out.println( "Data creation finished" );
     }
 
-    private static void runBenchmark( GraphDatabaseService db, int seed )
+    private static long runBenchmark( GraphDatabaseService db, Random random )
     {
-        System.out.println( "Benchmark started using seed: " + seed );
-        Random random = new Random( seed );
-        Stopwatch time = Stopwatch.start();
         long distance = 0;
         try ( Transaction tx = db.beginTx() )
         {
@@ -134,6 +163,105 @@ public class SimpleBenchmark
             }
             tx.commit();
         }
-        System.out.println( "Benchmark ended. Distance traveled " + distance + " Time: " + time.elapsed() + " Hops: " + TRAVEL_HOPS );
+        return distance;
+    }
+
+    private static long runBenchmarkWithKernelCursors( GraphDatabaseService db, Random random )
+    {
+        long distance = 0;
+        DependencyResolver deps = ((GraphDatabaseAPI) db).getDependencyResolver();
+        TokenHolders tokenHolders = deps.resolveDependency( TokenHolders.class );
+        int sortKey = tokenHolders.propertyKeyTokens().getIdByName( SORTVALUE );
+        int distanceKey = tokenHolders.propertyKeyTokens().getIdByName( DISTANCE );
+        Kernel kernel = deps.resolveDependency( Kernel.class );
+        try ( KernelTransaction tx = kernel.beginTransaction( KernelTransaction.Type.EXPLICIT, LoginContext.AUTH_DISABLED );
+                NodeCursor nodeCursor = tx.cursors().allocateNodeCursor( NULL );
+                RelationshipTraversalCursor relationshipCursor = tx.cursors().allocateRelationshipTraversalCursor( NULL );
+                PropertyCursor propertyCursor = tx.cursors().allocatePropertyCursor( NULL ) )
+        {
+            tx.dataRead().singleNode( startCityID, nodeCursor );
+            nodeCursor.next();
+            int hops = 0;
+            while ( hops++ < TRAVEL_HOPS )
+            {
+                Map<Integer,long[]> sortedRoads = new TreeMap<>();
+                nodeCursor.relationships( relationshipCursor, ALL_RELATIONSHIPS );
+                while ( relationshipCursor.next() )
+                {
+                    relationshipCursor.properties( propertyCursor );
+                    int roadSortValue = 0;
+                    int roadDistance = 0;
+                    while ( propertyCursor.next() )
+                    {
+                        if ( propertyCursor.propertyKey() == sortKey )
+                        {
+                            roadSortValue = ((IntValue) propertyCursor.propertyValue()).value();
+                        }
+                        else if ( propertyCursor.propertyKey() == distanceKey )
+                        {
+                            roadDistance = ((IntValue) propertyCursor.propertyValue()).value();
+                        }
+                    }
+                    sortedRoads.put( roadSortValue, new long[]{roadDistance, relationshipCursor.otherNodeReference()} );
+                }
+                long[][] roads = sortedRoads.values().toArray( new long[sortedRoads.size()][] );
+                long[] road = roads[random.nextInt( sortedRoads.size() )];
+                tx.dataRead().singleNode( road[1], nodeCursor );
+                nodeCursor.next();
+                distance += road[0];
+            }
+        }
+        catch ( TransactionFailureException e )
+        {
+            throw new RuntimeException( e );
+        }
+        return distance;
+    }
+
+    private static long runBenchmarkWithStorageCursors( GraphDatabaseService db, Random random )
+    {
+        long distance = 0;
+        DependencyResolver deps = ((GraphDatabaseAPI) db).getDependencyResolver();
+        TokenHolders tokenHolders = deps.resolveDependency( TokenHolders.class );
+        int sortKey = tokenHolders.propertyKeyTokens().getIdByName( SORTVALUE );
+        int distanceKey = tokenHolders.propertyKeyTokens().getIdByName( DISTANCE );
+        try ( StorageReader reader = deps.resolveDependency( StorageEngine.class ).newReader();
+                StorageNodeCursor nodeCursor = reader.allocateNodeCursor( NULL );
+                StorageRelationshipTraversalCursor relationshipCursor = reader.allocateRelationshipTraversalCursor( NULL );
+                StoragePropertyCursor propertyCursor = reader.allocatePropertyCursor( NULL ) )
+        {
+            nodeCursor.single( startCityID );
+            nodeCursor.next();
+            int hops = 0;
+            while ( hops++ < TRAVEL_HOPS )
+            {
+                Map<Integer,long[]> sortedRoads = new TreeMap<>();
+                nodeCursor.relationships( relationshipCursor, ALL_RELATIONSHIPS );
+                while ( relationshipCursor.next() )
+                {
+                    relationshipCursor.properties( propertyCursor );
+                    int roadSortValue = 0;
+                    int roadDistance = 0;
+                    while ( propertyCursor.next() )
+                    {
+                        if ( propertyCursor.propertyKey() == sortKey )
+                        {
+                            roadSortValue = ((IntValue) propertyCursor.propertyValue()).value();
+                        }
+                        else if ( propertyCursor.propertyKey() == distanceKey )
+                        {
+                            roadDistance = ((IntValue) propertyCursor.propertyValue()).value();
+                        }
+                    }
+                    sortedRoads.put( roadSortValue, new long[]{roadDistance, relationshipCursor.neighbourNodeReference()} );
+                }
+                long[][] roads = sortedRoads.values().toArray( new long[sortedRoads.size()][] );
+                long[] road = roads[random.nextInt( sortedRoads.size() )];
+                nodeCursor.single( road[1] );
+                nodeCursor.next();
+                distance += road[0];
+            }
+        }
+        return distance;
     }
 }
