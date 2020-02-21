9a2396642bb0af4b0846942c655b149e646cd2da
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2020-02-21 10:34:12 +0100

    Fix license header

--- private/benchmarks/benchmark-neo4j-common/src/main/java/com/neo4j/bench/common/database/Neo4jDatabaseNames.java
+++ private/benchmarks/benchmark-neo4j-common/src/main/java/com/neo4j/bench/common/database/Neo4jDatabaseNames.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
- * This file is a commercial add-on to Neo4j Enterprise Edition.
+ * This file is part of Neo4j internal tooling.
  */
 package com.neo4j.bench.common.database;
 
