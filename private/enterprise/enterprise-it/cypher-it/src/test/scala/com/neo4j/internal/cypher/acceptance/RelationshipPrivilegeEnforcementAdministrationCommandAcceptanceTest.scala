92c301d22cce1dd18aa160c956d4e768262fa0a8
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2020-02-26 12:12:22 +0100

    Revert "Security test doesn't assert degrees"
    This reverts commit 8a56fabf

--- private/enterprise/enterprise-it/cypher-it/src/test/scala/com/neo4j/internal/cypher/acceptance/RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest.scala
+++ private/enterprise/enterprise-it/cypher-it/src/test/scala/com/neo4j/internal/cypher/acceptance/RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest.scala
@@ -280,6 +280,7 @@ class RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest extend
     // THEN
     executeOnDefault("joe", "soap", "MATCH (a:Start) RETURN a", resultHandler = (row, _) => {
       val node = row.get("a").asInstanceOf[Node]
+      node.getDegree() should be(0)
       node.getRelationships().asScala.map(_.getType.name()).toSet should be(Set())
     }) should be(1)
 
@@ -290,6 +291,7 @@ class RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest extend
     // THEN
     executeOnDefault("joe", "soap", "MATCH (a:Start) RETURN a", resultHandler = (row, _) => {
       val node = row.get("a").asInstanceOf[Node]
+      node.getDegree() should be(1)
       node.getRelationships().asScala.map(_.getType.name()).toSet should be(Set("A"))
     }) should be(1)
 
@@ -300,6 +302,7 @@ class RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest extend
     // THEN
     executeOnDefault("joe", "soap", "MATCH (a:Start) RETURN a", resultHandler = (row, _) => {
       val node = row.get("a").asInstanceOf[Node]
+      node.getDegree() should be(2)
       node.getRelationships().asScala.map(_.getType.name()).toSet should be(Set("A", "B"))
     }) should be(1)
 
@@ -310,6 +313,7 @@ class RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest extend
     // THEN
     executeOnDefault("joe", "soap", "MATCH (a:Start) RETURN a", resultHandler = (row, _) => {
       val node = row.get("a").asInstanceOf[Node]
+      node.getDegree() should be(1)
       node.getRelationships().asScala.map(_.getType.name()).toSet should be(Set("B"))
     }) should be(1)
 
@@ -320,6 +324,7 @@ class RelationshipPrivilegeEnforcementAdministrationCommandAcceptanceTest extend
     // THEN
     executeOnDefault("joe", "soap", "MATCH (a:Start) RETURN a", resultHandler = (row, _) => {
       val node = row.get("a").asInstanceOf[Node]
+      node.getDegree() should be(0)
       node.getRelationships().asScala.map(_.getType.name()).toSet should be(Set())
     }) should be(1)
   }
