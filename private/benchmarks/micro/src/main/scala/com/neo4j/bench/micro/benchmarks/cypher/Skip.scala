2d5a19595e1a57118869a275540c6e8c38be57bc
Pontus Melke <pontusmelke@gmail.com>
2020-03-03 11:01:12 +0100

    Add micro benchmark for skip

---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/Skip.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.jmh.api.config.BenchmarkEnabled
+import com.neo4j.bench.jmh.api.config.ParamValues
+import com.neo4j.bench.micro.Main
+import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.data.DataGeneratorConfig
+import com.neo4j.bench.micro.data.DataGeneratorConfigBuilder
+import com.neo4j.bench.micro.data.Plans.IdGen
+import com.neo4j.bench.micro.data.Plans.astLiteralFor
+import com.neo4j.bench.micro.data.Plans.astVariable
+import com.neo4j.bench.micro.data.TypeParamValues.LNG
+import org.neo4j.cypher.internal.ast.semantics.SemanticTable
+import org.neo4j.cypher.internal.logical.plans
+import org.neo4j.cypher.internal.planner.spi.PlanContext
+import org.neo4j.kernel.impl.coreapi.InternalTransaction
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.TearDown
+import org.openjdk.jmh.infra.Blackhole
+
+@BenchmarkEnabled(true)
+class Skip extends AbstractCypherBenchmark {
+  @ParamValues(
+    allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, Slotted.NAME, Morsel.NAME, Parallel.NAME),
+    base = Array(Slotted.NAME, Morsel.NAME))
+  @Param(Array[String]())
+  var runtime: String = _
+
+  @ParamValues(
+    allowed = Array("1", "100", "10000", "1000000"),
+    base = Array("1", "1000000"))
+  @Param(Array[String]())
+  var toSkip: Int = _
+
+  override def description = "MATCH (n) WHERE n.key=$val RETURN n"
+
+  private val NODE_COUNT = 1000000
+
+  override protected def getConfig: DataGeneratorConfig =
+    new DataGeneratorConfigBuilder()
+      .withNodeCount(NODE_COUNT)
+      .isReusableStore(true)
+      .build()
+
+  override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
+    val node = astVariable("n")
+    val nodeIdName = node.name
+    val allNodeScan = plans.AllNodesScan(nodeIdName, Set.empty)(IdGen)
+    val skip = plans.Skip(allNodeScan, astLiteralFor(toSkip, LNG) )(IdGen)
+    val resultColumns = List(nodeIdName)
+    val produceResults = plans.ProduceResult(skip, resultColumns)(IdGen)
+
+    val table = SemanticTable().addNode(node)
+
+    (produceResults, table, resultColumns)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SampleTime))
+  def executePlan(threadState: SkipThreadState, bh: Blackhole): Long = {
+    val subscriber = new CountSubscriber(bh)
+    val result = threadState.executablePlan.execute(tx = threadState.tx, subscriber = subscriber)
+    result.consumeAll()
+    assertExpectedRowCount(NODE_COUNT - toSkip, subscriber)
+  }
+}
+
+object Skip {
+  def main(args: Array[String]): Unit = {
+    Main.run(classOf[Skip], args:_*)
+  }
+}
+
+@State(Scope.Thread)
+class SkipThreadState {
+  var tx: InternalTransaction = _
+  var executablePlan: ExecutablePlan = _
+
+  @Setup
+  def setUp(benchmarkState: Skip): Unit = {
+    executablePlan = benchmarkState.buildPlan(from(benchmarkState.runtime))
+    tx = benchmarkState.beginInternalTransaction()
+  }
+
+  @TearDown
+  def tearDown(): Unit = {
+    tx.close()
+  }
+}
