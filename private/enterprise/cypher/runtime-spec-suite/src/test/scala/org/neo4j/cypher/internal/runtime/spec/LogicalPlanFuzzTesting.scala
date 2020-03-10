eba1c82130d8dc1e8071c8c7a9b9134ed5de592c
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2020-03-10 09:59:12 +0100

    Output random seed used to generate plans in fuzz test

--- private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/LogicalPlanFuzzTesting.scala
+++ private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/LogicalPlanFuzzTesting.scala
@@ -5,9 +5,12 @@
  */
 package org.neo4j.cypher.internal.runtime.spec
 
+import java.util.Random
+
 import org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME
 import org.neo4j.cypher.internal.EnterpriseRuntimeContext
 import org.neo4j.cypher.internal.InterpretedRuntime
+import org.neo4j.cypher.internal.LogicalQuery
 import org.neo4j.cypher.internal.PipelinedRuntime.PIPELINED
 import org.neo4j.cypher.internal.SlottedRuntime
 import org.neo4j.cypher.internal.compiler.planner.logical.CardinalityCostModel
@@ -18,17 +21,15 @@ import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
 import org.neo4j.exceptions.CantCompileQueryException
 import org.neo4j.logging.AssertableLogProvider
 import org.neo4j.values.storable.RandomValues
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalacheck.Gen
+import org.scalacheck.rng.Seed
 
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
 
 // Class name not ending in `Test` to make sure `mvn test` won't run it
-class LogicalPlanFuzzTesting extends CypherFunSuite
-                             with GeneratorDrivenPropertyChecks {
-
-  implicit val config: PropertyCheckConfiguration = PropertyCheckConfiguration(minSuccessful = 100)
+class LogicalPlanFuzzTesting extends CypherFunSuite {
 
   private val managementService = ENTERPRISE.DEFAULT.newGraphManagementService()
   private val graphDb = managementService.database(DEFAULT_DATABASE_NAME)
@@ -36,7 +37,6 @@ class LogicalPlanFuzzTesting extends CypherFunSuite
   private val runtimeTestSupport = new RuntimeTestSupport[EnterpriseRuntimeContext](graphDb, ENTERPRISE.WITH_FUSING(ENTERPRISE.DEFAULT), false, logProvider) with GraphCreation[EnterpriseRuntimeContext] {
     override protected def runtimeTestSupport: RuntimeTestSupport[EnterpriseRuntimeContext] = this
   }
-  private val randVals = RandomValues.create()
 
   private val runtimes = Seq(InterpretedRuntime, SlottedRuntime, PIPELINED/*, PARALLEL*/)
 
@@ -62,48 +62,60 @@ class LogicalPlanFuzzTesting extends CypherFunSuite
 
     val generator = LogicalQueryGenerator.logicalQuery(txContext, Cost(10000))
 
+    val initialSeed = Seed.random() // use `Seed.fromBase64` to reproduce test failures
+    val iterationCount = sys.env.getOrElse("LOGICAL_PLAN_FUZZ_ITERATIONS", "100").toInt
+
     try {
-      forAll(generator) {
-        case WithState(logicalQuery, state) =>
-          val parameters = state.parameters.map(_ -> randVals.nextValue().asObject()).toMap
-
-          val cost = CardinalityCostModel(logicalQuery.logicalPlan, QueryGraphSolverInput.empty, logicalQuery.cardinalities)
-
-          println(logicalQuery.logicalPlan)
-          println(parameters)
-          println(cost)
-
-          runtimeTestSupport.startTx()
-
-          try {
-            val results = runtimes.map {
-              runtime => Try(runtimeTestSupport.executeAndConsumeTransactionally(logicalQuery, runtime, parameters))
-            }
-
-            results.zipWithIndex.tail.foreach {
-              case (Success(result), i) =>
-                withClue(s"Comparing ${runtimes(i).name} against ${runtimes.head.name}") {
-                  println(s"result.size = ${result.size}")
-                  if (result.size <= 1000)
-                    result should (contain theSameElementsAs results.head.get)
-                  else
-                    println(s"skipping check")
-                }
-              case (Failure(_: CantCompileQueryException), _) => // OK
-              case (Failure(e), i) =>
-                withClue(s"Error in ${runtimes(i).name}") {
-                  throw e
-                }
-            }
-          } finally {
-            runtimeTestSupport.stopTx()
-          }
+      Range(0, iterationCount).foldLeft(initialSeed) {
+        (seed: Seed, iter: Int) =>
+          println()
+          println(s"[${iter + 1}/$iterationCount] seed = ${seed.toBase64}")
+          runTest(seed, generator)
+          Seed.random()
       }
     } finally {
       txContext.close()
       tx.close()
       runtimeTestSupport.stop()
       managementService.shutdown()
+    }
+  }
+
+  private def runTest(seed: Seed, generator: Gen[WithState[LogicalQuery]]): Unit = {
+    val WithState(logicalQuery, state) = generator.pureApply(Gen.Parameters.default, seed)
+    val randVals = RandomValues.create(new Random(seed.long._1))
+    val parameters = state.parameters.map(_ -> randVals.nextValue().asObject()).toMap
+
+    val cost = CardinalityCostModel(logicalQuery.logicalPlan, QueryGraphSolverInput.empty, logicalQuery.cardinalities)
+
+    println(logicalQuery.logicalPlan)
+    println(parameters)
+    println(cost)
+
+    runtimeTestSupport.startTx()
+
+    try {
+      val results = runtimes.map {
+        runtime => Try(runtimeTestSupport.executeAndConsumeTransactionally(logicalQuery, runtime, parameters))
+      }
+
+      results.zipWithIndex.tail.foreach {
+        case (Success(result), i) =>
+          withClue(s"Comparing ${runtimes(i).name} against ${runtimes.head.name}") {
+            println(s"result.size = ${result.size}")
+            if (result.size <= 1000)
+              result should (contain theSameElementsAs results.head.get)
+            else
+              println(s"skipping check")
+          }
+        case (Failure(_: CantCompileQueryException), _) => // OK
+        case (Failure(e), i) =>
+          withClue(s"Error in ${runtimes(i).name}") {
+            throw e
+          }
+      }
+    } finally {
+      runtimeTestSupport.stopTx()
     }
   }
 }
