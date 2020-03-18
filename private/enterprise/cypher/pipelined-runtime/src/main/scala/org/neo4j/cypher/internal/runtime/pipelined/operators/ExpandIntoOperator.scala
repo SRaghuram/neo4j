4acbda87292d9b9754ca96159107825caa49ad4b
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2020-03-18 12:46:47 +0100

    Corrected some generated operation expand code
    Originally introduced in 52d0e0a8 by Mattias Finné

--- private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/operators/ExpandIntoOperator.scala
+++ private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/operators/ExpandIntoOperator.scala
@@ -389,6 +389,7 @@ class ExpandIntoOperatorTaskTemplate(inner: OperatorTaskTemplate,
 
 object ExpandIntoOperatorTaskTemplate {
   val CONNECTING_RELATIONSHIPS: Method = method[CachingExpandInto,
+    RelationshipTraversalCursor,
     NodeCursor,
     RelationshipTraversalCursor,
     Long,
