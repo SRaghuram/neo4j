9b9c988efb614c6f56026b68554805a3fee9a259
Balazs Lendvai <56266523+gfx54b@users.noreply.github.com>
2020-03-25 11:27:28 +0100

    Using COMPOSITE_CUMULATOR in TxPullResponseDecoder (#5212)

--- private/enterprise/causal-clustering/src/main/java/com/neo4j/causalclustering/catchup/v3/tx/TxPullResponseDecoder.java
+++ private/enterprise/causal-clustering/src/main/java/com/neo4j/causalclustering/catchup/v3/tx/TxPullResponseDecoder.java
@@ -33,6 +33,7 @@ public class TxPullResponseDecoder extends ByteToMessageDecoder
     public TxPullResponseDecoder( CommandReaderFactory commandReaderFactory )
     {
         reader = new VersionAwareLogEntryReader( commandReaderFactory );
+        setCumulator( COMPOSITE_CUMULATOR );
     }
 
     @Override
