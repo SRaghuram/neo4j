10abf7415103c06f8c4bfa637effde65551a2915
fickludd <johan.teleman@neotechnology.com>
2020-04-02 12:18:47 +0200

    Move OptionalArgumentState stuff to OptionalMorselBuffer file

--- private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/state/buffers/OptionalMorselBuffer.scala
+++ private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/state/buffers/OptionalMorselBuffer.scala
@@ -5,15 +5,24 @@
  */
 package org.neo4j.cypher.internal.runtime.pipelined.state.buffers
 
+import java.util
+
 import org.neo4j.cypher.internal.physicalplanning.ArgumentStateMapId
 import org.neo4j.cypher.internal.physicalplanning.BufferId
 import org.neo4j.cypher.internal.runtime.debug.DebugSupport
 import org.neo4j.cypher.internal.runtime.pipelined.execution.Morsel
+import org.neo4j.cypher.internal.runtime.pipelined.execution.MorselReadCursor
+import org.neo4j.cypher.internal.runtime.pipelined.execution.MorselRow
+import org.neo4j.cypher.internal.runtime.pipelined.state.ArgumentStateMap.ArgumentStateFactory
 import org.neo4j.cypher.internal.runtime.pipelined.state.ArgumentStateMap.ArgumentStateMaps
 import org.neo4j.cypher.internal.runtime.pipelined.state.ArgumentStateMap.ArgumentStateWithCompleted
 import org.neo4j.cypher.internal.runtime.pipelined.state.ArgumentStateMap.PerArgument
 import org.neo4j.cypher.internal.runtime.pipelined.state.QueryCompletionTracker
+import org.neo4j.cypher.internal.runtime.pipelined.state.StateFactory
 import org.neo4j.cypher.internal.runtime.pipelined.state.buffers.Buffers.AccumulatingBuffer
+import org.neo4j.cypher.internal.util.attribution.Id
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Extension of Morsel buffer that also holds an argument state map in order to track
@@ -131,4 +140,107 @@ class OptionalMorselBuffer(id: BufferId,
 
   override def toString: String =
     s"OptionalMorselBuffer(planId: $argumentStateMapId)$argumentStateMap"
+}
+
+// --- Inner Buffer ---
+
+/**
+ * For Optional, we need to keep track whether the Buffer held data at any point in time.
+ */
+trait OptionalBuffer {
+  self: Buffer[_] =>
+  /**
+   * @return `true` if this buffer held data at any point in time, `false` if it was always empty.
+   */
+  def didReceiveData: Boolean
+}
+
+/**
+ * Delegating [[Buffer]] used in argument state maps.
+ * Holds data for one argument row id in an [[OptionalBuffer]].
+ */
+class OptionalArgumentStateBuffer(argumentRowId: Long,
+                                  val argumentRow: MorselRow,
+                                  inner: Buffer[Morsel] with OptionalBuffer,
+                                  argumentRowIdsForReducers: Array[Long]) extends ArgumentStateBuffer(argumentRowId, inner, argumentRowIdsForReducers) {
+  /**
+   * @return `true` if this buffer held data at any point in time, `false` if it was always empty.
+   */
+  def didReceiveData: Boolean = inner.didReceiveData
+
+  /**
+   * Take all morsels from the buffer that are currently available.
+   */
+  def takeAll(): IndexedSeq[Morsel] = {
+    var morsel = take()
+    if (morsel != null) {
+      val morsels = new ArrayBuffer[Morsel]
+      do {
+        morsels += morsel
+        morsel = take()
+      } while (morsel != null)
+      morsels
+    } else {
+      null
+    }
+  }
+
+  override def toString: String = {
+    s"OptionalArgumentStateBuffer(argumentRowId=$argumentRowId, argumentRowIdsForReducers=[${argumentRowIdsForReducers.mkString(",")}], argumentMorsel=$argumentRow, inner=$inner)"
+  }
+}
+
+object OptionalArgumentStateBuffer {
+  class Factory(stateFactory: StateFactory, operatorId: Id) extends ArgumentStateFactory[ArgumentStateBuffer] {
+    override def newStandardArgumentState(argumentRowId: Long, argumentMorsel: MorselReadCursor, argumentRowIdsForReducers: Array[Long]): ArgumentStateBuffer =
+      new OptionalArgumentStateBuffer(argumentRowId, argumentMorsel.snapshot(), new StandardOptionalBuffer[Morsel](stateFactory.newBuffer[Morsel](operatorId)), argumentRowIdsForReducers)
+
+    override def newConcurrentArgumentState(argumentRowId: Long, argumentMorsel: MorselReadCursor, argumentRowIdsForReducers: Array[Long]): ArgumentStateBuffer =
+      new OptionalArgumentStateBuffer(argumentRowId, argumentMorsel.snapshot(), new ConcurrentOptionalBuffer[Morsel](stateFactory.newBuffer[Morsel](operatorId)), argumentRowIdsForReducers)
+  }
+}
+
+class StandardOptionalBuffer[T <: AnyRef](inner: Buffer[T]) extends Buffer[T] with OptionalBuffer {
+  private var _didReceiveData : Boolean = false
+
+  override def put(t: T): Unit = {
+    _didReceiveData = true
+    inner.put(t)
+  }
+
+  def didReceiveData: Boolean = _didReceiveData
+
+  override def foreach(f: T => Unit): Unit = inner.foreach(f)
+
+  override def hasData: Boolean = inner.hasData
+
+  override def take(): T = inner.take()
+
+  override def canPut: Boolean = inner.canPut
+
+  override def iterator: util.Iterator[T] = inner.iterator
+}
+
+class ConcurrentOptionalBuffer[T <: AnyRef](inner: Buffer[T]) extends Buffer[T] with OptionalBuffer {
+  @volatile
+  private var _didReceiveData : Boolean = false
+
+  override def put(t: T): Unit = {
+    _didReceiveData = true
+    inner.put(t)
+  }
+
+  def didReceiveData: Boolean = _didReceiveData
+
+  override def foreach(f: T => Unit): Unit = inner.foreach(f)
+
+  override def hasData: Boolean = inner.hasData
+
+  override def take(): T = inner.take()
+
+  override def canPut: Boolean = inner.canPut
+
+  override def iterator: util.Iterator[T] = inner.iterator
+
+  override def toString: String = s"OptionalMorselBuffer(didReceiveData=${_didReceiveData}, inner=${inner.toString})"
 }
