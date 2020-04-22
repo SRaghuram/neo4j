a304160460b3becac8f02bba6d5916bb235c36a5
fickludd <johan.teleman@neotechnology.com>
2020-04-22 09:30:14 +0200

    Use ArraySeq instead of IndexedSeq in AbstractSingletonArgumentStateMap.takeCompleted

---
+++ private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/state/Collections.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package org.neo4j.cypher.internal.runtime.pipelined.state
+
+import scala.collection.mutable
+
+object Collections {
+
+  // Curiously, this is faster than doing e.g. IndexedSeq(...) or ArraySeq(...)
+  def arraySeq[T <: AnyRef](t: T): mutable.ArraySeq[T] = {
+    val result = new mutable.ArraySeq[T](1)
+    result(0) = t
+    result
+  }
+}
