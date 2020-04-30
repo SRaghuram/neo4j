8f6a4e6aa5c1a35b3def448ee15e277c97ce7c87
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2020-04-30 13:58:13 +0100

    Add dbms.clientConfig procedure This returns only the less sensitive config items needed by browser.

--- private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/procedure/enterprise/builtin/BuiltInEnterpriseProceduresTest.java
+++ private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/procedure/enterprise/builtin/BuiltInEnterpriseProceduresTest.java
@@ -10,13 +10,25 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.List;
 
+import org.neo4j.collection.RawIterator;
+import org.neo4j.internal.kernel.api.exceptions.ProcedureException;
+import org.neo4j.internal.kernel.api.procs.ProcedureCallContext;
+import org.neo4j.internal.kernel.api.procs.QualifiedName;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.api.security.AnonymousContext;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.procedure.builtin.BuiltInDbmsProcedures;
+import org.neo4j.values.AnyValue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+import static org.neo4j.internal.helpers.collection.Iterators.asList;
+import static org.neo4j.internal.kernel.api.procs.ProcedureSignature.procedureName;
+import static org.neo4j.kernel.api.KernelTransaction.Type.IMPLICIT;
+import static org.neo4j.values.storable.Values.stringValue;
 
 @EnterpriseDbmsExtension
 class BuiltInEnterpriseProceduresTest extends SystemBuiltInEnterpriseProceduresTest
@@ -37,6 +49,27 @@ class BuiltInEnterpriseProceduresTest extends SystemBuiltInEnterpriseProceduresT
     void checkThatEnterpriseAndCommunityHaveSameResultColumsForListFunctions()
     {
         checkClassesHaveSameFinalFields( BuiltInDbmsProcedures.FunctionResult.class, EnterpriseBuiltInDbmsProcedures.FunctionResult.class );
+    }
+
+    @Test
+    void listClientConfig() throws Exception
+    {
+        QualifiedName procedureName = procedureName( "dbms", "clientConfig" );
+        GraphDatabaseAPI system = getGraphDatabaseAPI();
+        KernelTransaction transaction = system.beginTransaction( IMPLICIT, AnonymousContext.read() ).kernelTransaction();
+        int procedureId = transaction.procedures().procedureGet( procedureName ).id();
+        RawIterator<AnyValue[],ProcedureException> callResult =
+                transaction.procedures().procedureCallRead( procedureId, new AnyValue[]{}, ProcedureCallContext.EMPTY );
+        List<AnyValue[]> config = asList( callResult );
+        assertEquals( 7, config.size());
+
+        assertEquals( config.get( 0 )[0], stringValue( "browser.allow_outgoing_connections" ));
+        assertEquals( config.get( 1 )[0], stringValue( "browser.credential_timeout" ));
+        assertEquals( config.get( 2 )[0], stringValue( "browser.post_connect_cmd" ));
+        assertEquals( config.get( 3 )[0], stringValue( "browser.remote_content_hostname_whitelist" ));
+        assertEquals( config.get( 4 )[0], stringValue( "browser.retain_connection_credentials" ));
+        assertEquals( config.get( 5 )[0], stringValue( "dbms.default_database" ));
+        assertEquals( config.get( 6 )[0], stringValue( "dbms.security.auth_enabled" ));
     }
 
     private static void checkClassesHaveSameFinalFields( Class<?> community, Class<?> enterprise )
