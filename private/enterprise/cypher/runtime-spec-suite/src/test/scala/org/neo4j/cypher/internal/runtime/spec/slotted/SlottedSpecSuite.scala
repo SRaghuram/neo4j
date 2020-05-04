4e9d209df51b29d4981fd22b473645bd772621eb
Pontus Melke <pontusmelke@gmail.com>
2020-05-04 16:02:36 +0200

    fix missing import

--- private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/slotted/SlottedSpecSuite.scala
+++ private/enterprise/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/slotted/SlottedSpecSuite.scala
@@ -80,6 +80,7 @@ import org.neo4j.cypher.internal.runtime.spec.tests.ReactiveResultTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.RelationshipCountFromCountStoreTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.RightOuterHashJoinTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.RollupApplyTestBase
+import org.neo4j.cypher.internal.runtime.spec.tests.SelectOrSemiApplyTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.SemiApplyTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.ShortestPathTestBase
 import org.neo4j.cypher.internal.runtime.spec.tests.SkipTestBase
