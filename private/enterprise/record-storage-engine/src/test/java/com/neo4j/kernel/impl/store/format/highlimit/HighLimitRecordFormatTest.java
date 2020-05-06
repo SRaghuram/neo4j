35385cbc92719215246a5796f189b669ed93638a
Mattias Finné <mattias@neotechnology.com>
2020-05-06 14:31:18 +0200

    Fixes issue shrinking high_limit double-unit record back to single-unit
    In the high_limit record format some records may need more space to store
    its data and can therefore make use of a secondary record unit.
    Allocating a secondary unit for a record that needs it takes place right
    before writing the record to the store, when preparing it. The problem
    occurred when a record which previously required a secondary unit
    got changed to no longer do so and went back to single unit and fixed
    reference format (more on fixed reference format below).
    At that point the prepare method didn't mark this fact properly in the
    record state and so therefore the record would be written with the header
    saying one thing and the data written in a way not matching the header.
    
    Size-growth-wise a high_limit record will go through these stages:
    
    - (1) All pointers are each small enough such that a faster fixed-size
      fields variant of the format can be used. This implies single unit
    - (2) One or more pointers fails the fixed-size variant criterion, but
      as a whole the record fits in a single unit
    - (3) One or more pointers fails the fixed-size variant criterion and
      the data requires more space than a single unit provides
    
    These three variants have each their own unique combination of bits
    in the header byte that each high_limit record format has. The problem
    was that shrinking from (3) to (1) would still write the header as (3)
    but the data as (1) and therefore the next read of that record would
    essentially read complete garbage.

--- private/enterprise/record-storage-engine/src/test/java/com/neo4j/kernel/impl/store/format/highlimit/HighLimitRecordFormatTest.java
+++ private/enterprise/record-storage-engine/src/test/java/com/neo4j/kernel/impl/store/format/highlimit/HighLimitRecordFormatTest.java
@@ -5,12 +5,102 @@
  */
 package com.neo4j.kernel.impl.store.format.highlimit;
 
-import org.neo4j.kernel.impl.store.format.AbstractRecordFormatTest;
+import org.junit.jupiter.api.Test;
 
-public class HighLimitRecordFormatTest extends AbstractRecordFormatTest
+import java.io.IOException;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import org.neo4j.internal.id.BatchingIdSequence;
+import org.neo4j.io.pagecache.ByteArrayPageCursor;
+import org.neo4j.io.pagecache.PageCursor;
+import org.neo4j.io.pagecache.tracing.cursor.PageCursorTracer;
+import org.neo4j.kernel.impl.store.IntStoreHeader;
+import org.neo4j.kernel.impl.store.format.AbstractRecordFormatTest;
+import org.neo4j.kernel.impl.store.format.RecordFormat;
+import org.neo4j.kernel.impl.store.format.RecordGenerators.Generator;
+import org.neo4j.kernel.impl.store.format.RecordKey;
+import org.neo4j.kernel.impl.store.record.AbstractBaseRecord;
+
+import static java.lang.System.currentTimeMillis;
+
+class HighLimitRecordFormatTest extends AbstractRecordFormatTest
 {
-    public HighLimitRecordFormatTest()
+    HighLimitRecordFormatTest()
     {
         super( HighLimit.RECORD_FORMATS, 50, 50 );
+    }
+
+    @Test
+    void nodeGrowAndShrink() throws Exception
+    {
+        verifyWriteAndReadWhenGrowingAndShrinking( formats::node, generators::node, keys::node );
+    }
+
+    @Test
+    void relationshipGrowAndShrink() throws Exception
+    {
+        verifyWriteAndReadWhenGrowingAndShrinking( formats::relationship, generators::relationship, keys::relationship );
+    }
+
+    @Test
+    void relationshipGroupGrowAndShrink() throws Exception
+    {
+        verifyWriteAndReadWhenGrowingAndShrinking( formats::relationshipGroup, generators::relationshipGroup, keys::relationshipGroup );
+    }
+
+    private <R extends AbstractBaseRecord> void verifyWriteAndReadWhenGrowingAndShrinking(
+            Supplier<RecordFormat<R>> formatSupplier,
+            Supplier<Generator<R>> generatorSupplier,
+            Supplier<RecordKey<R>> keySupplier ) throws IOException
+    {
+        // GIVEN
+        RecordFormat<R> format = formatSupplier.get();
+        RecordKey<R> key = keySupplier.get();
+        Generator<R> generator = generatorSupplier.get();
+        int recordSize = format.getRecordSize( new IntStoreHeader( DATA_SIZE ) );
+        BatchingIdSequence idSequence = new BatchingIdSequence( 1 );
+        // WHEN
+        PageCursor cursor = ByteArrayPageCursor.wrap( recordSize );
+        long time = currentTimeMillis();
+        long endTime = time + TEST_TIME;
+        long i = 0;
+        for ( ; i < TEST_ITERATIONS && currentTimeMillis() < endTime; i++ )
+        {
+            // First generate a guaranteed double-unit record
+            R doubleUnitRecord = continueUntil(
+                    () -> prepared( generator.get( recordSize, format, random.nextLong( 1, format.getMaxId() ) ), format, recordSize, idSequence ),
+                    AbstractBaseRecord::requiresSecondaryUnit );
+            R readDoubleUnitRecord = verifyWriteAndReadRecord( true, format, key, recordSize, idSequence, cursor, i, doubleUnitRecord );
+
+            // Then change it to become single-unit, do this by using the generator to generate a new record and then copy
+            // the state of the secondary unit and ID from the previous double-unit into it. This mimics loading the record
+            // and changing its data and doesn't require special generator methods for randomly changing a record.
+            R shrunkRecord = continueUntil(
+                    () -> prepared( generator.get( recordSize, format, random.nextLong( 1, format.getMaxId() ) ), format, recordSize, idSequence ),
+                    AbstractBaseRecord::isUseFixedReferences );
+            shrunkRecord.setId( readDoubleUnitRecord.getId() );
+            shrunkRecord.setRequiresSecondaryUnit( readDoubleUnitRecord.requiresSecondaryUnit() );
+            shrunkRecord.setSecondaryUnitIdOnLoad( readDoubleUnitRecord.getSecondaryUnitId() );
+            shrunkRecord.setUseFixedReferences( readDoubleUnitRecord.isUseFixedReferences() );
+            verifyWriteAndReadRecord( true, format, key, recordSize, idSequence, cursor, i, shrunkRecord );
+        }
+    }
+
+    private <R extends AbstractBaseRecord> R prepared( R record, RecordFormat<R> format, int recordSize, BatchingIdSequence idSequence )
+    {
+        format.prepare( record, recordSize, idSequence, PageCursorTracer.NULL );
+        return record;
+    }
+
+    private static <T> T continueUntil( Supplier<T> generator, Predicate<T> filter )
+    {
+        T result;
+        do
+        {
+            result = generator.get();
+        }
+        while ( !filter.test( result ) );
+        return result;
     }
 }
