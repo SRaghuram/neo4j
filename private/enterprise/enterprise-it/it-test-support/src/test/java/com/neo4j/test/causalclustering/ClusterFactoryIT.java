1ed726927f88ef09ca31b5bcda46aec57a1e03fb
Martin Furmanski <martin.furmanski@neotechnology.com>
2020-05-12 17:30:30 +0200

    ClusterFactoryIT skip thread leakage guard

--- private/enterprise/enterprise-it/it-test-support/src/test/java/com/neo4j/test/causalclustering/ClusterFactoryIT.java
+++ private/enterprise/enterprise-it/it-test-support/src/test/java/com/neo4j/test/causalclustering/ClusterFactoryIT.java
@@ -14,9 +14,11 @@ import org.junit.jupiter.api.TestInstance;
 
 import org.neo4j.graphdb.Label;
 import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.SkipThreadLeakageGuard;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SkipThreadLeakageGuard
 class ClusterFactoryIT
 {
     @Nested
