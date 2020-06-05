c1b59a876c6978e709d2ab53291f359e2f4aae4a
Satia Herfert <satiaherfert@gmx.de>
2020-06-05 15:52:41 +0200

    Test Stdev.

---
+++ private/enterprise/cypher/pipelined-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/pipelined/aggregators/StdevAggregatorTest.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package org.neo4j.cypher.internal.runtime.pipelined.aggregators
+
+import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
+import org.neo4j.cypher.internal.runtime.interpreted.pipes.aggregation.AggregationFunction
+import org.neo4j.cypher.internal.runtime.interpreted.pipes.aggregation.DistinctFunction
+import org.neo4j.cypher.internal.runtime.interpreted.pipes.aggregation.StdevFunction
+import org.neo4j.cypher.internal.runtime.pipelined.aggregators.StdevAggregatorTest.stdev
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+import org.neo4j.memory.EmptyMemoryTracker
+import org.neo4j.values.storable.NumberValue
+
+object StdevAggregatorTest {
+  // using BigDecimal, because this naive implementation would otherwise
+  // accumulate large rounding errors.
+  def stdev(ints: Seq[Int]): Double = {
+    val n = ints.length
+    val sum = BigDecimal(ints.sum)
+    val sumSq = BigDecimal(ints.map(x => x * x).sum)
+    val variance = (sumSq - (sum * sum) / n) / (n - 1)
+    variance.bigDecimal.sqrt(variance.mc).doubleValue()
+  }
+}
+
+class FunctionStdevAggregatorTest extends StdevAggregatorTest with FunctionAggregatorTest {
+  override def getAggregationFunction(e: Expression): AggregationFunction = new StdevFunction(e, false, EmptyMemoryTracker.INSTANCE)
+}
+
+abstract class StdevAggregatorTest extends CypherFunSuite with AggregatorTest {
+  test("should stdev numbers") {
+    val result = runAggregation(randomIntValuesWithNulls).asInstanceOf[NumberValue].doubleValue()
+    result should be(stdev(randomInts) +- 0.0001)
+  }
+}
+
+class FunctionStdevDistinctAggregatorTest extends StdevDistinctAggregatorTest with FunctionAggregatorTest {
+  override def getAggregationFunction(exp: Expression): AggregationFunction = new DistinctFunction(exp, new StdevFunction(exp, false, EmptyMemoryTracker.INSTANCE), EmptyMemoryTracker.INSTANCE)
+}
+
+abstract class StdevDistinctAggregatorTest extends CypherFunSuite with AggregatorTest {
+  test("should stdev DISTINCT numbers") {
+    val result = runAggregation(randomIntValuesWithNulls).asInstanceOf[NumberValue].doubleValue()
+    result should be(stdev(randomInts.distinct) +- 0.0001)
+  }
+}
