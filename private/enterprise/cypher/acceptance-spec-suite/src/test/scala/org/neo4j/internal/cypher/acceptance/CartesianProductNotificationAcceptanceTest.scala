031ba49ecb3b648288c7fe5f8a9328acc27f9c6c
fickludd <johan.teleman@neotechnology.com>
2020-06-16 14:03:41 +0200

    Add JavaCC parser as opt-in parser in Neo4j

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/CartesianProductNotificationAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/CartesianProductNotificationAcceptanceTest.scala
@@ -133,7 +133,8 @@ class CartesianProductNotificationAcceptanceTest extends CypherFunSuite with Gra
     csvBufferSize = CSVResources.DEFAULT_BUFFER_SIZE,
     nonIndexedLabelWarningThreshold = 10000L,
     planSystemCommands = false,
-    readPropertiesFromCursor = false
+    readPropertiesFromCursor = false,
+    useJavaCCParser = true
   )
   private lazy val monitors = WrappedMonitors(kernelMonitors)
   private val metricsFactory = CachedMetricsFactory(SimpleMetricsFactory)
