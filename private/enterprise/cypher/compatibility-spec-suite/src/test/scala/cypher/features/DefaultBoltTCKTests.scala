6be61b4bbbb930331bce484c989e5fe45086b9d7
Therese Magnusson <scout.therese@gmail.com>
2020-06-17 13:41:20 +0200

    Added support for running feature tests over Bolt (#6274)
    Added both acceptance and TCK test classes that uses the new
    infrastructure. All these tests now both run with embedded and over
    bolt.
    
    Also bumped the driver version to 4.0.2 (to fix flakeyness of new tests)
    
    Co-authored-by: Petr Janouch <petr.janouch@neotechnology.com>

---
+++ private/enterprise/cypher/compatibility-spec-suite/src/test/scala/cypher/features/DefaultBoltTCKTests.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
+package cypher.features
+
+import java.util
+
+import com.neo4j.test.TestEnterpriseDatabaseManagementServiceBuilder
+import cypher.features.ScenarioTestHelper.createTests
+import cypher.features.ScenarioTestHelper.printComputedBlacklist
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
+class DefaultBoltTCKTests extends EnterpriseBaseTCKTests {
+
+  // If you want to only run a specific feature or scenario, go to the EnterpriseBaseTCKTests
+
+  @TestFactory
+  def runDefault(): util.Collection[DynamicTest] = {
+    createTests(scenarios, DefaultTestConfig, () => new TestEnterpriseDatabaseManagementServiceBuilder(), enterpriseDefaultTestConfig, useBolt = true)
+  }
+
+  @Disabled
+  def generateBlacklistDefault(): Unit = {
+    printComputedBlacklist(scenarios, DefaultTestConfig, () => new TestEnterpriseDatabaseManagementServiceBuilder(), useBolt = true)
+    fail("Do not forget to add @Disabled to this method")
+  }
+}
