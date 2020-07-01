225e5a6f8d9b3d800bb70077e8ea97f15de0772e
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2020-07-01 10:58:02 +0200

    Add micro benchmark for NodeRightOuterHashJoin

---
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/NodeRightOuterHashJoin.scala
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is part of Neo4j internal tooling.
+ */
+package com.neo4j.bench.micro.benchmarks.cypher
+
+import com.neo4j.bench.jmh.api.config.BenchmarkEnabled
+import com.neo4j.bench.jmh.api.config.ParamValues
+import com.neo4j.bench.micro.Main
+import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.data.ConstantGenerator.constant
+import com.neo4j.bench.micro.data.DataGeneratorConfig
+import com.neo4j.bench.micro.data.DataGeneratorConfigBuilder
+import com.neo4j.bench.micro.data.DiscreteGenerator.Bucket
+import com.neo4j.bench.micro.data.DiscreteGenerator.discrete
+import com.neo4j.bench.micro.data.LabelKeyDefinition
+import com.neo4j.bench.micro.data.Plans.IdGen
+import com.neo4j.bench.micro.data.Plans.Pos
+import com.neo4j.bench.micro.data.Plans.astLabelToken
+import com.neo4j.bench.micro.data.Plans.astPropertyKeyToken
+import com.neo4j.bench.micro.data.Plans.astVariable
+import com.neo4j.bench.micro.data.PropertyDefinition
+import com.neo4j.bench.micro.data.ValueGeneratorUtil.INT
+import com.neo4j.bench.micro.data.ValueGeneratorUtil.LNG
+import org.neo4j.cypher.internal.ast.semantics.SemanticTable
+import org.neo4j.cypher.internal.expressions.SignedDecimalIntegerLiteral
+import org.neo4j.cypher.internal.logical.plans
+import org.neo4j.cypher.internal.logical.plans.DoNotGetValue
+import org.neo4j.cypher.internal.logical.plans.IndexOrderNone
+import org.neo4j.cypher.internal.logical.plans.IndexedProperty
+import org.neo4j.cypher.internal.logical.plans.ProduceResult
+import org.neo4j.cypher.internal.logical.plans.RightOuterHashJoin
+import org.neo4j.cypher.internal.logical.plans.SingleQueryExpression
+import org.neo4j.cypher.internal.planner.spi.PlanContext
+import org.neo4j.graphdb.Label
+import org.neo4j.kernel.impl.coreapi.InternalTransaction
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.TearDown
+import org.openjdk.jmh.infra.Blackhole
+
+@BenchmarkEnabled(true)
+class NodeRightOuterHashJoin extends AbstractCypherBenchmark {
+  @ParamValues(
+    allowed = Array(CompiledByteCode.NAME, CompiledSourceCode.NAME, Interpreted.NAME, Slotted.NAME, Pipelined.NAME, Parallel.NAME),
+    base = Array(Interpreted.NAME, Slotted.NAME, Pipelined.NAME))
+  @Param(Array[String]())
+  var runtime: String = _
+
+  @ParamValues(
+    allowed = Array("0.001", "0.01", "0.1"),
+    base = Array("0.01"))
+  @Param(Array[String]())
+  var selectivity: Double = _
+
+  override def description = "Node Right Outer Hash Join"
+
+  private val NODE_COUNT = 1000000
+  private val LABEL = Label.label("SampleLabel")
+  private val KEY = "key"
+  private val VALUE = 42
+  private lazy val expectedRowCount = NODE_COUNT
+
+  override protected def getConfig: DataGeneratorConfig = {
+    val buckets = List(
+      new Bucket(selectivity, constant(LNG, VALUE)),
+      new Bucket(1 - selectivity, constant(INT, 1)))
+    val property = new PropertyDefinition(KEY, discrete(buckets: _*))
+    new DataGeneratorConfigBuilder()
+      .withNodeCount(NODE_COUNT)
+      .withLabels(LABEL)
+      .withNodeProperties(property)
+      .withSchemaIndexes(new LabelKeyDefinition(LABEL, KEY))
+      .isReusableStore(true)
+      .build()
+  }
+
+  override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
+    val node = astVariable("node")
+    val nodeIdName = node.name
+    val queryExpression = SingleQueryExpression(SignedDecimalIntegerLiteral(VALUE.toString)(Pos))
+    val lhs = plans.NodeIndexSeek(
+      nodeIdName,
+      astLabelToken(LABEL, planContext),
+      List(IndexedProperty(astPropertyKeyToken(KEY, planContext), DoNotGetValue)),
+      queryExpression,
+      Set.empty,
+      IndexOrderNone)(IdGen)
+    val rhs = plans.AllNodesScan(nodeIdName, Set.empty)(IdGen)
+    val join = RightOuterHashJoin(Set(nodeIdName), lhs, rhs)(IdGen)
+    val resultColumns = List(node.name)
+    val produceResults = ProduceResult(join, resultColumns)(IdGen)
+
+    val table = SemanticTable().addNode(node)
+
+    (produceResults, table, resultColumns)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SampleTime))
+  def executePlan(threadState: NodeRightOuterHashJoinThreadState, bh: Blackhole): Long = {
+    val subscriber = new CountSubscriber(bh)
+    val result = threadState.executablePlan.execute(tx = threadState.tx, subscriber = subscriber)
+    result.consumeAll()
+    assertExpectedRowCount(expectedRowCount, subscriber)
+  }
+}
+
+@State(Scope.Thread)
+class NodeRightOuterHashJoinThreadState {
+  var tx: InternalTransaction = _
+  var executablePlan: ExecutablePlan = _
+
+  @Setup
+  def setUp(benchmarkState: NodeRightOuterHashJoin): Unit = {
+    executablePlan = benchmarkState.buildPlan(from(benchmarkState.runtime))
+    tx = benchmarkState.beginInternalTransaction()
+  }
+
+  @TearDown
+  def tearDown(): Unit = {
+    tx.close()
+  }
+}
+
+object NodeRightOuterHashJoin {
+  def main(args: Array[String]): Unit = {
+    Main.run(classOf[NodeRightOuterHashJoin])
+  }
+}
+
