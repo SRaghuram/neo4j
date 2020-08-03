2fb78b206df01dba9872c8f40581490167278231
Pontus Melke <pontusmelke@gmail.com>
2020-08-03 11:24:51 +0200

    Use array instead of Seq
    This is anyway turned into arrays at a later stage and this makes it easier to reuse from
    java and generated code.

--- private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/operators/ProcedureCallOperator.scala
+++ private/enterprise/cypher/pipelined-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/pipelined/operators/ProcedureCallOperator.scala
@@ -5,22 +5,55 @@
  */
 package org.neo4j.cypher.internal.runtime.pipelined.operators
 
+import org.neo4j.codegen.api.Field
+import org.neo4j.codegen.api.IntermediateRepresentation
+import org.neo4j.codegen.api.IntermediateRepresentation.and
+import org.neo4j.codegen.api.IntermediateRepresentation.arrayLoad
+import org.neo4j.codegen.api.IntermediateRepresentation.arrayOf
+import org.neo4j.codegen.api.IntermediateRepresentation.block
+import org.neo4j.codegen.api.IntermediateRepresentation.cast
+import org.neo4j.codegen.api.IntermediateRepresentation.condition
+import org.neo4j.codegen.api.IntermediateRepresentation.constant
+import org.neo4j.codegen.api.IntermediateRepresentation.field
+import org.neo4j.codegen.api.IntermediateRepresentation.getStatic
+import org.neo4j.codegen.api.IntermediateRepresentation.invoke
+import org.neo4j.codegen.api.IntermediateRepresentation.invokeSideEffect
+import org.neo4j.codegen.api.IntermediateRepresentation.invokeStatic
+import org.neo4j.codegen.api.IntermediateRepresentation.loadField
+import org.neo4j.codegen.api.IntermediateRepresentation.loop
+import org.neo4j.codegen.api.IntermediateRepresentation.method
+import org.neo4j.codegen.api.IntermediateRepresentation.noop
+import org.neo4j.codegen.api.IntermediateRepresentation.setField
+import org.neo4j.codegen.api.IntermediateRepresentation.staticConstant
+import org.neo4j.codegen.api.LocalVariable
+import org.neo4j.codegen.api.Method
+import org.neo4j.codegen.api.StaticField
+import org.neo4j.cypher.internal.expressions
 import org.neo4j.cypher.internal.logical.plans.ProcedureSignature
 import org.neo4j.cypher.internal.profiling.OperatorProfileEvent
 import org.neo4j.cypher.internal.runtime.ListSupport
 import org.neo4j.cypher.internal.runtime.ProcedureCallMode
 import org.neo4j.cypher.internal.runtime.QueryContext
 import org.neo4j.cypher.internal.runtime.ReadWriteRow
+import org.neo4j.cypher.internal.runtime.compiled.expressions.ExpressionCompilation.DB_ACCESS
+import org.neo4j.cypher.internal.runtime.compiled.expressions.ExpressionCompilation.nullCheckIfRequired
+import org.neo4j.cypher.internal.runtime.compiled.expressions.IntermediateExpression
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
+import org.neo4j.cypher.internal.runtime.pipelined.OperatorExpressionCompiler
 import org.neo4j.cypher.internal.runtime.pipelined.execution.Morsel
 import org.neo4j.cypher.internal.runtime.pipelined.execution.MorselFullCursor
 import org.neo4j.cypher.internal.runtime.pipelined.execution.PipelinedQueryState
 import org.neo4j.cypher.internal.runtime.pipelined.execution.QueryResources
+import org.neo4j.cypher.internal.runtime.pipelined.operators.OperatorCodeGenHelperTemplates.CURSOR_POOL_V
+import org.neo4j.cypher.internal.runtime.pipelined.operators.OperatorCodeGenHelperTemplates.profileRow
 import org.neo4j.cypher.internal.runtime.pipelined.operators.ProcedureCallOperator.createProcedureCallContext
+import org.neo4j.cypher.internal.runtime.pipelined.procedures.Procedures
 import org.neo4j.cypher.internal.runtime.pipelined.state.ArgumentStateMap.ArgumentStateMaps
 import org.neo4j.cypher.internal.runtime.pipelined.state.Collections.singletonIndexedSeq
 import org.neo4j.cypher.internal.runtime.pipelined.state.MorselParallelizer
 import org.neo4j.cypher.internal.runtime.scheduling.WorkIdentity
+import org.neo4j.cypher.internal.util.attribution.Id
+import org.neo4j.exceptions.CantCompileQueryException
 import org.neo4j.internal.kernel.api.procs.ProcedureCallContext
 import org.neo4j.values.AnyValue
 
@@ -63,7 +96,7 @@ class ProcedureCallMiddleOperator(val workIdentity: WorkIdentity,
     val queryState = state.queryStateForExpressionEvaluation(resources)
     val readCursor = morsel.readCursor()
     while (readCursor.next()) {
-      val argValues = argExprs.map(arg => arg(readCursor, queryState))
+      val argValues = argExprs.map(arg => arg(readCursor, queryState)).toArray
       callMode.callProcedure(state.query, signature.id, argValues, createProcedureCallContext(state.query, originalVariables))
     }
   }
@@ -82,7 +115,7 @@ class ProcedureCallTask(inputMorsel: Morsel,
 
     val queryState = state.queryStateForExpressionEvaluation(resources)
     initExecutionContext.copyFrom(inputCursor, inputMorsel.longsPerRow, inputMorsel.refsPerRow)
-    val argValues = argExprs.map(arg => arg(initExecutionContext, queryState))
+    val argValues = argExprs.map(arg => arg(initExecutionContext, queryState)).toArray
     iterator = callMode.callProcedure(state.query, signature.id, argValues, createProcedureCallContext(state.query, originalVariables))
     true
   }
