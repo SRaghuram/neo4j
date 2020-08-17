dff8ed1015b805cc6f2bfc5a0743180346c54708
Jarosław Pałka <jaroslaw.palka@neotechnology.com>
2020-08-17 09:55:12 +0200

    fixed wrong formatting
    (cherry picked from commit c9d944c095fc00f3a7dc3d2ca797320e6dbf4cc9)
    (cherry picked from commit fe9ad44cc7e9f7ba70e9b45dd92b5b0cbfda6a2f)

--- private/benchmarks/macro/src/test/java/com/neo4j/bench/macro/workload/WorkloadTest.java
+++ private/benchmarks/macro/src/test/java/com/neo4j/bench/macro/workload/WorkloadTest.java
@@ -64,8 +64,8 @@ class WorkloadTest
     }
 
     @Test
-        // NOTE: test is a bit weak because, e.g., a query may contain some mutating clause name in a string, which is totally valid.
-        //       that is not the case for any query existing at this time, however, and having this sanity may protect us from quietly doing dumb things in future.
+    // NOTE: test is a bit weak because, e.g., a query may contain some mutating clause name in a string, which is totally valid.
+    //       that is not the case for any query existing at this time, however, and having this sanity may protect us from quietly doing dumb things in future.
     void shouldAlwaysMarkMutatingQueriesAsMutating()
     {
         try ( Resources resources = new Resources( temporaryFolder.absolutePath().toPath() ) )
@@ -91,8 +91,8 @@ class WorkloadTest
     }
 
     @Test
-        // NOTE: test is a bit weak because, e.g., a query may contain some mutating clause name in a string, which is totally valid.
-        //       that is not the case for any query existing at this time, however, and having this sanity may protect us from quietly doing dumb things in future.
+    // NOTE: test is a bit weak because, e.g., a query may contain some mutating clause name in a string, which is totally valid.
+    //       that is not the case for any query existing at this time, however, and having this sanity may protect us from quietly doing dumb things in future.
     void shouldNeverHaveMutatingWarmupQueries()
     {
         try ( Resources resources = new Resources( temporaryFolder.absolutePath().toPath() ) )
