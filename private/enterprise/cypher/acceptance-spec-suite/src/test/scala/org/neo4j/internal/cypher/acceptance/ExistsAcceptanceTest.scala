418fc0a71580da97c524bf19dac201ff22516970
Satia Herfert <satiaherfert@gmx.de>
2020-08-31 16:31:10 +0200

    Sort predicates in Selection by (cost, selectivity).
    This heuristic makes sure to evaluate costly predicates last.
    It also tries to trim to apply more selective predicates with
    the same cost first.
    
    This heuristic will hopefully be good enough in most cases,
    but to find the optimal ordering in all cases, some search
    algorithm is needed.

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ExistsAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ExistsAcceptanceTest.scala
@@ -931,7 +931,7 @@ class ExistsAcceptanceTest extends ExecutionEngineFunSuite with CypherComparison
     val plan = result.executionPlanDescription()
     plan should includeSomewhere.aPlan("SemiApply")
     plan should includeSomewhere.aPlan("Filter").containingArgument("cache[person.name] = $autostring_0")
-    plan should includeSomewhere.aPlan("Filter").containingArgument("dog:Dog AND dog.name = $autostring_0 AND dog.lastname = $autostring_0")
+    plan should includeSomewhere.aPlan("Filter").containingArgument("dog.name = $autostring_0 AND dog.lastname = $autostring_0 AND dog:Dog")
   }
 
   test("Should handle scoping and dependencies properly when subclause is in horizon") {
