8af72f669557cb5072b623e16ab022369e76a8dd
Mattias Finné <mattias@neotechnology.com>
2020-09-27 23:31:18 +0200

    Avoids System.out logging in consistency-it tests
    This allows for concurrent execution of those tests

--- private/enterprise/enterprise-it/consistency-it/src/test/java/com/neo4j/consistency/ConsistencyCheckServiceRecordFormatIT.java
+++ private/enterprise/enterprise-it/consistency-it/src/test/java/com/neo4j/consistency/ConsistencyCheckServiceRecordFormatIT.java
@@ -7,12 +7,12 @@ package com.neo4j.consistency;
 
 import com.neo4j.kernel.impl.store.format.highlimit.HighLimit;
 import com.neo4j.test.TestEnterpriseDatabaseManagementServiceBuilder;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.parallel.ResourceLock;
-import org.junit.jupiter.api.parallel.Resources;
+import org.assertj.core.description.Description;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.neo4j.configuration.Config;
@@ -22,22 +22,19 @@ import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.internal.helpers.progress.ProgressMonitorFactory;
 import org.neo4j.io.layout.DatabaseLayout;
 import org.neo4j.kernel.impl.store.format.aligned.PageAlignedV4_1;
 import org.neo4j.kernel.impl.store.format.standard.Standard;
-import org.neo4j.logging.log4j.Log4jLogProvider;
+import org.neo4j.logging.AssertableLogProvider;
 import org.neo4j.test.extension.Inject;
 import org.neo4j.test.extension.Neo4jLayoutExtension;
-import org.neo4j.test.extension.SuppressOutputExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 import static org.neo4j.configuration.GraphDatabaseSettings.record_format;
+import static org.neo4j.internal.helpers.progress.ProgressMonitorFactory.NONE;
 
 @Neo4jLayoutExtension
-@ExtendWith( SuppressOutputExtension.class )
-@ResourceLock( Resources.SYSTEM_OUT )
 class ConsistencyCheckServiceRecordFormatIT
 {
     @Inject
@@ -90,8 +87,20 @@ class ConsistencyCheckServiceRecordFormatIT
     private static void assertConsistentStore( DatabaseLayout databaseLayout ) throws Exception
     {
         ConsistencyCheckService service = new ConsistencyCheckService();
-        ConsistencyCheckService.Result result = service.runFullConsistencyCheck( databaseLayout, Config.defaults(),
-                ProgressMonitorFactory.textual( System.out ), new Log4jLogProvider( System.out ), true );
-        assertTrue( result.isSuccessful(), "Store is inconsistent" );
+        AssertableLogProvider logProvider = new AssertableLogProvider();
+        ConsistencyCheckService.Result result = service.runFullConsistencyCheck( databaseLayout, Config.defaults(), NONE, logProvider, false );
+        assertThat( result.isSuccessful() ).as( new Description()
+        {
+            @Override
+            public String value()
+            {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                try ( PrintStream stream = new PrintStream( out ) )
+                {
+                    logProvider.print( stream );
+                }
+                return out.toString();
+            }
+        } ).isTrue();
     }
 }
