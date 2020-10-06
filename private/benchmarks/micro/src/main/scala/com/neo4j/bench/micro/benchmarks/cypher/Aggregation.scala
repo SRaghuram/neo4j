5735452710cc5482ca98a18bec33ffd942f4d2de
Louise Söderström <louise.matematik@gmail.com>
2020-10-06 11:36:03 +0200

    Add security to Aggregation micro benchmark (#7394)
    * Add security to Aggregation micro benchmark
    
    The countstore cannot be used for restricted users, so for
    them count() is much slower and the performance of any
    changes needs to be tracked.

--- private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/Aggregation.scala
+++ private/benchmarks/micro/src/main/scala/com/neo4j/bench/micro/benchmarks/cypher/Aggregation.scala
@@ -5,9 +5,12 @@
  */
 package com.neo4j.bench.micro.benchmarks.cypher
 
+import com.neo4j.bench.common.Neo4jConfigBuilder
 import com.neo4j.bench.jmh.api.config.BenchmarkEnabled
 import com.neo4j.bench.jmh.api.config.ParamValues
 import com.neo4j.bench.micro.benchmarks.cypher.CypherRuntime.from
+import com.neo4j.bench.micro.data.DataGeneratorConfig
+import com.neo4j.bench.micro.data.DataGeneratorConfigBuilder
 import com.neo4j.bench.micro.data.Plans.IdGen
 import com.neo4j.bench.micro.data.Plans.astFunctionInvocation
 import com.neo4j.bench.micro.data.Plans.astParameter
@@ -18,6 +21,7 @@ import com.neo4j.bench.micro.data.TypeParamValues.LNG
 import com.neo4j.bench.micro.data.TypeParamValues.STR_SML
 import com.neo4j.bench.micro.data.TypeParamValues.listOf
 import com.neo4j.bench.micro.data.TypeParamValues.mapValuesOfList
+import org.neo4j.configuration.GraphDatabaseSettings
 import org.neo4j.cypher.internal.ast.ASTAnnotationMap
 import org.neo4j.cypher.internal.ast.semantics.ExpressionTypeInfo
 import org.neo4j.cypher.internal.ast.semantics.SemanticTable
@@ -52,12 +56,33 @@ class Aggregation extends AbstractCypherBenchmark {
   @Param(Array[String]())
   var propertyType: String = _
 
+  @ParamValues(
+    allowed = Array("true", "false"),
+    base = Array("true")
+  )
+  @Param(Array[String]())
+  var auth: Boolean = _
+
+  @ParamValues(
+    allowed = Array("full", "white", "black"),
+    base = Array("full", "white", "black")
+  )
+  @Param(Array[String]())
+  var user: String = _
+
   override def description = "Aggregation only, e.g., MATCH (n) RETURN count(n)"
 
   val VALUE_COUNT = 1000000
   val EXPECTED_ROW_COUNT = 1
 
   var params: MapValue = _
+
+  override protected def getConfig: DataGeneratorConfig =
+    new DataGeneratorConfigBuilder()
+      .isReusableStore(true)
+      .withNeo4jConfig(Neo4jConfigBuilder.empty()
+        .withSetting(GraphDatabaseSettings.auth_enabled, auth.toString).build())
+      .build();
 
   override def getLogicalPlanAndSemanticTable(planContext: PlanContext): (plans.LogicalPlan, SemanticTable, List[String]) = {
     val listElementType = cypherTypeFor(propertyType)
@@ -95,7 +120,7 @@ class AggregationThreadState {
   def setUp(benchmarkState: Aggregation): Unit = {
     benchmarkState.params = mapValuesOfList("list", listOf(benchmarkState.propertyType, benchmarkState.VALUE_COUNT))
     executablePlan = benchmarkState.buildPlan(from(benchmarkState.runtime))
-    tx = benchmarkState.beginInternalTransaction()
+    tx = benchmarkState.beginInternalTransaction(benchmarkState.users(benchmarkState.user))
   }
 
   @TearDown
