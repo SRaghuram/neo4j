4434b9db59c8a5b94349f3df04cabd8ba671c0a9
Fábio Botelho <fabio.botelho@neotechnology.com>
2020-10-07 17:50:48 +0100

    Warn for multi dc custom lb conflict

--- private/enterprise/causal-clustering/src/main/java/com/neo4j/causalclustering/core/CoreRoutingProcedureInstaller.java
+++ private/enterprise/causal-clustering/src/main/java/com/neo4j/causalclustering/core/CoreRoutingProcedureInstaller.java
@@ -14,29 +14,36 @@ import com.neo4j.causalclustering.routing.load_balancing.procedure.GetRoutingTab
 import com.neo4j.configuration.CausalClusteringSettings;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import org.neo4j.configuration.Config;
 import org.neo4j.dbms.database.DatabaseManager;
 import org.neo4j.kernel.api.procedure.CallableProcedure;
+import org.neo4j.logging.Log;
 import org.neo4j.logging.LogProvider;
 import org.neo4j.procedure.builtin.routing.BaseRoutingProcedureInstaller;
 
 public class CoreRoutingProcedureInstaller extends BaseRoutingProcedureInstaller
 {
+    static final String MULTI_DC_NOT_ENABLED_WARN = "Custom load balancing policy/plugin provided but multi dc not enabled";
+
     private final TopologyService topologyService;
     private final LeaderService leaderService;
     private final DatabaseManager<?> databaseManager;
     private final Config config;
     private final LogProvider logProvider;
+    private final Log log;
 
     public CoreRoutingProcedureInstaller( TopologyService topologyService, LeaderService leaderService, DatabaseManager<?> databaseManager, Config config,
-            LogProvider logProvider )
+                                          LogProvider logProvider )
     {
         this.topologyService = topologyService;
         this.leaderService = leaderService;
         this.databaseManager = databaseManager;
         this.config = config;
         this.logProvider = logProvider;
+        this.log = logProvider.getLog( getClass() );
     }
 
     @Override
@@ -49,8 +56,24 @@ public class CoreRoutingProcedureInstaller extends BaseRoutingProcedureInstaller
         }
         else
         {
+            if ( customBalancingConfigured() )
+            {
+                log.warn( MULTI_DC_NOT_ENABLED_WARN );
+            }
             return new GetRoutingTableProcedureForSingleDC( namespace, topologyService, leaderService, databaseManager, config, logProvider );
         }
+    }
+
+    private boolean customBalancingConfigured()
+    {
+        var lbPlugin = config.get( CausalClusteringSettings.load_balancing_plugin );
+        var lbDefault = CausalClusteringSettings.load_balancing_plugin.defaultValue();
+        var customLBRequested = !Objects.equals( lbDefault, lbPlugin );
+
+        var groups = config.getGroups( LoadBalancingServerPoliciesGroup.class );
+        var customServerPolicyConfigured = !groups.isEmpty();
+
+        return customLBRequested || customServerPolicyConfigured;
     }
 
     private LoadBalancingProcessor loadLoadBalancingProcessor()
--- private/enterprise/causal-clustering/src/test/java/com/neo4j/causalclustering/core/CoreRoutingProcedureInstallerTest.java
+++ private/enterprise/causal-clustering/src/test/java/com/neo4j/causalclustering/core/CoreRoutingProcedureInstallerTest.java
@@ -8,11 +8,14 @@ package com.neo4j.causalclustering.core;
 import com.neo4j.causalclustering.common.StubClusteredDatabaseManager;
 import com.neo4j.causalclustering.discovery.TopologyService;
 import com.neo4j.causalclustering.routing.load_balancing.LeaderService;
+import com.neo4j.causalclustering.routing.load_balancing.filters.IdentityFilter;
+import com.neo4j.causalclustering.routing.load_balancing.plugins.server_policies.ServerInfo;
 import com.neo4j.causalclustering.routing.load_balancing.procedure.GetRoutingTableProcedureForMultiDC;
 import com.neo4j.causalclustering.routing.load_balancing.procedure.GetRoutingTableProcedureForSingleDC;
 import com.neo4j.configuration.CausalClusteringSettings;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.neo4j.configuration.Config;
@@ -20,9 +23,12 @@ import org.neo4j.internal.kernel.api.exceptions.ProcedureException;
 import org.neo4j.internal.kernel.api.procs.ProcedureSignature;
 import org.neo4j.internal.kernel.api.procs.QualifiedName;
 import org.neo4j.kernel.api.procedure.GlobalProcedures;
+import org.neo4j.logging.AssertableLogProvider;
+import org.neo4j.logging.LogProvider;
 import org.neo4j.logging.NullLogProvider;
 import org.neo4j.procedure.impl.GlobalProceduresRegistry;
 
+import static com.neo4j.causalclustering.core.CoreRoutingProcedureInstaller.MULTI_DC_NOT_ENABLED_WARN;
 import static java.util.stream.Collectors.toSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,6 +36,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.neo4j.logging.LogAssertions.assertThat;
 
 class CoreRoutingProcedureInstallerTest
 {
@@ -39,7 +46,7 @@ class CoreRoutingProcedureInstallerTest
         var config = newConfig( false );
         var procedures = spy( new GlobalProceduresRegistry() );
 
-        installRoutingProcedures( config, procedures );
+        installRoutingProcedures( config, procedures, NullLogProvider.getInstance() );
 
         verifyRegisteredProcedureNames( procedures );
         verify( procedures, times( 2 ) ).register( any( GetRoutingTableProcedureForSingleDC.class ) );
@@ -51,7 +58,7 @@ class CoreRoutingProcedureInstallerTest
         var config = newConfig( true );
         var procedures = spy( new GlobalProceduresRegistry() );
 
-        installRoutingProcedures( config, procedures );
+        installRoutingProcedures( config, procedures, NullLogProvider.getInstance() );
 
         verifyRegisteredProcedureNames( procedures );
         verify( procedures, times( 2 ) ).register( any( GetRoutingTableProcedureForMultiDC.class ) );
@@ -62,13 +69,13 @@ class CoreRoutingProcedureInstallerTest
         return Config.defaults( CausalClusteringSettings.multi_dc_license, multiDC );
     }
 
-    private static void installRoutingProcedures( Config config, GlobalProcedures procedures ) throws ProcedureException
+    private static void installRoutingProcedures( Config config, GlobalProcedures procedures, LogProvider logProvider ) throws ProcedureException
     {
         var topologyService = mock( TopologyService.class );
         var leaderService = mock( LeaderService.class );
 
         var installer = new CoreRoutingProcedureInstaller( topologyService, leaderService, new StubClusteredDatabaseManager(), config,
-                NullLogProvider.getInstance() );
+                                                           logProvider );
         installer.install( procedures );
     }
 
@@ -79,10 +86,46 @@ class CoreRoutingProcedureInstallerTest
                 new QualifiedName( new String[]{"dbms", "cluster", "routing"}, "getRoutingTable" ) );
 
         var actualNames = procedures.getAllProcedures()
-                .stream()
-                .map( ProcedureSignature::name )
-                .collect( toSet() );
+                                    .stream()
+                                    .map( ProcedureSignature::name )
+                                    .collect( toSet() );
 
         assertEquals( expectedNames, actualNames );
+    }
+
+    @Test
+    void shouldWarnIfCustomLoadBalancerRequestedForSingleDC() throws ProcedureException
+    {
+        var config = Config.defaults(
+                Map.of( CausalClusteringSettings.multi_dc_license, false,
+                        CausalClusteringSettings.load_balancing_plugin, "some plugin"
+                )
+        );
+
+        var procedures = new GlobalProceduresRegistry();
+
+        var logger = new AssertableLogProvider();
+        installRoutingProcedures( config, procedures, logger );
+        assertThat( logger )
+                .forLevel( AssertableLogProvider.Level.WARN )
+                .containsMessages( MULTI_DC_NOT_ENABLED_WARN );
+    }
+
+    @Test
+    void shouldWarnIfCustomServerPolicyProvidedButSingleDC() throws ProcedureException
+    {
+        var config = Config.defaults(
+                Map.of( CausalClusteringSettings.multi_dc_license, false,
+                        LoadBalancingServerPoliciesGroup.group( "default" ).value, IdentityFilter.<ServerInfo>as()
+                )
+        );
+
+        var procedures = new GlobalProceduresRegistry();
+
+        var logger = new AssertableLogProvider();
+        installRoutingProcedures( config, procedures, logger );
+        assertThat( logger )
+                .forLevel( AssertableLogProvider.Level.WARN )
+                .containsMessages( MULTI_DC_NOT_ENABLED_WARN );
     }
 }
