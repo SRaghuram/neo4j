31827180dac417e8948227d980d61f955cd3a38f
Sascha Peukert <sascha.peukert@neotechnology.com>
2020-11-04 15:34:47 +0100

    Provide better feedback on erroneous property value

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/HelpfulErrorMessagesTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/HelpfulErrorMessagesTest.scala
@@ -291,4 +291,9 @@ class HelpfulErrorMessagesTest extends ExecutionEngineFunSuite with CypherCompar
       tx.execute("CYPHER runtime=slotted expressionEngine=compiled RETURN 1").resultAsString() should not be null
     )
   }
+
+  test("should throw sensible and helpful error message on wrong property value type") {
+    failWithError(Configs.InterpretedAndSlotted,"CREATE (n{prop:$param})",
+      "Property values can only be of primitive types or arrays thereof. Encountered: Map{A -> String(\"B\")}.", Map("param"-> Map("A" -> "B")))
+  }
 }
