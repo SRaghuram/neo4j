743ffbb16c4f34d832c0d8823bc9212f2cd25a83
Pontus Melke <pontusmelke@gmail.com>
2020-11-12 13:31:56 +0100

    Fix assertion and make sure tests run in CI
    The test had an invalid assertion comparing `ListValue` and `AnyValue[]`.
    This hasn't been noticed because we only run files called `*Test` or `*IT`.

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/TextValueSpecificationTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/TextValueSpecificationTest.scala
@@ -11,13 +11,14 @@ import org.neo4j.values.storable.TextValue
 import org.neo4j.values.storable.Values.stringArray
 import org.neo4j.values.storable.Values.stringValue
 import org.neo4j.values.storable.Values.utf8Value
+import org.neo4j.values.virtual.VirtualValues.fromArray
 import org.scalacheck.Arbitrary
 import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
 import org.scalatest.prop.Configuration
 
-object TextValueSpecification extends Properties("TextValue") with Configuration {
+object TextValueSpecificationTest extends Properties("TextValue") with Configuration {
 
   private val substringGen = for {
     string <- Arbitrary.arbitrary[String]
@@ -97,10 +98,10 @@ object TextValueSpecification extends Properties("TextValue") with Configuration
       val utf8 = utf8Value(x.getBytes(StandardCharsets.UTF_8))
       val split = x.split(find)
       if (x != find) {
-        stringArray(split: _*) == value.split(find).asArray() &&
+        fromArray(stringArray(split: _*)) == value.split(find) &&
           value.split(find) == utf8.split(find)
       } else {
-        value.split(find) == utf8.split(find) && value.split(find).asArray() == stringArray("", "")
+        value.split(find) == utf8.split(find) && value.split(find) == fromArray(stringArray("", ""))
       }
 
   }
@@ -134,7 +135,7 @@ object TextValueSpecification extends Properties("TextValue") with Configuration
         utf8Value(string.getBytes(StandardCharsets.UTF_8)).substring(start, length))
   }
 
-  implicit override val generatorDrivenConfig: TextValueSpecification.PropertyCheckConfiguration =
+  implicit override val generatorDrivenConfig: TextValueSpecificationTest.PropertyCheckConfiguration =
     PropertyCheckConfig(minSuccessful = 1000)
 
   private def equivalent(t1: TextValue, t2: TextValue) =
