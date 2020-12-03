e3a837d1e4c4058406996e6af249c86037baaa4f
Jarosław Pałka <jaroslaw.palka@neotechnology.com>
2020-12-03 19:23:11 +0100

    migrated tests to Junit5 & bumped benchmark-infra-deployment version
    (cherry picked from commit 41fde862427c202f6b25c1d9dfcdc82aa9f68652)
    (cherry picked from commit a154d94af5a4b38843ef39b06d3b6c698539d4da)
    (cherry picked from commit 739bc89440af51011ea97646a8299b36cc4d0d1f)
    (cherry picked from commit c6451f484bea970ef48d8ace0c99b1122532be39)

--- private/benchmarks/benchmark-infra/benchmark-infra-common/src/test/java/com/neo4j/bench/infra/InfraNamesHelperTest.java
+++ private/benchmarks/benchmark-infra/benchmark-infra-common/src/test/java/com/neo4j/bench/infra/InfraNamesHelperTest.java
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  * This file is part of Neo4j internal tooling.
  */
 package com.neo4j.bench.infra;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InfraNamesHelperTest
 {
--- private/benchmarks/benchmark-infra/benchmark-infra-deployment/src/test/java/com/neo4j/bench/infra/deployment/BatchInfrastructureTest.java
+++ private/benchmarks/benchmark-infra/benchmark-infra-deployment/src/test/java/com/neo4j/bench/infra/deployment/BatchInfrastructureTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  * This file is part of Neo4j internal tooling.
  */
 package com.neo4j.bench.infra.deployment;
 
 import com.neo4j.bench.model.util.JsonUtil;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.ec2.model.InstanceType;
 
 import java.util.Collections;
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BatchInfrastructureTest
 {
