56dce4f0f10e9002421452495efae955bbd51e04
Henrik Nyman <henrik.nyman@neotechnology.com>
2020-12-18 11:49:58 +0100

    Fix license headers

--- private/enterprise/cypher/pipelined-runtime/src/main/java/org/neo4j/cypher/internal/runtime/pipelined/operators/CompiledStreamingOperatorWithMorsel.java
+++ private/enterprise/cypher/pipelined-runtime/src/main/java/org/neo4j/cypher/internal/runtime/pipelined/operators/CompiledStreamingOperatorWithMorsel.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
 package org.neo4j.cypher.internal.runtime.pipelined.operators;
 
 /**
