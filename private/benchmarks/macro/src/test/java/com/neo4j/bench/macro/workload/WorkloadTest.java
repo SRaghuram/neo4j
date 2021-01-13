ee000d7dbce2337cfafe2646b7ec24748bae94fc
eebus <simpan.svensson@gmail.com>
2021-01-13 15:08:41 +0100

    removed comma
    (cherry picked from commit 9c6f66f0b49fb21382459df197fe6d6600d3db49)

--- private/benchmarks/macro/src/test/java/com/neo4j/bench/macro/workload/WorkloadTest.java
+++ private/benchmarks/macro/src/test/java/com/neo4j/bench/macro/workload/WorkloadTest.java
@@ -550,7 +550,7 @@ class WorkloadTest
     @Test
     public void queriesNamesShouldNotHaveCommas() throws IOException
     {
-        try ( Resources resources = new Resources( temporaryFolder.newFolder().toPath() ) )
+        try ( Resources resources = new Resources( temporaryFolder.absolutePath().toPath() ) )
         {
             for ( Workload workload : Workload.all( resources, Deployment.embedded() ) )
             {
