853b2929c3b61e5ac56bebbfd69bc62d96bd027a
Mattias Finné <mattias@neotechnology.com>
2021-01-29 15:11:11 +0100

    A test for rolling upgrade through dense node transactions

--- private/enterprise/enterprise-it/causalclustering-it/src/test/java/com/neo4j/upgrade/TransactionLogUpgradeIT.java
+++ private/enterprise/enterprise-it/causalclustering-it/src/test/java/com/neo4j/upgrade/TransactionLogUpgradeIT.java
@@ -10,7 +10,8 @@ import com.neo4j.causalclustering.common.ClusterMember;
 import com.neo4j.test.causalclustering.ClusterConfig;
 import com.neo4j.test.causalclustering.ClusterExtension;
 import com.neo4j.test.causalclustering.ClusterFactory;
-import org.agrona.collections.MutableBoolean;
+import org.apache.commons.lang3.mutable.MutableBoolean;
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,16 +22,27 @@ import org.junit.jupiter.api.parallel.Resources;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 import org.neo4j.dbms.database.DbmsRuntimeRepository;
 import org.neo4j.dbms.database.DbmsRuntimeVersion;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.internal.kernel.api.security.LoginContext;
 import org.neo4j.internal.recordstorage.Command;
 import org.neo4j.io.pagecache.tracing.cursor.PageCursorTracer;
 import org.neo4j.kernel.KernelVersion;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.impl.coreapi.InternalTransaction;
 import org.neo4j.kernel.impl.store.MetaDataStore;
 import org.neo4j.kernel.impl.transaction.CommittedTransactionRepresentation;
 import org.neo4j.kernel.impl.transaction.log.LogicalTransactionStore;
@@ -47,6 +59,7 @@ import static com.neo4j.causalclustering.common.CausalClusteringTestHelpers.asse
 import static com.neo4j.causalclustering.common.CausalClusteringTestHelpers.startDatabase;
 import static com.neo4j.causalclustering.common.CausalClusteringTestHelpers.stopDatabase;
 import static java.lang.Integer.max;
+import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_METHOD;
@@ -54,6 +67,7 @@ import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAM
 import static org.neo4j.configuration.GraphDatabaseSettings.SYSTEM_DATABASE_NAME;
 import static org.neo4j.dbms.database.ComponentVersion.DBMS_RUNTIME_COMPONENT;
 import static org.neo4j.dbms.database.SystemGraphComponent.VERSION_LABEL;
+import static org.neo4j.test.Race.throwing;
 import static org.neo4j.test.assertion.Assert.assertEventually;
 import static org.neo4j.test.conditions.Conditions.equalityCondition;
 
@@ -149,7 +163,6 @@ class TransactionLogUpgradeIT
         //Then
         assertRuntimeVersion( DbmsRuntimeVersion.V4_2 );
         assertKernelVersion( KernelVersion.V4_2 );
-        doWriteTransaction();
 
         //When
         Race race = new Race().withRandomStartDelays().withEndCondition( () -> isKernelVersion( KernelVersion.LATEST ) );
@@ -157,7 +170,7 @@ class TransactionLogUpgradeIT
         {
             systemDb.executeTransactionally( "CALL dbms.upgrade()" );
         }, 1 );
-        race.addContestants( max( Runtime.getRuntime().availableProcessors() - 1, 2 ), Race.throwing( () -> {
+        race.addContestants( max( Runtime.getRuntime().availableProcessors() - 1, 2 ), throwing( () -> {
             doWriteTransaction();
             Thread.sleep( ThreadLocalRandom.current().nextInt( 0, 2 ) );
         } ) );
@@ -167,6 +180,128 @@ class TransactionLogUpgradeIT
         assertRuntimeVersion( DbmsRuntimeVersion.LATEST_DBMS_RUNTIME_COMPONENT_VERSION );
         assertKernelVersion( KernelVersion.LATEST );
         assertUpgradeTransactionInOrder( KernelVersion.V4_2, KernelVersion.LATEST, startTransaction );
+    }
+
+    @Test
+    void shouldUpgradeKernelVersionInTransactionOnDenseNodeOnRuntimeUpgradeStressTest() throws Throwable
+    {
+        //Given
+        setRuntimeVersion( DbmsRuntimeVersion.V4_2 );
+        setKernelVersion( KernelVersion.V4_2 );
+        restartDatabase();
+        long startTransaction = cluster.awaitLeader().resolveDependency( DEFAULT_DATABASE_NAME, TransactionIdStore.class ).getLastCommittedTransactionId();
+
+        //Then
+        assertRuntimeVersion( DbmsRuntimeVersion.V4_2 );
+        assertKernelVersion( KernelVersion.V4_2 );
+        long nodeId = createDenseNode();
+
+        //When
+        Race race = new Race().withRandomStartDelays().withEndCondition( new BooleanSupplier()
+        {
+            private final AtomicLong timeOfUpgrade = new AtomicLong();
+
+            @Override
+            public boolean getAsBoolean()
+            {
+                if ( isKernelVersion( KernelVersion.V4_3_D3 ) )
+                {
+                    // Notice the time of upgrade...
+                    timeOfUpgrade.compareAndSet( 0, currentTimeMillis() );
+                }
+                // ... and continue a little while after it happened so that we get transactions both before and after
+                return timeOfUpgrade.get() != 0 && (currentTimeMillis() - timeOfUpgrade.get()) > 1_000;
+            }
+        } );
+        race.addContestant( throwing( () ->
+        {
+            Thread.sleep( ThreadLocalRandom.current().nextInt( 0, 1_000 ) );
+            systemDb.executeTransactionally( "CALL dbms.upgrade()" );
+        } ), 1 );
+        race.addContestants( max( Runtime.getRuntime().availableProcessors() - 1, 2 ), throwing( () -> {
+            addRelationshipToNode( nodeId );
+            Thread.sleep( ThreadLocalRandom.current().nextInt( 0, 2 ) );
+        } ) );
+        race.go( 1, TimeUnit.MINUTES );
+
+        //Then
+        assertRuntimeVersion( DbmsRuntimeVersion.LATEST_DBMS_RUNTIME_COMPONENT_VERSION );
+        assertKernelVersion( KernelVersion.LATEST );
+        assertUpgradeTransactionInOrder( KernelVersion.V4_2, KernelVersion.LATEST, startTransaction );
+        assertDegrees( nodeId );
+    }
+
+    private void assertDegrees( long nodeId )
+    {
+        // Why assert degrees specifically? This particular upgrade: V4_2 -> V4_3_D3 changes how dense node degrees are stored
+        // so it's a really good indicator that everything there works
+        Map<String,Map<Direction,MutableLong>> referenceActualDegrees = null;
+        for ( ClusterMember member : cluster.coreMembers() )
+        {
+            try ( InternalTransaction tx = member.database( DEFAULT_DATABASE_NAME ).beginTransaction(
+                    KernelTransaction.Type.EXPLICIT, LoginContext.AUTH_DISABLED ) )
+            {
+                Node node = tx.getNodeById( nodeId );
+                Map<String,Map<Direction,MutableLong>> actualDegrees = new HashMap<>();
+                node.getRelationships().forEach(
+                        r -> actualDegrees.computeIfAbsent( r.getType().name(), t -> new HashMap<>() ).computeIfAbsent( directionOf( node, r ),
+                                d -> new MutableLong() ).increment() );
+                MutableLong actualTotalDegree = new MutableLong();
+                actualDegrees.forEach( ( typeName, directions ) ->
+                {
+                    long actualTotalDirectionDegree = 0;
+                    for ( Map.Entry<Direction,MutableLong> actualDirectionDegree : directions.entrySet() )
+                    {
+                        assertThat( node.getDegree( RelationshipType.withName( typeName ), actualDirectionDegree.getKey() ) ).isEqualTo(
+                                actualDirectionDegree.getValue().longValue() );
+                        actualTotalDirectionDegree += actualDirectionDegree.getValue().longValue();
+                    }
+                    assertThat( node.getDegree( RelationshipType.withName( typeName ) ) ).isEqualTo( actualTotalDirectionDegree );
+                    actualTotalDegree.add( actualTotalDirectionDegree );
+                } );
+                assertThat( node.getDegree() ).isEqualTo( actualTotalDegree.longValue() );
+
+                if ( referenceActualDegrees == null )
+                {
+                    referenceActualDegrees = actualDegrees;
+                }
+                else
+                {
+                    assertThat( actualDegrees ).isEqualTo( referenceActualDegrees );
+                }
+            }
+        }
+    }
+
+    private Direction directionOf( Node node, Relationship relationship )
+    {
+        return relationship.getStartNode().equals( node ) ? relationship.getEndNode().equals( node ) ? Direction.BOTH : Direction.OUTGOING : Direction.INCOMING;
+    }
+
+    private long createDenseNode() throws Exception
+    {
+        MutableLong nodeId = new MutableLong();
+        cluster.coreTx( ( db, transaction ) ->
+        {
+            Node node = transaction.createNode();
+            for ( int i = 0; i < 100; i++ )
+            {
+                node.createRelationshipTo( transaction.createNode(), RelationshipType.withName( "TYPE_" + (i % 3) ) );
+            }
+            nodeId.setValue( node.getId() );
+            transaction.commit();
+        } );
+        return nodeId.longValue();
+    }
+
+    private void addRelationshipToNode( long nodeId ) throws Exception
+    {
+        cluster.coreTx( ( db, transaction ) ->
+        {
+            transaction.getNodeById( nodeId ).createRelationshipTo( transaction.createNode(),
+                    RelationshipType.withName( "TYPE_" + ThreadLocalRandom.current().nextInt( 3 ) ) );
+            transaction.commit();
+        } );
     }
 
     private void doWriteTransaction() throws Exception
@@ -250,8 +385,8 @@ class TransactionLogUpgradeIT
     private boolean isKernelVersion( KernelVersion kernelVersion )
     {
         MutableBoolean allMatch = new MutableBoolean( true );
-        doOnAllMetaDataStore( mds -> allMatch.set( allMatch.get() && mds.kernelVersion().equals( kernelVersion ) ) );
-        return allMatch.get();
+        doOnAllMetaDataStore( mds -> allMatch.setValue( allMatch.booleanValue() && mds.kernelVersion().equals( kernelVersion ) ) );
+        return allMatch.booleanValue();
     }
 
     private void doOnAllMetaDataStore( Consumer<MetaDataStore> metaDataConsumer )
