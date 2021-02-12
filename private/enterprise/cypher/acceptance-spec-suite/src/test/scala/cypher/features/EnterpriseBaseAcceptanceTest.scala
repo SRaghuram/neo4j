6f1ee407e3df5cfab0684b587f14c385b78bf230
Hannes Voigt <30618026+hvub@users.noreply.github.com>
2021-02-12 17:08:55 +0100

    TCK M16 Adjustments (#8644)
    Update OpenCypher dependency to 1.0.0-M16:
    
    * Adjust error message to error detail translation in cypher.features.Neo4jExceptionToExecutionFailed
    * Denylist file discovery now work with blacklist located in filesystem as well as located in a jar file (this is required to run TCK from the TCK repo against a public Neo4j artifact)
    * Denylist can filter for specific examples of a scenario outline by referring to the example number, which the TCK API provides in M16
    * The debug filter facility in cypher.features.BaseFeatureTest allows filtering for a TCK category (like it already can for feature and scenario)
    * The compatibility-spec-suite/src/test/resources/blacklists/interpreted.txt blacklist has been adjust to reflect changes in scenarios name.
    * Update denylists for TCK and for each runtime
    
    Co-authored-by: Hannes Voigt <30618026+hvub@users.noreply.github.com>

--- private/enterprise/cypher/acceptance-spec-suite/src/test/scala/cypher/features/EnterpriseBaseAcceptanceTest.scala
+++ private/enterprise/cypher/acceptance-spec-suite/src/test/scala/cypher/features/EnterpriseBaseAcceptanceTest.scala
@@ -16,6 +16,7 @@ import org.opencypher.tools.tck.api.Scenario
 abstract class EnterpriseBaseAcceptanceTest extends BaseFeatureTest {
   // these two should be empty on commit!
   // Use a substring match, for example "UnwindAcceptance"
+  val categoryToRun = ""
   val featureToRun = ""
   val scenarioToRun = ""
 
@@ -23,12 +24,15 @@ abstract class EnterpriseBaseAcceptanceTest extends BaseFeatureTest {
     (metrics_enabled -> java.lang.Boolean.FALSE) +
     (cypher_worker_count -> java.lang.Integer.valueOf(-1))
 
-  lazy val scenarios: Seq[Scenario] =  filterScenarios(BaseFeatureTestHolder.allAcceptanceScenarios, featureToRun, scenarioToRun)
+  lazy val scenarios: Seq[Scenario] =  filterScenarios(BaseFeatureTestHolder.allAcceptanceScenarios, categoryToRun, featureToRun, scenarioToRun)
 
   @Test
   def debugTokensNeedToBeEmpty(): Unit = {
     // besides the obvious reason this test is also here (and not using assert)
     // to ensure that any import optimizer doesn't remove the correct import for fail (used by the commented out methods further down)
+    if (!categoryToRun.equals(""))
+      fail("categoryToRun is only for debugging and should not be committed")
+
     if (!scenarioToRun.equals(""))
       fail("scenarioToRun is only for debugging and should not be committed")
 
--- private/enterprise/cypher/compatibility-spec-suite/src/test/scala/cypher/features/EnterpriseBaseTCKTests.scala
+++ private/enterprise/cypher/compatibility-spec-suite/src/test/scala/cypher/features/EnterpriseBaseTCKTests.scala
@@ -15,10 +15,11 @@ import org.opencypher.tools.tck.api.Scenario
 abstract class EnterpriseBaseTCKTests extends BaseFeatureTest {
 
   // these two should be empty on commit!
+  val categoryToRun = ""
   val featureToRun = ""
   val scenarioToRun = ""
 
-  lazy val scenarios: Seq[Scenario] = filterScenarios(BaseFeatureTestHolder.allTckScenarios, featureToRun, scenarioToRun)
+  lazy val scenarios: Seq[Scenario] = filterScenarios(BaseFeatureTestHolder.allTckScenarios, categoryToRun, featureToRun, scenarioToRun)
 
   val enterpriseDefaultTestConfig: collection.Map[Setting[_], AnyRef] = defaultTestConfig + (metrics_enabled -> java.lang.Boolean.FALSE)
 
@@ -26,6 +27,9 @@ abstract class EnterpriseBaseTCKTests extends BaseFeatureTest {
   def debugTokensNeedToBeEmpty(): Unit = {
     // besides the obvious reason this test is also here (and not using assert)
     // to ensure that any import optimizer doesn't remove the correct import for fail (used by the commented out methods further down)
+    if (!categoryToRun.equals(""))
+      fail("categoryToRun is only for debugging and should not be committed")
+
     if (!scenarioToRun.equals(""))
       fail("scenarioToRun is only for debugging and should not be committed")
 
