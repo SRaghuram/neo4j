5199dcca0d7acca9509d10df00189abbc6b2d6ed
MishaDemianenko <12656787+mishademianenko@users.noreply.github.com>
2021-03-16 11:07:35 +0100

    Restore commented out tests, other minor changes.

--- private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/dbms/database/EnterpriseIOControllerIT.java
+++ private/enterprise/enterprise-it/kernel-it/src/test/java/com/neo4j/dbms/database/EnterpriseIOControllerIT.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ * This file is a commercial add-on to Neo4j Enterprise Edition.
+ */
 package com.neo4j.dbms.database;
 
 import com.neo4j.kernel.impl.pagecache.iocontroller.ConfigurableIOController;
