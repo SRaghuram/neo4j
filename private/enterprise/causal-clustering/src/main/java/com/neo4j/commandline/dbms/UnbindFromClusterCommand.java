841984f93310f6e2b269f5f9507694c89676c071
Aurélien Arena <aurelien.arena@neotechnology.com>
2021-03-29 16:29:24 +0100

    Unbind command preserving the cluster state folder. (#8977)

--- private/enterprise/causal-clustering/src/main/java/com/neo4j/commandline/dbms/UnbindFromClusterCommand.java
+++ private/enterprise/causal-clustering/src/main/java/com/neo4j/commandline/dbms/UnbindFromClusterCommand.java
@@ -7,32 +7,69 @@ package com.neo4j.commandline.dbms;
 
 import com.neo4j.causalclustering.core.state.ClusterStateLayout;
 import com.neo4j.configuration.CausalClusteringSettings;
+import picocli.CommandLine;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Clock;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 import org.neo4j.cli.AbstractCommand;
 import org.neo4j.cli.CommandFailedException;
 import org.neo4j.cli.ExecutionContext;
 import org.neo4j.commandline.dbms.LockChecker;
 import org.neo4j.configuration.Config;
+import org.neo4j.configuration.ConfigUtils;
 import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.dbms.archive.Utils;
+import org.neo4j.io.compress.ZipUtils;
 import org.neo4j.io.layout.Neo4jLayout;
 import org.neo4j.kernel.internal.locker.FileLockException;
+import org.neo4j.time.Clocks;
 
+import static java.lang.String.format;
+import static org.neo4j.configuration.GraphDatabaseSettings.record_format;
 import static picocli.CommandLine.Command;
+import static picocli.CommandLine.Help.Visibility.ALWAYS;
 
 @Command(
         name = "unbind",
-        header = "Removes all cluster state.",
-        description = "Removes all cluster state, so that the instance can rebind to a cluster."
+        header = "Removes and archives all cluster state.",
+        description = "Removes and archives all cluster state, so that the instance can rebind to a cluster."
 )
 public class UnbindFromClusterCommand extends AbstractCommand
 {
+    @CommandLine.Option( names = "--archive-cluster-state", arity = "1", required = false, showDefaultValue = ALWAYS, paramLabel = "<true/false>",
+            description = "Enable or disable the cluster state archiving." )
+    private boolean isClusterStateArchiveEnabled;
+
+    @CommandLine.Option( names = "--archive-path", required = false, paramLabel = "<path>",
+            description = "Destination (file or folder) of the cluster state archive." )
+    private Path clusterStateArchiveFile;
+
+    private Clock clock;
+
+    private static String ARCHIVE_DEFAULT_NAME_PATTERN = "unbound_cluster_state.%s.zip";
+
+    private static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+                                                             .withZone( ZoneId.systemDefault());
+
     public UnbindFromClusterCommand( ExecutionContext ctx )
     {
         super( ctx );
+        this.clock = Clocks.systemClock();
+    }
+
+    public UnbindFromClusterCommand( ExecutionContext ctx, Clock clock )
+    {
+        super( ctx );
+        this.clock = clock;
     }
 
     private Config loadNeo4jConfig( Path homeDir, Path configDir )
@@ -58,6 +95,11 @@ public class UnbindFromClusterCommand extends AbstractCommand
 
                 if ( ctx.fs().fileExists( clusterStateDirectory ) )
                 {
+                    if ( isClusterStateArchiveEnabled )
+                    {
+                        Path archive = buildArchivePath( neo4jLayout, clusterStateArchiveFile );
+                        archiveClusterState( archive, clusterStateDirectory, neo4jLayout.serverIdFile() );
+                    }
                     deleteClusterStateIn( clusterStateDirectory );
                 }
                 else
@@ -74,11 +116,66 @@ public class UnbindFromClusterCommand extends AbstractCommand
         }
         catch ( Exception e )
         {
-            throw new RuntimeException( e );
+            throw new RuntimeException( e.getMessage(), e );
         }
     }
 
-    private void deleteClusterStateIn( Path target ) throws UnbindFailureException
+    private void archiveClusterState( Path archive, Path clusterStateDirectory, Path serverIdfile ) throws IOException
+    {
+        checkWritableFile( archive );
+
+        zip( clusterStateDirectory, archive );
+
+        if ( Files.exists( serverIdfile ) )
+        {
+            zip( serverIdfile, archive );
+        }
+    }
+
+    private void zip( Path source, Path target ) throws IOException
+    {
+        if ( Files.isReadable( source ) )
+        {
+            try
+            {
+                ZipUtils.zip( ctx.fs(), source, target );
+            }
+            catch ( IOException e )
+            {
+                Files.deleteIfExists( target );
+                throw new IOException(
+                        format( "Unable to create the archive: %s: %s", target.toString(), e.getMessage() ), e );
+            }
+        }
+        else
+        {
+            Files.deleteIfExists( target );
+            throw new IOException( format( "Can not create the archive %s because the following source is not readable: %s.",
+                                           target.toString(), source.toString() ) );
+        }
+    }
+
+    private Path buildArchivePath( Neo4jLayout neo4jLayout, Path clusterStateArchiveFile )
+    {
+        if ( Objects.isNull( clusterStateArchiveFile ) )
+        {
+            return neo4jLayout.homeDirectory().resolve( generateDefaultArchiveName() );
+        }
+        else
+        {
+            return Files.isDirectory( clusterStateArchiveFile )
+                   ? clusterStateArchiveFile.resolve( generateDefaultArchiveName() )
+                   : clusterStateArchiveFile;
+        }
+    }
+
+    private String generateDefaultArchiveName()
+    {
+        String formattedInstant = DATE_TIME_FORMATTER.format( clock.instant() );
+        return format( ARCHIVE_DEFAULT_NAME_PATTERN, formattedInstant );
+    }
+
+    private void deleteClusterStateIn( Path target ) throws IOException
     {
         try
         {
@@ -86,15 +183,27 @@ public class UnbindFromClusterCommand extends AbstractCommand
         }
         catch ( IOException e )
         {
-            throw new UnbindFailureException( e );
+            throw new IOException(
+                    format( "Unable to delete the cluster state directory: %s: %s", target.toString(), e.getMessage() ), e );
         }
     }
 
-    private static class UnbindFailureException extends Exception
+    private void checkWritableFile( Path filePath ) throws FileSystemException
     {
-        UnbindFailureException( Exception e )
+        try
         {
-            super( e );
+            Utils.checkWritableDirectory( filePath.getParent() );
+        }
+        catch ( FileSystemException e )
+        {
+           throw new FileSystemException( filePath.getParent().toAbsolutePath().toString(),
+                                          null,
+                                          format( "Can't write the archive because the destination directory is not writable." ) );
+        }
+
+        if ( Files.exists( filePath ) )
+        {
+            throw new FileAlreadyExistsException( filePath.toAbsolutePath().toString(), null, format( "Archive already exists.") );
         }
     }
 }
--- private/enterprise/causal-clustering/src/test/java/com/neo4j/commandline/dbms/UnbindFromClusterCommandTest.java
+++ private/enterprise/causal-clustering/src/test/java/com/neo4j/commandline/dbms/UnbindFromClusterCommandTest.java
@@ -6,6 +6,7 @@
 package com.neo4j.commandline.dbms;
 
 import com.neo4j.causalclustering.core.state.ClusterStateLayout;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,14 +15,29 @@ import org.junit.jupiter.params.provider.MethodSource;
 import picocli.CommandLine;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Clock;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.neo4j.cli.CommandFailedException;
 import org.neo4j.cli.ExecutionContext;
@@ -33,16 +49,17 @@ import org.neo4j.io.layout.Neo4jLayout;
 import org.neo4j.test.extension.Inject;
 import org.neo4j.test.extension.testdirectory.TestDirectoryExtension;
 import org.neo4j.test.rule.TestDirectory;
+import org.neo4j.time.Clocks;
+import org.neo4j.time.FakeClock;
 
 import static com.neo4j.configuration.CausalClusteringSettings.DEFAULT_CLUSTER_STATE_DIRECTORY_NAME;
 import static com.neo4j.configuration.CausalClusteringSettings.cluster_state_directory;
 import static java.lang.String.format;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATA_DIR_NAME;
@@ -92,22 +109,30 @@ class UnbindFromClusterCommandTest
         {
             CommandLine.usage( command, new PrintStream( out ), CommandLine.Help.Ansi.OFF );
         }
-        assertThat( baos.toString().trim(), equalTo( String.format(
-                "Removes all cluster state.%n" +
+        assertThat( baos.toString().trim() ).isEqualTo( String.format(
+                "Removes and archives all cluster state.%n" +
                 "%n" +
                 "USAGE%n" +
                 "%n" +
-                "unbind [--expand-commands] [--verbose]%n" +
+                "unbind [--expand-commands] [--verbose] [--archive-cluster-state=<true/false>]%n" +
+                "       [--archive-path=<path>]%n" +
                 "%n" +
                 "DESCRIPTION%n" +
                 "%n" +
-                "Removes all cluster state, so that the instance can rebind to a cluster.%n" +
+                "Removes and archives all cluster state, so that the instance can rebind to a%n" +
+                "cluster.%n" +
                 "%n" +
                 "OPTIONS%n" +
                 "%n" +
                 "      --verbose           Enable verbose output.%n" +
-                "      --expand-commands   Allow command expansion in config value evaluation."
-        ) ) );
+                "      --expand-commands   Allow command expansion in config value evaluation.%n" +
+                "      --archive-cluster-state=<true/false>%n" +
+                "                          Enable or disable the cluster state archiving.%n" +
+                "                            Default: false%n" +
+                "      --archive-path=<path>%n" +
+                "                          Destination (file or folder) of the cluster state%n" +
+                "                            archive."
+        ) ) ;
     }
 
     @Test
@@ -124,12 +149,216 @@ class UnbindFromClusterCommandTest
                 CommandLine.populateCommand( command );
                 command.execute();
             } );
-            assertThat( commandException.getMessage(), containsString( "Database is currently locked. Please shutdown database." ) );
+            assertThat( commandException.getMessage() ).contains( "Database is currently locked. Please shutdown database." ) ;
         }
         finally
         {
             fileLock.release();
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void archiveTargetLocationDoesntExist( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+
+        var command = new UnbindFromClusterCommand( ctx );
+
+        // when
+        CommandLine.populateCommand( command, "--archive-cluster-state=true", format( "--archive-path=%s", Path.of( "/location/doesnt/exist/" ).toString() ) );
+
+        Exception exception = assertThrows( Exception.class, () -> command.execute() );
+
+        // then
+        String expectedMessage = "Can't write the archive because the destination directory is not writable.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue( actualMessage.contains( expectedMessage ) );
+        assertTrue( fs.fileExists( clusterStateDir ) );
+        assertTrue( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldFailWhenUserSpecifiedArchiveAlreadyExists( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        var userSpecifiedArchiveFile = createUserSpecifiedArchiveDirectory().resolve( "archive.zip" );
+        Files.createFile( userSpecifiedArchiveFile );
+
+        var command = new UnbindFromClusterCommand( ctx );
+
+        // when
+        CommandLine.populateCommand( command, "--archive-cluster-state=true", format( "--archive-path=%s", userSpecifiedArchiveFile.toString() ) );
+        Exception exception = assertThrows( Exception.class, () -> command.execute() );
+
+        // then
+        String expectedMessage = "Archive already exists.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue( actualMessage.contains( expectedMessage ) );
+        assertTrue( fs.fileExists( clusterStateDir ) );
+        assertTrue( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldFailWhenArchiveAlreadyExists( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        Clock fakeClock = Clocks.fakeClock();
+        createFakeDefaultArchive( fakeClock );
+
+        var command = new UnbindFromClusterCommand( ctx, fakeClock );
+        CommandLine.populateCommand( command, "--archive-cluster-state=true" );
+
+        // when
+        Exception exception = assertThrows( Exception.class, () -> command.execute() );
+
+        // then
+        String expectedMessage = "Archive already exists.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue( actualMessage.contains( expectedMessage ) );
+        assertTrue( fs.fileExists( clusterStateDir ) );
+        assertTrue( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldWriteArchiveAtSpecifiedAbsoluteLocation( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        var userSpecifiedArchiveFile = createUserSpecifiedArchiveDirectory().resolve( "archive.zip" );
+
+        Set<byte[]> expected = getBytesOfFilesIn( List.of( clusterStateDir, serverIdStore ) );
+
+        var command = new UnbindFromClusterCommand( ctx );
+        CommandLine.populateCommand( command, "--archive-cluster-state=true" );
+
+        // when
+        CommandLine.populateCommand( command, format( "--archive-path=%s", userSpecifiedArchiveFile.toString() ) );
+        command.execute();
+
+        // then
+        Set<byte[]> actual = getBytesOfFilesIn( userSpecifiedArchiveFile );
+
+        assertTrue( Files.exists( userSpecifiedArchiveFile ) );
+        assertThat( expected ).containsExactlyInAnyOrderElementsOf( actual );
+        assertFalse( fs.fileExists( clusterStateDir ) );
+        assertFalse( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldWriteArchiveAtSpecifiedLocation( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        var userSpecifiedArchiveDirectory = createUserSpecifiedArchiveDirectory();
+        FakeClock fakeClock = Clocks.fakeClock();
+        Set<byte[]> expected = getBytesOfFilesIn( List.of( clusterStateDir, serverIdStore ) );
+
+        var command = new UnbindFromClusterCommand( ctx, fakeClock );
+
+        // when
+        CommandLine.populateCommand( command, "--archive-cluster-state=true", format( "--archive-path=%s", userSpecifiedArchiveDirectory.toString() ) );
+        command.execute();
+
+        // then
+        Path archive = userSpecifiedArchiveDirectory.resolve( getArchiveNameUsingClock( fakeClock ) );
+        Set<byte[]> actual = getBytesOfFilesIn( archive );
+
+        assertTrue( archiveIsPresentIn( userSpecifiedArchiveDirectory ) );
+        assertThat( expected ).containsExactlyInAnyOrderElementsOf( actual );
+        assertFalse( fs.fileExists( clusterStateDir ) );
+        assertFalse( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldArchiveClusterEvenIfStoreIdDoesntExist( String clusterStateDirectory ) throws Exception
+    {
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        FakeClock fakeClock = Clocks.fakeClock();
+        Set<byte[]> expected = getBytesOfFilesIn( List.of( clusterStateDir ) );
+
+        var command = new UnbindFromClusterCommand( ctx, fakeClock );
+
+        // when
+        CommandLine.populateCommand( command, format( "--archive-cluster-state=true" ) );
+        command.execute();
+
+        // then
+        Path archive = ctx.homeDir().resolve( getArchiveNameUsingClock( fakeClock ) );
+        Set<byte[]> actual = getBytesOfFilesIn( archive );
+
+        assertTrue( archiveIsPresentIn( ctx.homeDir() ) );
+        assertThat( expected ).containsExactlyInAnyOrderElementsOf( actual );
+        assertFalse( fs.fileExists( clusterStateDir ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldArchiveClusterStateDirectoryAndServerId( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        FakeClock fakeClock = Clocks.fakeClock();
+        Set<byte[]> expected = getBytesOfFilesIn( List.of( clusterStateDir, serverIdStore ) );
+
+        var command = new UnbindFromClusterCommand( ctx, fakeClock );
+
+        // when
+        CommandLine.populateCommand( command, format( "--archive-cluster-state=true" ) );
+        command.execute();
+
+        // then
+        Path archive = ctx.homeDir().resolve( getArchiveNameUsingClock( fakeClock ) );
+        Set<byte[]> actual = getBytesOfFilesIn( archive );
+
+        assertTrue( archiveIsPresentIn( ctx.homeDir() ) );
+        assertThat( expected ).containsExactlyInAnyOrderElementsOf( actual );
+        assertFalse( fs.fileExists( clusterStateDir ) );
+        assertFalse( fs.fileExists( serverIdStore ) );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "data" )
+    void shouldNotArchiveClusterStateDirectoryAndServerId( String clusterStateDirectory ) throws Exception
+    {
+        // given
+        createUnlockedFakeDbDir();
+        var clusterStateDir = createClusterStateDirAndSetInConfig( clusterStateDirectory );
+        var serverIdStore = createServerIdStore();
+        var command = new UnbindFromClusterCommand( ctx );
+
+        // when
+        CommandLine.populateCommand( command, format( "--archive-cluster-state=false" ) );
+        command.execute();
+
+        // then
+        assertFalse( archiveIsPresentIn( ctx.homeDir() ) );
+        assertFalse( fs.fileExists( clusterStateDir ) );
+        assertFalse( fs.fileExists( serverIdStore ) );
     }
 
     @ParameterizedTest
@@ -163,11 +392,47 @@ class UnbindFromClusterCommandTest
         verify( err ).println( "This instance was not bound. No work performed." );
     }
 
+    private Path createUserSpecifiedArchiveDirectory() throws IOException
+    {
+        var userArchiveDir = testDir.homePath().resolve( "user_specified_archive_dir" );
+        fs.mkdirs( userArchiveDir );
+        return userArchiveDir;
+    }
+
+    private String getArchiveNameUsingClock( Clock clock )
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "yyyyMMddHHmmss")
+                                                       .withZone( ZoneId.systemDefault());
+        String timestamp = formatter.format( clock.instant() );
+        String fileName = format( "unbound_cluster_state.%s.zip", timestamp );
+        return fileName;
+    }
+
+    private Path createFakeDefaultArchive( Clock clock ) throws IOException
+    {
+        String fileName = getArchiveNameUsingClock( clock );
+        Path archivePath = ctx.homeDir().resolve( fileName );
+        Files.createFile( archivePath );
+        return archivePath;
+    }
+
+    private boolean archiveIsPresentIn( Path path ) throws IOException
+    {
+        return Files.find( path,
+                    1,
+                    ( p, a ) -> {
+                        File file = p.toFile();
+                        return file.isFile() && file.getName().startsWith( "unbound_cluster_state" );
+                    } )
+             .count() == 1;
+    }
+
     private Path createClusterStateDirAndSetInConfig( String def ) throws IOException
     {
         var path = neo4jLayout.homeDirectory().resolve( def );
         var clusterStateDirectory = ClusterStateLayout.of( path ).getClusterStateDirectory();
         fs.mkdirs( clusterStateDirectory );
+        FileUtils.writeStringToFile(  new File( clusterStateDirectory.toFile(), "fake_file" )  , "fake content", Charset.defaultCharset() );
 
         var setting = format( "%s=%s", cluster_state_directory.name(), path.toString().replace( '\\', '/' ) );
         fs.mkdirs( ctx.confDir() );
@@ -199,4 +464,50 @@ class UnbindFromClusterCommandTest
         assertNotNull( fileLock, "Unable to acquire a store lock" );
         return fileLock;
     }
+
+    private Set<byte[]> getBytesOfFilesIn( Collection<Path> paths ) throws IOException
+    {
+        Set<byte[]> bytesSet = new HashSet<>();
+
+        SimpleFileVisitor<Path> simpleFileVisitor = new SimpleFileVisitor<>()
+        {
+            @Override
+            public FileVisitResult visitFile( Path file, BasicFileAttributes attrs )
+                    throws IOException
+            {
+                byte[] bytes = FileUtils.readFileToByteArray( file.toFile() );
+                bytesSet.add( bytes );
+                return FileVisitResult.CONTINUE;
+            }
+        };
+
+        for ( Path path : paths )
+        {
+            Files.walkFileTree( path, simpleFileVisitor );
+        }
+        return bytesSet;
+    }
+
+    private Set<byte[]> getBytesOfFilesIn( Path archive ) throws IOException
+    {
+        Set<byte[]> bytesSet = new HashSet<>();
+
+        try ( ZipInputStream zis = new ZipInputStream( new FileInputStream( archive.toFile() ) ) )
+        {
+            ZipEntry zipEntry = zis.getNextEntry();
+
+            while ( zipEntry != null )
+            {
+                if ( !zipEntry.isDirectory() )
+                {
+                    byte[] allBytes = zis.readAllBytes();
+
+                    bytesSet.add( allBytes );
+                }
+                zipEntry = zis.getNextEntry();
+            }
+        }
+        return  bytesSet;
+    }
+
 }
